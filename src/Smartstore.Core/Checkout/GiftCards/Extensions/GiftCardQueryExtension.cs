c93b2d3195f56aaf52f75fa5161edd40e2bb7e8e
Murat Cakir <muratc@smartstore.de>
2020-12-18 03:59:48 +0100

    DON'T create query modifiers for single criterions!!!!!!!!!!

--- src/Smartstore.Core/Checkout/GiftCards/Extensions/GiftCardQueryExtension.cs
+++ src/Smartstore.Core/Checkout/GiftCards/Extensions/GiftCardQueryExtension.cs
@@ -9,42 +9,6 @@ namespace Smartstore.Core.Checkout.GiftCards
     public static class GiftCardQueryExtension
     {
         /// <summary>
-        /// Gets gift card by identifier
-        /// </summary>
-        /// <param name="giftCardId">Gift card identifier</param>
-        /// <returns>Gift card query filtered by card identifier</returns>
-        public static IQueryable<GiftCard> ApplyCardFilter(this IQueryable<GiftCard> query, int giftCardId)
-        {
-            Guard.NotNull(query, nameof(query));
-
-            return query.Where(x => x.Id == giftCardId);
-        }
-
-        /// <summary>
-        /// Gets gift cards by activation
-        /// </summary>
-        /// <param name="isActivated">Whether gift cards are activated or not</param>
-        /// <returns>Gift cards query filtered by activation</returns>
-        public static IQueryable<GiftCard> ApplyActivationFilter(this IQueryable<GiftCard> query, bool isActivated)
-        {
-            Guard.NotNull(query, nameof(query));
-
-            return query.Where(x => x.IsActivated == isActivated);
-        }
-
-        /// <summary>
-        /// Gets gift cards by orderItemId
-        /// </summary>
-        /// <param name="orderItemId">Purchased with order item identifier</param>
-        /// <returns>Gift cards query filtered by order item identifier</returns>
-        public static IQueryable<GiftCard> ApplyOrderItemFilter(this IQueryable<GiftCard> query, int orderItemId)
-        {
-            Guard.NotNull(query, nameof(query));
-
-            return query.Where(x => x.OrderItemId == orderItemId);
-        }
-
-        /// <summary>
         /// Get gift cards within time period
         /// </summary>        
         /// <param name="startTime">Start date time inclusive</param>
@@ -73,18 +37,6 @@ namespace Smartstore.Core.Checkout.GiftCards
             Guard.NotNull(query, nameof(query));
 
             return query.Where(x => couponCodes.Contains(x.CouponCode));
-        }
-
-        /// <summary>
-        /// Gets gift cards by coupon code
-        /// </summary>        
-        /// <param name="couponCodes">Coupon code to match</param>
-        /// <returns>Gift cards query filtered by coupon code</returns>
-        public static IQueryable<GiftCard> ApplyCouponCodeFilter(this IQueryable<GiftCard> query, string couponCode)
-        {
-            Guard.NotNull(query, nameof(query));
-
-            return query.Where(x => x.CouponCode == couponCode);
         }
     }
 }
