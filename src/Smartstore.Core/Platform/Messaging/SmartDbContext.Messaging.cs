a55892bbdb742fabc3afd24bcd4d1c96cb0b14a9
Murat Cakir <muratc@smartstore.de>
2020-12-29 20:27:12 +0100

    No empty lines between props in a group please, unless they have dev docs.

--- src/Smartstore.Core/Platform/Messaging/SmartDbContext.Messaging.cs
+++ src/Smartstore.Core/Platform/Messaging/SmartDbContext.Messaging.cs
@@ -6,15 +6,10 @@ namespace Smartstore.Core.Data
     public partial class SmartDbContext
     {
         public DbSet<Campaign> Campaigns { get; set; }
-
         public DbSet<EmailAccount> EmailAccounts { get; set; }
-
         public DbSet<MessageTemplate> MessageTemplates { get; set; }
-
         public DbSet<NewsLetterSubscription> NewsLetterSubscriptions { get; set; }
-
         public DbSet<QueuedEmail> QueuedEmails { get; set; }
-
         public DbSet<QueuedEmailAttachment> QueuedEmailAttachments { get; set; }
     }
 }
