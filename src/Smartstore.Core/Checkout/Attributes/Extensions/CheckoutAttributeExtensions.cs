0615932ff6d584706e1b655b7dfe29175b54628b
Marcel Schmidt <marcel.schmidt@smartstore.com>
2020-12-29 21:57:06 +0100

    Checkout attributes extension

--- src/Smartstore.Core/Checkout/Attributes/Extensions/CheckoutAttributeExtensions.cs
+++ src/Smartstore.Core/Checkout/Attributes/Extensions/CheckoutAttributeExtensions.cs
@@ -1,9 +1,7 @@
-﻿using Smartstore.Core.Catalog.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Xml;
+using Smartstore.Core.Catalog.Attributes;
+using Smartstore.Core.Checkout.Cart;
 
 namespace Smartstore.Core.Checkout.Attributes
 {
@@ -18,86 +16,39 @@ namespace Smartstore.Core.Checkout.Attributes
         public static bool ShouldHaveValues(this CheckoutAttribute attribute)
         {
             return attribute is not null
-                && attribute.AttributeControlType is not AttributeControlType.TextBox 
+                && attribute.AttributeControlType is not AttributeControlType.TextBox
                 or AttributeControlType.MultilineTextbox
                 or AttributeControlType.Datepicker
                 or AttributeControlType.FileUpload;
         }
 
         /// <summary>
-        /// Removes attributes from list which require shippable products
-        /// </summary>
-        public static IEnumerable<CheckoutAttribute> RemoveShippableAttributes(this IEnumerable<CheckoutAttribute> attributes)
+        /// Removes attributes from list which require shippable products, if there are no shippable products in the cart
+        /// </summary>        
+        public static void RemoveShippableAttributes(this IEnumerable<CheckoutAttribute> attributes, IList<OrganizedShoppingCartItem> cart)
         {
             Guard.NotNull(attributes, nameof(attributes));
 
-            return attributes.Where(x => !x.ShippableProductRequired).ToList();
+            if (cart.IsNullOrEmpty() || !cart.IsShippingRequired())
+                return;
+
+            attributes = attributes.Where(x => !x.ShippableProductRequired);
         }
 
         /// <summary>
-        /// Adds an attribute value xml element
+        /// Gets checkout attribute values by id. 
         /// </summary>
-        public static string AddAttribute(this CheckoutAttribute attribute, string value, string attributes = "")
+        /// <param name="attributes"></param>
+        /// <param name="attributeId"></param>
+        /// <returns></returns>
+        public static List<string> GetAttributeValuesById(this IEnumerable<CheckoutAttribute> attributes, int attributeId)
         {
-            Guard.NotNull(attribute, nameof(attribute));
-            Guard.NotNull(value, nameof(value));
+            Guard.NotNull(attributes, nameof(attributes));
 
-            // TODO: (core) Build a convenient helper type for attributesXml (e.g. "ProductAttributeSelection")
-
-            var result = string.Empty;
-            try
-            {
-                var xmlDoc = new XmlDocument();
-                if (!attributes.HasValue())
-                {
-                    xmlDoc.AppendChild(xmlDoc.CreateElement("Attributes"));
-                }
-                else
-                {
-                    xmlDoc.LoadXml(attributes);
-                }
-
-                var rootElement = (XmlElement)xmlDoc.SelectSingleNode(@"//Attributes");
-
-                // Find existing
-                XmlElement xmlAttribute = null;
-                var nodeList = xmlDoc.SelectNodes(@"//Attributes/CheckoutAttribute");
-                foreach (XmlNode node in nodeList)
-                {
-                    if (node.Attributes is null || node.Attributes["ID"] is null)
-                        continue;
-
-                    var str = node.Attributes["ID"].InnerText.Trim();
-                    if (int.TryParse(str, out var id) && id == attribute.Id)
-                    {
-                        xmlAttribute = (XmlElement)node;
-                        break;
-                    }
-                }
-
-                // Create a new one if none was found
-                if (xmlAttribute is null)
-                {
-                    xmlAttribute = xmlDoc.CreateElement("CheckoutAttribute");
-                    xmlAttribute.SetAttribute("ID", attribute.Id.ToString());
-                    rootElement.AppendChild(xmlAttribute);
-                }
-
-                var xmlAttributeValue = xmlDoc.CreateElement("CheckoutAttributeValue");
-                xmlAttribute.AppendChild(xmlAttributeValue);
-
-                var xmlValue = xmlDoc.CreateElement("Value");
-                xmlValue.InnerText = value;
-                xmlAttributeValue.AppendChild(xmlValue);
-
-                result = xmlDoc.OuterXml;
-            }
-            catch (Exception ex)
-            {
-                Debug.Write(ex.ToString());
-            }
-
-            return result;
+            return attributes
+                .Where(x => x.Id == attributeId)
+                .SelectMany(x => x.CheckoutAttributeValues.Select(x => x.Id.ToString()))
+                .ToList();
         }
     }
 }
