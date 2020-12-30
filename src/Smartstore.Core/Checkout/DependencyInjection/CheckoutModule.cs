723de9d9ba4e61f4cf12ae3e0071471f8960d3cd
Murat Cakir <muratc@smartstore.de>
2020-12-30 03:05:17 +0100

    Added missing CheckoutModule

---
+++ src/Smartstore.Core/Checkout/DependencyInjection/CheckoutModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Smartstore.Core.Checkout.Attributes;
+using Smartstore.Core.Checkout.GiftCards;
+
+namespace Smartstore.Core.DependencyInjection
+{
+    public sealed class CheckoutModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CheckoutAttributeFormatter>().As<ICheckoutAttributeFormatter>().InstancePerLifetimeScope();
+            builder.RegisterType<CheckoutAttributeParser>().As<ICheckoutAttributeParser>().InstancePerLifetimeScope();
+            builder.RegisterType<GiftCardService>().As<IGiftCardService>().InstancePerLifetimeScope();
+        }
+    }
+}
