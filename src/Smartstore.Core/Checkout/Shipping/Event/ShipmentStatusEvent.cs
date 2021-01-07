697f2830bd670a4b23c055eaf42d0d11b4432982
Marcel Schmidt <marcel.schmidt@smartstore.com>
2021-01-07 22:03:16 +0100

    Shipping method type + Shipment status event

---
+++ src/Smartstore.Core/Checkout/Shipping/Event/ShipmentStatusEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Smartstore.Core.Checkout.Shipping.Event
+{
+    /// <summary>
+    /// Represents a shipment status event
+    /// </summary>
+    public partial class ShipmentStatusEvent
+    {
+        /// <summary>
+        /// Gets or sets Event name
+        /// </summary>
+        public string EventName { get; set; }
+        /// <summary>
+        /// Gets or sets the location
+        /// </summary>
+        public string Location { get; set; }
+        /// <summary>
+        /// Gets or sets the Two-letter country code
+        /// </summary>
+        public string CountryCode { get; set; }
+        /// <summary>
+        /// Gets or sets the date time
+        /// </summary>
+        public DateTime? Date { get; set; }
+    }
+}
