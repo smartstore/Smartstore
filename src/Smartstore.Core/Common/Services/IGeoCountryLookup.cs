17bd5abc357418f0d39fcc84325b6fca03302971
Michael Herzog <herzog@smartstore.com>
2021-01-11 10:11:58 +0100

    Added missing GeoLookupService to Common module

---
+++ src/Smartstore.Core/Common/Services/IGeoCountryLookup.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+namespace Smartstore.Core.Common.Services
+{
+    public sealed class LookupCountryResponse
+    {
+        /// <summary>
+        /// The GeoName ID for the country. 
+        /// </summary>
+        public int? GeoNameId { get; set; }
+
+        /// <summary>
+        /// The english name of the country.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The two-letter ISO 3166-1 alpha code for the country
+        /// </summary>
+        public string IsoCode { get; set; }
+
+        /// <summary>
+        /// This is true if the country is a member state of the European Union.
+        /// </summary>
+        public bool IsInEu { get; set; }
+    }
+
+    /// <summary>
+    /// Country lookup helper for IPv4/6 addresses
+    /// </summary>
+    public partial interface IGeoCountryLookup
+    {
+        LookupCountryResponse LookupCountry(string addr);
+
+        LookupCountryResponse LookupCountry(IPAddress addr);
+    }
+}
---
+++ src/Smartstore.Core/Common/Services/_GeoCountryLookup.cs
@@ -0,0 +1,59 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Smartstore.Core.Common.Services
+//{
+//    public partial class GeoCountryLookup : DisposableObject, IGeoCountryLookup
+//    {
+//        private readonly DatabaseReader _reader;
+
+//        public GeoCountryLookup()
+//        {
+//            // Old database not usable anymore due to US (Florida?) legal issues from 01 Jan 2020 on.
+//            //_reader = new DatabaseReader(CommonHelper.MapPath("~/App_Data/GeoLite2/GeoLite2-Country.mmdb"));
+
+//            _reader = new DatabaseReader(CommonHelper.MapPath("~/App_Data/Geo/dbip-country-lite.mmdb"));
+//        }
+
+//        public LookupCountryResponse LookupCountry(string addr)
+//        {
+//            if (addr.HasValue() && IPAddress.TryParse(addr, out var ipAddress))
+//            {
+//                return LookupCountry(ipAddress);
+//            }
+
+//            return null;
+//        }
+
+//        public LookupCountryResponse LookupCountry(IPAddress addr)
+//        {
+//            Guard.NotNull(addr, nameof(addr));
+
+//            if (_reader.TryCountry(addr, out var response) && response.Country != null)
+//            {
+//                var country = response.Country;
+//                return new LookupCountryResponse
+//                {
+//                    GeoNameId = country.GeoNameId,
+//                    IsoCode = country.IsoCode,
+//                    Name = country.Name,
+//                    IsInEu = country.IsInEuropeanUnion
+//                };
+//            }
+
+//            return null;
+//        }
+
+//        protected override void OnDispose(bool disposing)
+//        {
+//            if (disposing && _reader != null)
+//            {
+//                _reader.Dispose();
+//            }
+//        }
+//    }
+//}
