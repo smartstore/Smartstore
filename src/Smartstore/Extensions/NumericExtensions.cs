7a59144da3a411fe497f4c169d5efc858df14246
Murat Cakir <muratc@smartstore.de>
2021-01-12 22:02:49 +0100

    Added Clamp extension methods

--- src/Smartstore/Extensions/NumericExtensions.cs
+++ src/Smartstore/Extensions/NumericExtensions.cs
@@ -110,5 +110,135 @@ namespace Smartstore
         }
 
         #endregion
+
+        #region Clamp
+
+        /// <summary>
+        /// Restricts a <see cref="byte"/> to be within a specified range.
+        /// </summary>
+        /// <param name="value">The value to clamp.</param>
+        /// <param name="min">The minimum value. If value is less than min, min will be returned.</param>
+        /// <param name="max">The maximum value. If value is greater than max, max will be returned.</param>
+        /// <returns>
+        /// The <see cref="byte"/> representing the clamped value.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte Clamp(this byte value, byte min, byte max)
+        {
+            // Order is important here as someone might set min to higher than max.
+            if (value >= max)
+            {
+                return max;
+            }
+
+            if (value <= min)
+            {
+                return min;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Restricts a <see cref="uint"/> to be within a specified range.
+        /// </summary>
+        /// <param name="value">The The value to clamp.</param>
+        /// <param name="min">The minimum value. If value is less than min, min will be returned.</param>
+        /// <param name="max">The maximum value. If value is greater than max, max will be returned.</param>
+        /// <returns>
+        /// The <see cref="int"/> representing the clamped value.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint Clamp(this uint value, uint min, uint max)
+        {
+            if (value >= max)
+            {
+                return max;
+            }
+
+            if (value <= min)
+            {
+                return min;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Restricts a <see cref="int"/> to be within a specified range.
+        /// </summary>
+        /// <param name="value">The The value to clamp.</param>
+        /// <param name="min">The minimum value. If value is less than min, min will be returned.</param>
+        /// <param name="max">The maximum value. If value is greater than max, max will be returned.</param>
+        /// <returns>
+        /// The <see cref="int"/> representing the clamped value.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Clamp(this int value, int min, int max)
+        {
+            if (value >= max)
+            {
+                return max;
+            }
+
+            if (value <= min)
+            {
+                return min;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Restricts a <see cref="float"/> to be within a specified range.
+        /// </summary>
+        /// <param name="value">The The value to clamp.</param>
+        /// <param name="min">The minimum value. If value is less than min, min will be returned.</param>
+        /// <param name="max">The maximum value. If value is greater than max, max will be returned.</param>
+        /// <returns>
+        /// The <see cref="float"/> representing the clamped value.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Clamp(this float value, float min, float max)
+        {
+            if (value >= max)
+            {
+                return max;
+            }
+
+            if (value <= min)
+            {
+                return min;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Restricts a <see cref="double"/> to be within a specified range.
+        /// </summary>
+        /// <param name="value">The The value to clamp.</param>
+        /// <param name="min">The minimum value. If value is less than min, min will be returned.</param>
+        /// <param name="max">The maximum value. If value is greater than max, max will be returned.</param>
+        /// <returns>
+        /// The <see cref="double"/> representing the clamped value.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double Clamp(this double value, double min, double max)
+        {
+            if (value >= max)
+            {
+                return max;
+            }
+
+            if (value <= min)
+            {
+                return min;
+            }
+
+            return value;
+        }
+
+        #endregion
     }
 }
