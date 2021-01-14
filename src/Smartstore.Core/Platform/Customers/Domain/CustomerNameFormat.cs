926472fd04e2aedfefe30a1a9f32b84096170fbc
Michael Herzog <herzog@smartstore.com>
2021-01-14 12:08:34 +0100

    More customer domain objects

---
+++ src/Smartstore.Core/Platform/Customers/Domain/CustomerNameFormat.cs
@@ -0,0 +1,29 @@
+﻿namespace Smartstore.Core.Customers
+{
+    /// <summary>
+    /// Represents the customer name fortatting enumeration.
+    /// </summary>
+    public enum CustomerNameFormat
+    {
+        /// <summary>
+        /// Show emails
+        /// </summary>
+        ShowEmail = 1,
+        /// <summary>
+        /// Show usernames
+        /// </summary>
+        ShowUsername = 2,
+        /// <summary>
+        /// Show full names
+        /// </summary>
+        ShowFullName = 3,
+        /// <summary>
+        /// Show first name
+        /// </summary>
+        ShowFirstName = 4,
+        /// <summary>
+        /// Show shorted name and city
+        /// </summary>
+        ShowNameAndCity = 5
+    }
+}
---
+++ src/Smartstore.Core/Platform/Customers/Domain/SystemCustomerRoleNames.cs
@@ -0,0 +1,15 @@
+﻿namespace Smartstore.Core.Customers
+{
+    public static partial class SystemCustomerRoleNames
+    {
+        public static string SuperAdministrators => "SuperAdmins";
+
+        public static string Administrators => "Administrators";
+
+        public static string ForumModerators => "ForumModerators";
+
+        public static string Registered => "Registered";
+
+        public static string Guests => "Guests";
+    }
+}
