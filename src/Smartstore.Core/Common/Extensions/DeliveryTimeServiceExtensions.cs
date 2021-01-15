c3184c9a767dcc0c3c501929c849d8605521c773
Michael Herzog <herzog@smartstore.com>
2021-01-15 16:16:42 +0100

    Used product extension method for GetDeliveryTimeAsync

--- src/Smartstore.Core/Common/Extensions/DeliveryTimeServiceExtensions.cs
+++ src/Smartstore.Core/Common/Extensions/DeliveryTimeServiceExtensions.cs
@@ -2,24 +2,19 @@
 using Smartstore.Core.Catalog.Products;
 using Smartstore.Core.Common;
 using Smartstore.Core.Common.Services;
+using Smartstore.Core.Domain.Catalog;
 
 namespace Smartstore
 {
     public static class DeliveryTimeServiceExtensions
     {
         /// <summary>
-        /// Gets the product delivery time according to stock
+        /// Gets the product delivery time according to stock.
         /// </summary>
-        public static Task<DeliveryTime> GetDeliveryTimeAsync(this IDeliveryTimeService service, Product product, bool displayAccordingToStock)
+        public static Task<DeliveryTime> GetDeliveryTimeAsync(this IDeliveryTimeService service, Product product, CatalogSettings catalogSettings)
         {
-            var deliveryTimeId = product.GetDeliveryTimeIdAccordingToStock(displayAccordingToStock);
+            var deliveryTimeId = product.GetDeliveryTimeIdAccordingToStock(catalogSettings);
             return service.GetDeliveryTimeAsync(deliveryTimeId, true);
-        }
-
-        public static int GetDeliveryTimeIdAccordingToStock(this Product product, bool displayAccordingToStock)
-        {
-            // TODO: (MH) (core) Use product extension when it's ready.
-            return 0;
         }
     }
 }
