9fd971d93a5376a02a6d1fab73711714c6adc53e
Marcel Schmidt <marcel.schmidt@smartstore.com>
2021-01-18 22:19:14 +0100

    IAttributesAware interface

---
+++ src/Smartstore/Domain/IAttributeAware.cs
@@ -0,0 +1,13 @@
+﻿namespace Smartstore.Domain
+{
+    /// <summary>
+    /// Represents an entity with attributes.
+    /// </summary>
+    public partial interface IAttributeAware
+    {
+        /// <summary>
+        /// Gets or sets the raw attributes string in XML or JSON format.
+        /// </summary>
+        public string RawAttributes { get; set; }
+    }
+}
