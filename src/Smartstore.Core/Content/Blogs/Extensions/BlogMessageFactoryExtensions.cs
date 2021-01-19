ffdb3f915fcc1b1a2cd921c4b45588da8ccbbeed
Michael Herzog <herzog@smartstore.com>
2021-01-19 16:27:54 +0100

    Blog service

---
+++ src/Smartstore.Core/Content/Blogs/Extensions/BlogMessageFactoryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Smartstore.Core.Messages;
+
+namespace Smartstore.Core.Content.Blogs
+{
+    public static class BlogMessageFactoryExtensions
+    {
+        /// <summary>
+        /// Sends a blog comment notification message to a store owner.
+        /// </summary>
+        public static Task<CreateMessageResult> SendBlogCommentNotificationMessage(this IMessageFactory factory, BlogComment blogComment, int languageId = 0)
+        {
+            Guard.NotNull(blogComment, nameof(blogComment));
+
+            return factory.CreateMessageAsync(MessageContext.Create(MessageTemplateNames.BlogCommentStoreOwner, languageId, customer: blogComment.Customer), true, blogComment);
+        }
+    }
+}
