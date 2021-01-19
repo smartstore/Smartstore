c3bf8c8d34a0775743cd837b6b1fe6a4ff965357
Michael Herzog <herzog@smartstore.com>
2021-01-19 17:43:00 +0100

    Registered some services

---
+++ src/Smartstore.Core/Content/DependencyInjection/ContentModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using Smartstore.Core.Content.Blogs;
+using Smartstore.Core.Content.News;
+
+namespace Smartstore.Core.DependencyInjection
+{
+    public sealed class ContentModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<BlogService>().As<IBlogService>().InstancePerLifetimeScope();
+            builder.RegisterType<NewsService>().As<INewsService>().InstancePerLifetimeScope();
+        }
+    }
+}
--- src/Smartstore.Core/Platform/Messaging/DependencyInjection/MessagingModule.cs
+++ src/Smartstore.Core/Platform/Messaging/DependencyInjection/MessagingModule.cs
@@ -10,6 +10,9 @@ namespace Smartstore.Core.DependencyInjection
             builder.RegisterType<EmailAccountService>().As<IEmailAccountService>().InstancePerLifetimeScope();
             builder.RegisterType<QueuedEmailService>().As<IQueuedEmailService>().InstancePerLifetimeScope();
             builder.RegisterType<NewsletterSubscriptionService>().As<INewsletterSubscriptionService>().InstancePerLifetimeScope();
+            builder.RegisterType<MessageFactory>().As<IMessageFactory>().InstancePerLifetimeScope();
+            builder.RegisterType<MessageModelProvider>().As<IMessageModelProvider>().InstancePerLifetimeScope();
+            builder.RegisterType<CampaignService>().As<ICampaignService>().InstancePerLifetimeScope();
         }
     }
 }
