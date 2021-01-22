a5d87a38f01b150d102da78c9d8c10e7456256ac
Murat Cakir <muratc@smartstore.de>
2021-01-22 23:01:35 +0100

    Media IUrlHelper extensions

---
+++ src/Smartstore.Core/Content/Media/Extensions/IUrlHelperExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Smartstore.Core.Content.Media.Imaging;
+
+namespace Smartstore.Core.Content.Media
+{
+    public static class IUrlHelperExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string Media(this IUrlHelper urlHelper, MediaFileInfo file, int thumbnailSize = 0, string host = null, bool doFallback = true)
+        {
+            var mediaService = urlHelper.ActionContext.HttpContext.RequestServices.GetRequiredService<IMediaService>();
+            return mediaService.GetUrl(file, thumbnailSize, host, doFallback);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string Media(this IUrlHelper urlHelper, MediaFileInfo file, ProcessImageQuery query, string host = null, bool doFallback = true)
+        {
+            var mediaService = urlHelper.ActionContext.HttpContext.RequestServices.GetRequiredService<IMediaService>();
+            return mediaService.GetUrl(file, query, host, doFallback);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Task<string> MediaAsync(this IUrlHelper urlHelper, int? fileId, int thumbnailSize = 0, string host = null, bool doFallback = true)
+        {
+            var mediaService = urlHelper.ActionContext.HttpContext.RequestServices.GetRequiredService<IMediaService>();
+            return mediaService.GetUrlAsync(fileId, thumbnailSize, host, doFallback);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Task<string> MediaAsync(this IUrlHelper urlHelper, int? fileId, ProcessImageQuery query, string host = null, bool doFallback = true)
+        {
+            var mediaService = urlHelper.ActionContext.HttpContext.RequestServices.GetRequiredService<IMediaService>();
+            return mediaService.GetUrlAsync(fileId, query, host, doFallback);
+        }
+    }
+}
