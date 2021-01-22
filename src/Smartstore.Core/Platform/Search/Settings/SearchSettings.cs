f6b7532a6f3434cf16f795dd36e79f5cfb4b2bbd
Murat Cakir <muratc@smartstore.de>
2021-01-22 20:41:03 +0100

    Prefer property initializer over ctor.

--- src/Smartstore.Core/Platform/Search/Settings/SearchSettings.cs
+++ src/Smartstore.Core/Platform/Search/Settings/SearchSettings.cs
@@ -7,70 +7,50 @@ namespace Smartstore.Core.Search
     // TODO: (mg) (core) Put catalog specific search stuff to Smartstore.Core.Catalog.Search.
     public class SearchSettings : ISettings
     {
-        public SearchSettings()
-        {
-            SearchMode = SearchMode.Contains;
-            SearchFields = new List<string> { "sku", "shortdescription", "tagname", "manufacturer", "category" };
-            InstantSearchEnabled = true;
-            ShowProductImagesInInstantSearch = true;
-            InstantSearchNumberOfProducts = 10;
-            InstantSearchTermMinLength = 2;
-            FilterMinHitCount = 1;
-            FilterMaxChoicesCount = 20;
-            DefaultSortOrder = ProductSortingEnum.Relevance;
-
-            BrandDisplayOrder = 1;
-            PriceDisplayOrder = 2;
-            RatingDisplayOrder = 3;
-            DeliveryTimeDisplayOrder = 4;
-            AvailabilityDisplayOrder = 5;
-            NewArrivalsDisplayOrder = 6;
-        }
-
         /// <summary>
         /// Gets or sets the search mode
         /// </summary>
-        public SearchMode SearchMode { get; set; }
+        public SearchMode SearchMode { get; set; } = SearchMode.Contains;
 
         /// <summary>
         /// Gets or sets name of fields to be searched. The name field is always searched.
         /// </summary>
-        public List<string> SearchFields { get; set; }
+        public List<string> SearchFields { get; set; } = new List<string> { "sku", "shortdescription", "tagname", "manufacturer", "category" };
 
         /// <summary>
         /// Gets or sets a value indicating whether instant-search is enabled
         /// </summary>
-        public bool InstantSearchEnabled { get; set; }
+        public bool InstantSearchEnabled { get; set; } = true;
 
         /// <summary>
         /// Gets or sets a value indicating whether to show product images in instant-search result
         /// </summary>
-        public bool ShowProductImagesInInstantSearch { get; set; }
+        public bool ShowProductImagesInInstantSearch { get; set; } = true;
 
         /// <summary>
         /// Gets or sets the number of products to return when using "instant-search" feature
         /// </summary>
-        public int InstantSearchNumberOfProducts { get; set; }
+        public int InstantSearchNumberOfProducts { get; set; } = 10;
 
         /// <summary>
         /// Gets or sets a minimum instant-search term length
         /// </summary>
-        public int InstantSearchTermMinLength { get; set; }
+        public int InstantSearchTermMinLength { get; set; } = 2;
 
         /// <summary>
         /// Gets or sets the minimum hit count for a filter value. Values with a lower hit count are not displayed.
         /// </summary>
-        public int FilterMinHitCount { get; set; }
+        public int FilterMinHitCount { get; set; } = 1;
 
         /// <summary>
         /// Gets or sets the maximum number of filter values to be displayed.
         /// </summary>
-        public int FilterMaxChoicesCount { get; set; }
+        public int FilterMaxChoicesCount { get; set; } = 20;
 
         /// <summary>
         /// Gets or sets the default sort order in search results
         /// </summary>
-        public ProductSortingEnum DefaultSortOrder { get; set; }
+        public ProductSortingEnum DefaultSortOrder { get; set; } = ProductSortingEnum.Relevance;
 
         /// <summary>
         /// Hidden setting indicating whether to use catalog search instead of Linq search in backend.
@@ -98,12 +78,12 @@ namespace Smartstore.Core.Search
         public bool AvailabilityDisabled { get; set; }
         public bool NewArrivalsDisabled { get; set; }
 
-        public int BrandDisplayOrder { get; set; }
-        public int PriceDisplayOrder { get; set; }
-        public int RatingDisplayOrder { get; set; }
-        public int DeliveryTimeDisplayOrder { get; set; }
-        public int AvailabilityDisplayOrder { get; set; }
-        public int NewArrivalsDisplayOrder { get; set; }
+        public int BrandDisplayOrder { get; set; } = 1;
+        public int PriceDisplayOrder { get; set; } = 2;
+        public int RatingDisplayOrder { get; set; } = 3;
+        public int DeliveryTimeDisplayOrder { get; set; } = 4;
+        public int AvailabilityDisplayOrder { get; set; } = 5;
+        public int NewArrivalsDisplayOrder { get; set; } = 6;
 
         #endregion
     }
