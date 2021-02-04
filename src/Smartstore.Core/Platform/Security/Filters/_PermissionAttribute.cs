b0d0f8ae4131562ac6bd7c79fbb4df91210c43a1
Murat Cakir <muratc@smartstore.de>
2021-02-04 21:46:15 +0100

    PermissionAttribute

---
+++ src/Smartstore.Core/Platform/Security/Filters/_PermissionAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Smartstore.Core.Security
+{
+    /// <summary>
+    /// Checks request permission for the current user.
+    /// </summary>
+    public class PermissionAttribute : TypeFilterAttribute
+    {
+        /// <summary>
+        /// e.g. [Permission(PermissionSystemNames.Customer.Read)]
+        /// </summary>
+        /// <param name="systemName">The system name of the permission.</param>
+        /// <param name="showUnauthorizedMessage">Whether to show an unauthorization message.</param>
+        public PermissionAttribute(string systemName, bool showUnauthorizedMessage = true) 
+            : base(typeof(PermissionFilter))
+        {
+            Arguments = new object[] { systemName, showUnauthorizedMessage };
+
+            SystemName = systemName;
+            ShowUnauthorizedMessage = showUnauthorizedMessage;
+        }
+
+        /// <summary>
+        /// The system name of the permission.
+        /// </summary>
+        public string SystemName { get; }
+
+        /// <summary>
+        /// Whether to show an unauthorization message.
+        /// </summary>
+        public bool ShowUnauthorizedMessage { get; }
+
+        class PermissionFilter : IAsyncAuthorizationFilter
+        {
+            private readonly IWorkContext _workContext;
+            private readonly IPermissionService _permissionService;
+
+            public PermissionFilter(IWorkContext workContext, IPermissionService permissionService)
+            {
+                _workContext = workContext;
+                _permissionService = permissionService;
+            }
+
+            public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+            {
+                // TODO: (core) Implement PermissionFilter
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
