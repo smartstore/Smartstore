56c6813bb4ecd5478b7ddeca84d9a4283d22fbcc
Michael Herzog <herzog@smartstore.com>
2021-02-10 16:34:02 +0100

    BlockHandlerBase

--- src/Smartstore.Core/Content/Blocks/BlockHandlerBase.cs
+++ src/Smartstore.Core/Content/Blocks/BlockHandlerBase.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Newtonsoft.Json;
@@ -101,20 +102,89 @@ namespace Smartstore.Core.Content.Blocks
 
 		public void Render(IBlockContainer element, IEnumerable<string> templates, IHtmlHelper htmlHelper)
 		{
-			// TODO: (mh) (core) Implement BlockHandlerBase.Render()
-			//RenderCore(element, templates, htmlHelper, htmlHelper.ViewContext.Writer);
+			RenderCore(element, templates, htmlHelper, htmlHelper.ViewContext.Writer);
 			throw new NotImplementedException();
 		}
 
 		public IHtmlContent ToHtmlContent(IBlockContainer element, IEnumerable<string> templates, IHtmlHelper htmlHelper)
 		{
-			// TODO: (mh) (core) Implement BlockHandlerBase.ToHtmlContent()
 			using (var writer = new StringWriter(CultureInfo.CurrentCulture))
 			{
-				//RenderCore(element, templates, htmlHelper, writer);
-				//return MvcHtmlString.Create(writer.ToString());
-				throw new NotImplementedException();
+				RenderCore(element, templates, htmlHelper, writer);
+				return new HtmlString(writer.ToString());
 			}
+		}
+
+		protected virtual void RenderCore(IBlockContainer element, IEnumerable<string> templates, IHtmlHelper htmlHelper, TextWriter textWriter)
+		{
+			RenderByView(element, templates, htmlHelper, textWriter);
+		}
+
+        protected void RenderByView(IBlockContainer element, IEnumerable<string> templates, IHtmlHelper htmlHelper, TextWriter textWriter)
+        {
+            Guard.NotNull(element, nameof(element));
+            Guard.NotNull(templates, nameof(templates));
+            Guard.NotNull(htmlHelper, nameof(htmlHelper));
+            Guard.NotNull(textWriter, nameof(textWriter));
+
+            var viewContext = htmlHelper.ViewContext;
+
+            if (!element.Metadata.IsInbuilt)
+            {
+                // Change "area" token in RouteData in order to begin search in the plugin's view folder.
+                var originalRouteData = htmlHelper.ViewContext.RouteData;
+                var routeData = new RouteData(originalRouteData);
+                routeData.Values.Merge(originalRouteData.Values);
+                routeData.DataTokens["area"] = element.Metadata.AreaName;
+
+                viewContext = new ViewContext
+                {
+                    RouteData = routeData,
+                    HttpContext = htmlHelper.ViewContext.HttpContext
+                };
+            }
+
+            var viewResult = FindFirstView(element.Metadata, templates, viewContext, out var searchedLocations);
+
+            if (viewResult == null)
+            {
+                var msg = string.Format("No template found for '{0}'. Searched locations:\n{1}.", string.Join(", ", templates), string.Join("\n", searchedLocations));
+                Logger.Debug(msg);
+                throw new FileNotFoundException(msg);
+            }
+
+            // TODO: (mh) (core) Test this.
+            var viewData = new ViewDataDictionary((ViewDataDictionary)element.Block);
+            viewData.TemplateInfo.HtmlFieldPrefix = "Block";
+
+            viewContext = new ViewContext(
+                htmlHelper.ViewContext,
+                viewResult.View,
+                viewData,
+                htmlHelper.ViewContext.TempData,
+                textWriter,
+                new HtmlHelperOptions());
+
+            viewResult.View.RenderAsync(viewContext);
+        }
+
+		private ViewEngineResult FindFirstView(IBlockMetadata blockMetadata, IEnumerable<string> templates, ViewContext viewContext, out ICollection<string> searchedLocations)
+		{
+			searchedLocations = new List<string>();
+
+			var viewEngine = Services.Resolve<IRazorViewEngine>();
+			foreach (var template in templates)
+			{
+				var viewName = string.Concat("BlockTemplates/", blockMetadata.SystemName, "/", template);
+				var viewResult = viewEngine.FindView(viewContext, viewName, false);
+				searchedLocations.AddRange(viewResult.SearchedLocations);
+				if (viewResult.View != null)
+				{
+					return viewResult;
+				}
+			}
+
+			return null;
 		}
 	}
 }
