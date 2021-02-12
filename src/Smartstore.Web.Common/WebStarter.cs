b7ae0b6b8ea7c9946e5f127098f4809d7e262f5b
Michael Herzog <herzog@smartstore.com>
2021-02-12 18:17:24 +0100

    Integrated menu taghelper into frontend (note that test route in footer is working)

--- src/Smartstore.Web.Common/WebStarter.cs
+++ src/Smartstore.Web.Common/WebStarter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Smartstore.Core;
@@ -6,6 +6,7 @@ using Smartstore.Core.Content.Seo.Routing;
 using Smartstore.Engine;
 using Smartstore.Engine.Builders;
 using Smartstore.Web.Rendering;
+using Smartstore.Web.TagHelpers;
 
 namespace Smartstore.Web
 {
@@ -16,7 +17,47 @@ namespace Smartstore.Web
             services.AddScoped<IWorkContext, WebWorkContext>();
             services.AddScoped<IPageAssetBuilder, PageAssetBuilder>();
             services.AddSingleton<IIconExplorer, IconExplorer>();
+            services.AddScoped<IMenuPublisher, MenuPublisher>();
             services.AddScoped<SlugRouteTransformer>();
+        }
+
+        public override void ConfigureContainer(ContainerBuilder builder, IApplicationContext appContext, bool isActiveModule)
+        {
+            builder.RegisterType<MenuService>().As<IMenuService>().InstancePerLifetimeScope();
+
+            var menuResolverTypes = appContext.TypeScanner.FindTypes<IMenuResolver>(ignoreInactiveModules: true);
+            foreach (var type in menuResolverTypes)
+            {
+                builder.RegisterType(type).As<IMenuResolver>().PropertiesAutowired(PropertyWiringOptions.None).InstancePerLifetimeScope();
+            }
+
+            builder.RegisterType<DatabaseMenu>().Named<IMenu>("database").InstancePerDependency();
+
+            var menuTypes = appContext.TypeScanner.FindTypes<IMenu>(ignoreInactiveModules: true);
+            foreach (var type in menuTypes)
+            {
+                builder.RegisterType(type).As<IMenu>().PropertiesAutowired(PropertyWiringOptions.None).InstancePerLifetimeScope();
+            }
+
+            var menuItemProviderTypes = appContext.TypeScanner.FindTypes<IMenuItemProvider>(ignoreInactiveModules: true);
+            foreach (var type in menuItemProviderTypes)
+            {
+                var attribute = type.GetAttribute<MenuItemProviderAttribute>(false);
+                var registration = builder.RegisterType(type).As<IMenuItemProvider>().PropertiesAutowired(PropertyWiringOptions.None).InstancePerLifetimeScope();
+                registration.WithMetadata<MenuItemProviderMetadata>(m =>
+                {
+                    m.For(em => em.ProviderName, attribute.ProviderName);
+                    m.For(em => em.AppendsMultipleItems, attribute.AppendsMultipleItems);
+                });
+            }
+
+            // TODO: (mh) (core) Uncomment when MenuActionFilter & MenuResultFilter are available.
+            //if (DataSettings.DatabaseIsInstalled())
+            //{
+            //    // We have to register two classes, otherwise the filters would be called twice.
+            //    builder.RegisterType<MenuActionFilter>().AsActionFilterFor<SmartController>(0);
+            //    builder.RegisterType<MenuResultFilter>().AsResultFilterFor<SmartController>(0);
+            //}
         }
 
         public override void MapRoutes(EndpointRoutingBuilder builder)
@@ -44,6 +85,10 @@ namespace Smartstore.Web
             {
                 // Register routes from SlugRouteTransformer solely needed for URL creation, NOT for route matching.
                 SlugRouteTransformer.Routers.Each(x => x.MapRoutes(routes));
+
+                // INFO: Test route
+                // TODO: (mh) (core) Remove test route when not needed anymore.
+                routes.MapLocalizedControllerRoute("RecentlyAddedProducts", "newproducts/", new { controller = "Catalog", action = "RecentlyAddedProducts" });
 
                 // TODO: (core) Very last route: PageNotFound?
                 routes.MapControllerRoute("PageNotFound", "{*path}", new { controller = "Error", action = "NotFound" });
