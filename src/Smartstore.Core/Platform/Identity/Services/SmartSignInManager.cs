12db561f4d6cfe4f1181e9fbc100be4287030371
Murat Cakir <muratc@smartstore.de>
2021-02-12 03:49:18 +0100

    SmartSignInManager

---
+++ src/Smartstore.Core/Platform/Identity/Services/SmartSignInManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Smartstore.Core.Data;
+using Smartstore.Core.Identity;
+
+namespace Smartstore.Core.Identity
+{
+    public class SmartSignInManager : SignInManager<Customer>
+    {
+        private readonly SmartDbContext _db;
+        private readonly CustomerSettings _customerSettings;
+
+        public SmartSignInManager(UserManager<Customer> userManager,
+            IHttpContextAccessor contextAccessor,
+            IUserClaimsPrincipalFactory<Customer> claimsFactory,
+            IOptions<IdentityOptions> optionsAccessor,
+            ILogger<SignInManager<Customer>> logger,
+            IAuthenticationSchemeProvider schemes,
+            IUserConfirmation<Customer> confirmation,
+            SmartDbContext db,
+            CustomerSettings customerSettings)
+            : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
+        {
+            _db = db;
+            _customerSettings = customerSettings;
+        }
+        
+        /// <inheritdoc />
+        public override async Task<SignInResult> PasswordSignInAsync(string userNameOrEmail, string password, bool isPersistent, bool lockoutOnFailure)
+        {
+            Customer user;
+
+            if (_customerSettings.CustomerLoginType == CustomerLoginType.Email)
+            {
+                user = await UserManager.FindByEmailAsync(userNameOrEmail);
+            }
+            else if (_customerSettings.CustomerLoginType == CustomerLoginType.Username)
+            {
+                user = await UserManager.FindByNameAsync(userNameOrEmail);
+            }
+            else
+            {
+                user = await UserManager.FindByEmailAsync(userNameOrEmail) ?? await UserManager.FindByNameAsync(userNameOrEmail);
+            }
+
+            if (user == null)
+            {
+                return SignInResult.Failed;
+            }
+
+            return await PasswordSignInAsync(user, password, isPersistent, lockoutOnFailure);
+        }
+
+        /// <inheritdoc />
+        public override async Task<SignInResult> PasswordSignInAsync(Customer user, string password, bool isPersistent, bool lockoutOnFailure)
+        {
+            if (user == null || user.Deleted)
+            {
+                return SignInResult.Failed;
+            }
+
+            if (!user.Active)
+            {
+                return SignInResult.NotAllowed;
+            }
+
+            if (!user.IsRegistered())
+            {
+                return SignInResult.NotAllowed;
+            }
+
+            var result = await base.PasswordSignInAsync(user, password, isPersistent, lockoutOnFailure);
+
+            if (result.Succeeded)
+            {
+                user.LastLoginDateUtc = DateTime.UtcNow;
+                _db.TryUpdate(user);
+                await _db.SaveChangesAsync();
+            }
+
+            return result;
+        }
+    }
+}
