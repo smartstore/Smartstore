739ad054b5885abc3997c540c11b0849881fdd93
Marcus  Gesing <gesing@smartstore.com>
2021-02-15 16:06:41 +0100

    ProductRuleProvider (in progress)

---
+++ src/Smartstore.Core/Catalog/Rules/IProductRuleProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Smartstore.Core.Catalog.Search;
+
+namespace Smartstore.Core.Catalog.Rules
+{
+    /// <summary>
+    /// Interface for a product rule provider.
+    /// </summary>
+    public partial interface IProductRuleProvider
+    {
+        /// <summary>
+        /// Creates a rule expression group.
+        /// </summary>
+        /// <param name="ruleSetId">Rule set identifier.</param>
+        /// <returns>Expression group.</returns>
+        Task<SearchFilterExpressionGroup> CreateExpressionGroupAsync(int ruleSetId);
+
+        /// <summary>
+        /// Searches for products that match the filter expressions.
+        /// </summary>
+        /// <param name="filters">Filter expressions.</param>
+        /// <param name="pageIndex">Page index.</param>
+        /// <param name="pageSize">Page size.</param>
+        /// <returns>Catalog search result.</returns>
+        Task<CatalogSearchResult> SearchAsync(SearchFilterExpression[] filters, int pageIndex = 0, int pageSize = int.MaxValue);
+    }
+}
