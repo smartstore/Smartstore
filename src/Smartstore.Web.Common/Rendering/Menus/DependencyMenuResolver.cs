d9f6f2572c76b97cf5259f61685df728844fb978
Michael Herzog <herzog@smartstore.com>
2021-02-16 15:39:35 +0100

    More work on menus

---
+++ src/Smartstore.Web.Common/Rendering/Menus/DependencyMenuResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Smartstore.Web.Rendering.Menus
+{
+    public class DependencyMenuResolver : IMenuResolver
+    {
+        private readonly IDictionary<string, IMenu> _menus;
+
+        public DependencyMenuResolver(IEnumerable<IMenu> menus)
+        {
+            _menus = menus.ToDictionarySafe(x => x.Name, x => x, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public int Order => 0;
+
+        public Task<bool> ExistsAsync(string menuName)
+        {
+            Guard.NotEmpty(menuName, nameof(menuName));
+
+            return Task.FromResult(_menus.ContainsKey(menuName));
+        }
+
+        public IMenu Resolve(string menuName)
+        {
+            _menus.TryGetValue(menuName, out var menu);
+            return menu;
+        }
+    }
+}
