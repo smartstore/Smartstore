88f3ee79e17ea8db72e301b1d8bfaec9615c614a
Murat Cakir <muratc@smartstore.de>
2021-02-17 03:09:49 +0100

    RulesStarter --> RuleStarter

--- src/Smartstore.Core/Platform/Rules/Bootstrapping/RuleStarter.cs
+++ src/Smartstore.Core/Platform/Rules/Bootstrapping/RuleStarter.cs
@@ -7,7 +7,7 @@ using Smartstore.Engine.Builders;
 
 namespace Smartstore.Core.Bootstrapping
 {
-    public class RulesStarter : StarterBase
+    public class RuleStarter : StarterBase
     {
         public override void ConfigureContainer(ContainerBuilder builder, IApplicationContext appContext, bool isActiveModule)
         {
