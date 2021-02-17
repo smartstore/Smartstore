e01fa1ad3c8a41d8f5369d30c7faa7846ba84626
Michael Herzog <herzog@smartstore.com>
2021-02-17 20:02:46 +0100

    Attributes Taghelper

---
+++ src/Smartstore.Web.Common/TagHelpers/Shared/AttributesTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Smartstore.Web.TagHelpers.Shared
+{
+    [HtmlTargetElement("*", Attributes = AttributesName)]
+    public class AttributesTagHelper : TagHelper
+    {
+        const string AttributesName = "attributes";
+
+        /// <summary>
+        /// A <see cref="AttributeDictionary"/> instance.
+        /// </summary>
+        [HtmlAttributeName(AttributesName)]
+        public AttributeDictionary Attributes { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            foreach (var attr in Attributes)
+            {
+                output.MergeAttribute(attr.Key, attr.Value, false);
+            }
+        }
+    }
+}
