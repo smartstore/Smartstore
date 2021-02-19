e424ed343cfca2feeb54638f2154e585281d8c21
Murat Cakir <muratc@smartstore.de>
2021-02-19 23:45:08 +0100

    AttributesTagHelper: - Renamed attribute name to sm-attrs - sm-attr-*="(bool Condition, string Value)" > replaces Html.Attr(). E.g.: sm-attr-class='(Model.HasClass, "some-css-class")'

--- src/Smartstore.Web.Common/TagHelpers/Shared/AttributesTagHelper.cs
+++ src/Smartstore.Web.Common/TagHelpers/Shared/AttributesTagHelper.cs
@@ -1,24 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Smartstore.Web.TagHelpers.Shared
 {
     [HtmlTargetElement("*", Attributes = AttributesName)]
+    [HtmlTargetElement("*", Attributes = ConditionalAttributePrefix + "*")]
     public class AttributesTagHelper : TagHelper
     {
-        const string AttributesName = "attributes";
+        const string AttributesName = "sm-attrs";
+        const string ConditionalAttributePrefix = "sm-attr-";
+
+        private IDictionary<string, (bool Condition, string Value)> _conditionalAttributes;
 
         /// <summary>
-        /// A <see cref="AttributeDictionary"/> instance.
+        /// An <see cref="AttributeDictionary"/> instance whose content should be merged with local attributes.
         /// </summary>
         [HtmlAttributeName(AttributesName)]
         public AttributeDictionary Attributes { get; set; }
 
+        /// <summary>
+        /// Additional conditional attributes.
+        /// </summary>
+        [HtmlAttributeName("sm-all-conditional-attrs", DictionaryAttributePrefix = ConditionalAttributePrefix)]
+        public IDictionary<string, (bool Condition, string Value)> ConditionalAttributes
+        {
+            get
+            {
+                return _conditionalAttributes ??= new Dictionary<string, (bool, string)>(StringComparer.OrdinalIgnoreCase);
+            }
+            set
+            {
+                _conditionalAttributes = value;
+            }
+        }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            foreach (var attr in Attributes)
+            if (Attributes != null)
             {
-                output.MergeAttribute(attr.Key, attr.Value, false);
+                foreach (var attr in Attributes)
+                {
+                    output.MergeAttribute(attr.Key, attr.Value, false);
+                }
+            }
+
+            if (_conditionalAttributes != null && _conditionalAttributes.Count > 0)
+            {
+                foreach (var kvp in _conditionalAttributes)
+                {
+                    if (kvp.Value.Condition)
+                    {
+                        output.MergeAttribute(kvp.Key, kvp.Value.Value, false);
+                    }
+                }
             }
         }
     }
