4b932de1d0bfd9e6125b67fe0f568c6e37992a78
Marcus  Gesing <gesing@smartstore.com>
2021-02-19 09:39:15 +0100

    InstallPermissionsInitializer

---
+++ src/Smartstore.Core/Platform/Security/Bootstrapping/InstallPermissionsInitializer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Smartstore.Core.Data;
+using Smartstore.Core.Security;
+using Smartstore.Engine;
+using Smartstore.Engine.Initialization;
+
+namespace Smartstore.Core.Bootstrapping
+{
+    public class InstallPermissionsInitializer : IApplicationInitializer
+    {
+        private readonly SmartDbContext _db;
+        private readonly IPermissionService _permissionService;
+        private readonly ITypeScanner _typeScanner;
+
+        public InstallPermissionsInitializer(SmartDbContext db, IPermissionService permissionService, ITypeScanner typeScanner)
+        {
+            _db = db;
+            _permissionService = permissionService;
+            _typeScanner = typeScanner;
+        }
+
+        public ILogger Logger { get; set; } = NullLogger.Instance;
+
+        public int Order => 0;
+        public bool ThrowOnError => true;
+
+        public async Task InitializeAsync()
+        {
+            var removeUnusedPermissions = true;
+            var providers = new List<IPermissionProvider>();
+
+            // TODO: (core) PluginManager.PluginChangeDetected and GetAppliedMigrations().Any() are still required in InstallPermissionsInitializer?
+            if (/*PluginManager.PluginChangeDetected || DbMigrationContext.Current.GetAppliedMigrations().Any() ||*/ !await _db.PermissionRecords.AnyAsync())
+            {
+                // INFO: even if no module has changed: directly after a DB migration this code block MUST run. It seems awkward
+                // that pending migrations exist when binaries has not changed. But after a manual DB reset for a migration rerun
+                // nobody touches the binaries usually.
+
+                // Core permission provider and all module providers.
+                var types = _typeScanner.FindTypes<IPermissionProvider>(ignoreInactiveModules: true).ToList();
+                foreach (var type in types)
+                {
+                    if (Activator.CreateInstance(type) is IPermissionProvider provider)
+                    {
+                        providers.Add(provider);
+                    }
+                    else
+                    {
+                        removeUnusedPermissions = false;
+                        Logger.Warn($"Cannot create instance of IPermissionProvider {type.Name.NaIfEmpty()}.");
+                    }
+                }
+            }
+            else
+            {
+                // Always check core permission provider.
+                providers.Add(new StandardPermissionProvider());
+
+                // Keep unused permissions in database (has no negative effects) as long as at least one module changed.
+                removeUnusedPermissions = false;
+            }
+
+            await _permissionService.InstallPermissionsAsync(providers.ToArray(), removeUnusedPermissions);
+        }
+
+        public void OnFail(Exception exception)
+        {
+        }
+    }
+}
---
+++ src/Smartstore.Core/Platform/Security/Services/StandardPermissionProvider.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Smartstore.Core.Identity;
+
+namespace Smartstore.Core.Security
+{
+    /// <summary>
+    /// Provider to get permissions of the core module.
+    /// </summary>
+    public partial class StandardPermissionProvider : IPermissionProvider
+    {
+        public virtual IEnumerable<PermissionRecord> GetPermissions()
+        {
+            var permissionSystemNames = PermissionHelper.GetPermissions(typeof(Permissions));
+            var permissions = permissionSystemNames.Select(x => new PermissionRecord { SystemName = x });
+
+            return permissions;
+        }
+
+        public virtual IEnumerable<DefaultPermissionRecord> GetDefaultPermissions()
+        {
+            return new[]
+            {
+                new DefaultPermissionRecord
+                {
+                    CustomerRoleSystemName = SystemCustomerRoleNames.Administrators,
+                    PermissionRecords = new[]
+                    {
+                        new PermissionRecord { SystemName = Permissions.Catalog.Self },
+                        new PermissionRecord { SystemName = Permissions.Customer.Self },
+                        new PermissionRecord { SystemName = Permissions.Order.Self },
+                        new PermissionRecord { SystemName = Permissions.Promotion.Self },
+                        new PermissionRecord { SystemName = Permissions.Cms.Self },
+                        new PermissionRecord { SystemName = Permissions.Configuration.Self },
+                        new PermissionRecord { SystemName = Permissions.System.Self },
+                        new PermissionRecord { SystemName = Permissions.Cart.Self },
+                        new PermissionRecord { SystemName = Permissions.Media.Self }
+                    }
+                },
+                new DefaultPermissionRecord
+                {
+                    CustomerRoleSystemName = SystemCustomerRoleNames.ForumModerators,
+                    PermissionRecords = new[]
+                    {
+                        new PermissionRecord { SystemName = Permissions.Catalog.DisplayPrice },
+                        new PermissionRecord { SystemName = Permissions.Cart.AccessShoppingCart },
+                        new PermissionRecord { SystemName = Permissions.Cart.AccessWishlist },
+                        new PermissionRecord { SystemName = Permissions.System.AccessShop }
+                    }
+                },
+                new DefaultPermissionRecord
+                {
+                    CustomerRoleSystemName = SystemCustomerRoleNames.Guests,
+                    PermissionRecords = new[]
+                    {
+                        new PermissionRecord { SystemName = Permissions.Catalog.DisplayPrice },
+                        new PermissionRecord { SystemName = Permissions.Cart.AccessShoppingCart },
+                        new PermissionRecord { SystemName = Permissions.Cart.AccessWishlist },
+                        new PermissionRecord { SystemName = Permissions.System.AccessShop }
+                    }
+                },
+                new DefaultPermissionRecord
+                {
+                    CustomerRoleSystemName = SystemCustomerRoleNames.Registered,
+                    PermissionRecords = new[]
+                    {
+                        new PermissionRecord { SystemName = Permissions.Catalog.DisplayPrice },
+                        new PermissionRecord { SystemName = Permissions.Cart.AccessShoppingCart },
+                        new PermissionRecord { SystemName = Permissions.Cart.AccessWishlist },
+                        new PermissionRecord { SystemName = Permissions.System.AccessShop }
+                    }
+                }
+            };
+        }
+    }
+}
