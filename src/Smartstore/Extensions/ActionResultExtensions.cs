1d29b440ffc1b59ad080fafc1fee44e2f280ad14
Michael Herzog <herzog@smartstore.com>
2021-02-19 17:20:12 +0100

    Ammendment for menu filter

---
+++ src/Smartstore/Extensions/ActionResultExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Smartstore
+{
+    public static class ActionResultExtensions
+    {
+        public static bool IsHtmlViewResult(this IActionResult result)
+        {
+            var contentResult = result as ContentResult;
+            if (contentResult != null)
+            {
+                return contentResult.ContentType.EqualsNoCase("text/html");
+            }
+
+            return false;
+        }
+    }
+}
