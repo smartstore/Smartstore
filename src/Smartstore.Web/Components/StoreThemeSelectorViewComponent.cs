b044b1eae8c2fc93afc25766fd3d87d6c01c842e
Michael Herzog <herzog@smartstore.com>
2021-03-11 14:46:16 +0100

    StoreThemeSelector

---
+++ src/Smartstore.Web/Components/StoreThemeSelectorViewComponent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Theming;
+using Smartstore.Web.Models.Common;
+using Smartstore.Web.Theming;
+
+namespace Smartstore.Web.Components
+{
+    public class StoreThemeSelectorViewComponent : SmartViewComponent
+    {
+        private readonly Lazy<IThemeRegistry> _themeRegistry;
+        private readonly ThemeSettings _themeSettings;
+        private readonly IThemeContext _themeContext;
+
+        public StoreThemeSelectorViewComponent(Lazy<IThemeRegistry> themeRegistry, ThemeSettings themeSettings, IThemeContext themeContext)
+        {
+            _themeRegistry = themeRegistry;
+            _themeSettings = themeSettings;
+            _themeContext = themeContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            if (!_themeSettings.AllowCustomerToSelectTheme)
+                return Empty();
+
+            var currentTheme = _themeRegistry.Value.GetThemeManifest(_themeContext.WorkingThemeName);
+
+            ViewBag.CurrentStoreTheme = new StoreThemeModel()
+            {
+                Name = currentTheme.ThemeName,
+                Title = currentTheme.ThemeTitle
+            };
+
+            ViewBag.AvailableStoreThemes = _themeRegistry.Value
+                .GetThemeManifests()
+                .Select(x =>
+                {
+                    return new StoreThemeModel
+                    {
+                        Name = x.ThemeName,
+                        Title = x.ThemeTitle
+                    };
+                })
+                .ToList();
+
+            return View();
+        }
+    }
+}
---
+++ src/Smartstore.Web/Controllers/CommonController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Theming;
+using Smartstore.Web.Theming;
+
+namespace Smartstore.Web.Controllers
+{
+    public class CommonController : SmartController
+    {
+        private readonly IThemeContext _themeContext;
+        private readonly Lazy<IThemeRegistry> _themeRegistry;
+        private readonly ThemeSettings _themeSettings;
+
+        public CommonController(IThemeContext themeContext, Lazy<IThemeRegistry> themeRegistry, ThemeSettings themeSettings)
+        {
+            _themeContext = themeContext;
+            _themeRegistry = themeRegistry;
+            _themeSettings = themeSettings;
+        }
+
+        [HttpPost]
+        public IActionResult ChangeTheme(string themeName, string returnUrl = null)
+        {
+            if (!_themeSettings.AllowCustomerToSelectTheme || (themeName.HasValue() && !_themeRegistry.Value.ThemeManifestExists(themeName)))
+            {
+                return NotFound();
+            }
+
+            _themeContext.WorkingThemeName = themeName;
+
+            if (HttpContext.Request.IsAjaxRequest())
+            {
+                return Json(new { Success = true });
+            }
+
+            return RedirectToReferrer(returnUrl);
+        }
+    }
+}
