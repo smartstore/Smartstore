9dbf8fc3d5a2209159520f9458fec8ba474611f4
Michael Herzog <herzog@smartstore.com>
2021-03-12 18:38:19 +0100

    Topbar

---
+++ src/Smartstore.Web/Components/TopBarViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Content.Menus;
+using Smartstore.Core.Domain.Catalog;
+using Smartstore.Core.Identity;
+using Smartstore.Core.Security;
+using Smartstore.Web.Models.Common;
+
+namespace Smartstore.Web.Components
+{
+    public class TopBarViewComponent : SmartViewComponent
+    {
+        private readonly CatalogSettings _catalogSettings;
+        private readonly CustomerSettings _customerSettings;
+
+        public TopBarViewComponent(CatalogSettings catalogSettings, CustomerSettings customerSettings)
+        {
+            _catalogSettings = catalogSettings;
+            _customerSettings = customerSettings;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var customer = Services.WorkContext.CurrentCustomer;
+            
+            var model = new TopBarModel
+            {
+                RecentlyAddedProductsEnabled = _catalogSettings.RecentlyAddedProductsEnabled,
+                CustomerEmailUsername = customer.IsRegistered() ? (_customerSettings.CustomerLoginType != CustomerLoginType.Email ? customer.Username : customer.Email) : "",
+                IsCustomerImpersonated = Services.WorkContext.CurrentImpersonator != null,
+                IsAuthenticated = customer.IsRegistered(),
+                DisplayAdminLink = Services.Permissions.Authorize(Permissions.System.AccessBackend),
+                HasContactUsPage = (await Url.TopicAsync("ContactUs")).ToString().HasValue(),
+                DisplayLoginLink = _customerSettings.UserRegistrationType != UserRegistrationType.Disabled
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Smartstore.Web/Models/Common/TopBarModel.cs
@@ -0,0 +1,15 @@
+﻿using Smartstore.Web.Modelling;
+
+namespace Smartstore.Web.Models.Common
+{
+    public partial class TopBarModel : ModelBase
+    {
+        public bool RecentlyAddedProductsEnabled { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public bool DisplayLoginLink { get; set; }
+        public bool DisplayAdminLink { get; set; }
+        public bool IsCustomerImpersonated { get; set; }
+        public string CustomerEmailUsername { get; set; }
+        public bool HasContactUsPage { get; set; }
+    }
+}
