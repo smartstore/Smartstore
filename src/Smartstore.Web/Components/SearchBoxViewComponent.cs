144b249c796e216d0a23f7aee6c1bed70ccb09bb
Michael Herzog <herzog@smartstore.com>
2021-03-12 17:55:59 +0100

    SearchBox

---
+++ src/Smartstore.Web/Components/SearchBoxViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Catalog.Search;
+using Smartstore.Core.Catalog.Search.Modelling;
+using Smartstore.Core.Security;
+using Smartstore.Web.Models.Search;
+
+namespace Smartstore.Web.Components
+{
+    public class SearchBoxViewComponent : SmartViewComponent
+    {
+        private readonly ICatalogSearchQueryFactory _queryFactory;
+        private readonly SearchSettings _searchSettings;
+
+        public SearchBoxViewComponent(ICatalogSearchQueryFactory queryFactory, SearchSettings searchSettings)
+        {
+            _queryFactory = queryFactory;
+            _searchSettings = searchSettings;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = new SearchBoxModel
+            {
+                Origin = "Search/Search",
+                InstantSearchUrl = Url.RouteUrl("InstantSearch"),
+                InputPlaceholder = T("Search.SearchBox.Tooltip"),
+                InstantSearchEnabled = _searchSettings.InstantSearchEnabled && Services.Permissions.Authorize(Permissions.System.AccessShop),
+                ShowThumbsInInstantSearch = _searchSettings.ShowProductImagesInInstantSearch,
+                SearchTermMinimumLength = _searchSettings.InstantSearchTermMinLength,
+                CurrentQuery = _queryFactory.Current?.Term
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Smartstore.Web/Models/Search/SearchBoxModel.cs
@@ -0,0 +1,15 @@
+﻿using Smartstore.Web.Modelling;
+
+namespace Smartstore.Web.Models.Search
+{
+    public partial class SearchBoxModel : ModelBase
+    {
+        public string Origin { get; set; }
+        public string InputPlaceholder { get; set; }
+        public int SearchTermMinimumLength { get; set; }
+        public string InstantSearchUrl { get; set; }
+        public bool InstantSearchEnabled { get; set; }
+        public bool ShowThumbsInInstantSearch { get; set; }
+        public string CurrentQuery { get; set; }
+    }
+}
