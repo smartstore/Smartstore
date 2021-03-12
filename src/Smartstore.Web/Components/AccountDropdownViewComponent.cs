94fa62d08b8aa5d71a723944f140c0cfd7373769
Michael Herzog <herzog@smartstore.com>
2021-03-12 18:12:03 +0100

    AccountDropdown

---
+++ src/Smartstore.Web/Components/AccountDropdownViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Security;
+using Smartstore.Web.Models.Common;
+
+namespace Smartstore.Web.Components
+{
+    public class AccountDropdownViewComponent : SmartViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var customer = Services.WorkContext.CurrentCustomer;
+
+            // TODO: (mh) (core) PMs must be prepared in Forum Module and links must be inserted into account_dropdown_after. 
+
+            var model = new AccountDropdownModel
+            {
+                IsAuthenticated = customer.IsRegistered(),
+                DisplayAdminLink = Services.Permissions.Authorize(Permissions.System.AccessBackend),
+                ShoppingCartEnabled = Services.Permissions.Authorize(Permissions.Cart.AccessShoppingCart),
+                WishlistEnabled = Services.Permissions.Authorize(Permissions.Cart.AccessWishlist),
+
+                // TODO: (mh) (core) Why were these commented out?
+                //ShoppingCartItems = customer.CountProductsInCart(ShoppingCartType.ShoppingCart, Services.StoreContext.CurrentStore.Id),
+                //WishlistItems = customer.CountProductsInCart(ShoppingCartType.Wishlist, _serServicesvices.StoreContext.CurrentStore.Id)
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Smartstore.Web/Models/Common/AccountDropdownModel.cs
@@ -0,0 +1,38 @@
+﻿using Smartstore.Web.Modelling;
+
+namespace Smartstore.Web.Models.Common
+{
+    public partial class AccountDropdownModel : EntityModelBase
+    {
+        public string Name { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public bool DisplayAdminLink { get; set; }
+        public bool ShoppingCartEnabled { get; set; }
+        public int ShoppingCartItems { get; set; }
+        public bool WishlistEnabled { get; set; }
+        public int WishlistItems { get; set; }
+
+        // TODO: (mh) (core) Remove obsolete properties.
+        // INFO: PMs will be prepared in Forum Module.
+        //public bool AllowPrivateMessages { get; set; }
+        //public string UnreadPrivateMessages { get; set; }
+        //public string AlertMessage { get; set; }
+
+
+        // TODO: (mh) (core) Remove unused properties.
+        //[LocalizedDisplay("Account.Login.Fields.Email")]
+        //public string Email { get; set; }
+
+        //public bool UsernamesEnabled { get; set; }
+
+        //[LocalizedDisplay("Account.Login.Fields.UserName")]
+        //public string Username { get; set; }
+
+        //[DataType(DataType.Password)]
+        //[LocalizedDisplay("Account.Login.Fields.Password")]
+        //public string Password { get; set; }
+
+        //[LocalizedDisplay("Account.Login.Fields.RememberMe")]
+        //public bool RememberMe { get; set; }
+    }
+}
