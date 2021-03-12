0c64d7671879ca73b523d34522bdedf7c743bfbd
Michael Herzog <herzog@smartstore.com>
2021-03-12 17:40:03 +0100

    ShopLogo (WIP: caching is missing)

---
+++ src/Smartstore.Web/Components/ShopLogoViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Content.Media;
+using Smartstore.Web.Models.Common;
+
+namespace Smartstore.Web.Components
+{
+    public class ShopLogoViewComponent : SmartViewComponent
+    {
+        private readonly Lazy<IMediaService> _mediaService;
+
+        public ShopLogoViewComponent(Lazy<IMediaService> mediaService)
+        {
+            _mediaService = mediaService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // TODO: (mh) (core) caching.
+
+            var store = Services.StoreContext.CurrentStore;
+            var logo = await _mediaService.Value.GetFileByIdAsync(store.LogoMediaFileId, MediaLoadFlags.AsNoTracking);
+
+            var model = new ShopLogoModel
+            {
+                LogoUploaded = logo != null,
+                LogoTitle = store.Name
+            };
+
+            if (logo != null)
+            {
+                model.LogoUrl = await _mediaService.Value.GetUrlAsync(logo.Id, 0, null, false);
+                model.LogoWidth = logo.Size.Width;
+                model.LogoHeight = logo.Size.Height;
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Smartstore.Web/Models/Common/ShopLogoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Smartstore.Web.Models.Common
+{
+    public class ShopLogoModel
+    {
+        public bool LogoUploaded { get; set; }
+        public string LogoUrl { get; set; }
+        public int LogoWidth { get; set; }
+        public int LogoHeight { get; set; }
+        public string LogoTitle { get; set; }
+    }
+}
