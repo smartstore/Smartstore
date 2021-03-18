9bf62f9b0bcd0a8e0cf2d3c8babe4d9920153cc2
Michael Herzog <herzog@smartstore.com>
2021-03-18 13:30:13 +0100

    Minor fix for MetaProperties & cleanup

--- src/Smartstore.Web/Models/Common/AccountDropdownModel.cs
+++ src/Smartstore.Web/Models/Common/AccountDropdownModel.cs
@@ -15,28 +15,5 @@ namespace Smartstore.Web.Models.Common
         public int WishlistItems { get; set; }
 
         public List<MenuItem> MenuItems { get; } = new();
-
-        // TODO: (mh) (core) Remove obsolete properties.
-        // INFO: PMs will be prepared in Forum Module.
-        //public bool AllowPrivateMessages { get; set; }
-        //public string UnreadPrivateMessages { get; set; }
-        //public string AlertMessage { get; set; }
-
-
-        // TODO: (mh) (core) Remove unused properties.
-        //[LocalizedDisplay("Account.Login.Fields.Email")]
-        //public string Email { get; set; }
-
-        //public bool UsernamesEnabled { get; set; }
-
-        //[LocalizedDisplay("Account.Login.Fields.UserName")]
-        //public string Username { get; set; }
-
-        //[DataType(DataType.Password)]
-        //[LocalizedDisplay("Account.Login.Fields.Password")]
-        //public string Password { get; set; }
-
-        //[LocalizedDisplay("Account.Login.Fields.RememberMe")]
-        //public bool RememberMe { get; set; }
     }
 }
