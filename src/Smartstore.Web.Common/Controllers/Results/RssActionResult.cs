286100b39168e871926b53c6a1a4bbe65110480f
Michael Herzog <herzog@smartstore.com>
2021-03-24 13:44:50 +0100

    Made RssActionResult async (former result returned error  'call writeasync or set allowsynchronousio to true')

--- src/Smartstore.Web.Common/Controllers/Results/RssActionResult.cs
+++ src/Smartstore.Web.Common/Controllers/Results/RssActionResult.cs
@@ -1,5 +1,8 @@
-﻿using System.ServiceModel.Syndication;
+﻿using System.IO;
+using System.ServiceModel.Syndication;
+using System.Threading.Tasks;
 using System.Xml;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Smartstore.Web.Controllers
@@ -8,17 +11,23 @@ namespace Smartstore.Web.Controllers
     {
         public SyndicationFeed Feed { get; set; }
 
-        public override void ExecuteResult(ActionContext context)
+        public override async Task ExecuteResultAsync(ActionContext context)
         {
             context.HttpContext.Response.ContentType = "application/rss+xml";
 
             var rssFormatter = new Rss20FeedFormatter(Feed, false);
-            var settings = new XmlWriterSettings { Indent = true, IndentChars = "\t", CheckCharacters = false };
-
-            using (var writer = XmlWriter.Create(context.HttpContext.Response.Body, settings))
+            var settings = new XmlWriterSettings { Indent = true, IndentChars = "\t", CheckCharacters = false, Async = true };
+            
+            string content;
+            using (StringWriter stringWriter = new StringWriter())
+            using (var writer = XmlWriter.Create(stringWriter, settings))
             {
                 rssFormatter.WriteTo(writer);
+                await writer.FlushAsync();
+                content = stringWriter.ToString();
             }
+
+            await context.HttpContext.Response.WriteAsync(content);
         }
     }
 }
