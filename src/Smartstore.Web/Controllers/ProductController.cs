117fb9482fdc9c4d046e485e217c41897f2356e5
Michael Herzog <herzog@smartstore.com>
2021-03-24 20:00:22 +0100

    UpdateProductDetails

--- src/Smartstore.Web/Controllers/ProductController.cs
+++ src/Smartstore.Web/Controllers/ProductController.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using Smartstore.Core.Catalog;
 using Smartstore.Core.Catalog.Attributes;
 using Smartstore.Core.Catalog.Products;
@@ -10,9 +16,12 @@ using Smartstore.Core.Common.Settings;
 using Smartstore.Core.Content.Media;
 using Smartstore.Core.Content.Menus;
 using Smartstore.Core.Data;
+using Smartstore.Core.Localization;
 using Smartstore.Core.Security;
 using Smartstore.Core.Seo;
 using Smartstore.Core.Stores;
+using Smartstore.Web.Models.Catalog;
+using Smartstore.Web.Razor;
 
 namespace Smartstore.Web.Controllers
 {
@@ -21,10 +30,12 @@ namespace Smartstore.Web.Controllers
         private readonly SmartDbContext _db;
         private readonly IProductService _productService;
         private readonly IProductTagService _productTagService;
+        private readonly IProductAttributeService _productAttributeService;
         private readonly IRecentlyViewedProductsService _recentlyViewedProductsService;
         private readonly IAclService _aclService;
         private readonly IStoreMappingService _storeMappingService;
         private readonly ICatalogSearchService _catalogSearchService;
+        private readonly IMediaService _mediaService;
         private readonly MediaSettings _mediaSettings;
         private readonly CatalogSettings _catalogSettings;
         private readonly IProductCompareService _productCompareService;
@@ -33,32 +44,38 @@ namespace Smartstore.Web.Controllers
         private readonly SeoSettings _seoSettings;
         private readonly ContactDataSettings _contactDataSettings;
         private readonly Lazy<IUrlHelper> _urlHelper;
+        private readonly Lazy<IRazorViewInvoker> _razorViewInvoker;
 
         public ProductController(
             SmartDbContext db,
             IProductService productService,
             IProductTagService productTagService,
+            IProductAttributeService productAttributeService,
             IRecentlyViewedProductsService recentlyViewedProductsService,
             IProductCompareService productCompareService,
             IAclService aclService,
             IStoreMappingService storeMappingService,
             ICatalogSearchService catalogSearchService,
+            IMediaService mediaService,
             MediaSettings mediaSettings,
             CatalogSettings catalogSettings,
             CatalogHelper helper,
             IBreadcrumb breadcrumb,
             SeoSettings seoSettings,
             ContactDataSettings contactDataSettings,
-            Lazy<IUrlHelper> urlHelper)
+            Lazy<IUrlHelper> urlHelper,
+            Lazy<IRazorViewInvoker> razorViewInvoker)
         {
             _db = db;
             _productService = productService;
             _productTagService = productTagService;
+            _productAttributeService = productAttributeService;
             _recentlyViewedProductsService = recentlyViewedProductsService;
             _productCompareService = productCompareService;
             _aclService = aclService;
             _storeMappingService = storeMappingService;
             _catalogSearchService = catalogSearchService;
+            _mediaService = mediaService;
             _mediaSettings = mediaSettings;
             _catalogSettings = catalogSettings;
             _helper = helper;
@@ -66,6 +83,7 @@ namespace Smartstore.Web.Controllers
             _seoSettings = seoSettings;
             _contactDataSettings = contactDataSettings;
             _urlHelper = urlHelper;
+            _razorViewInvoker = razorViewInvoker;
         }
 
         #region Products
@@ -145,6 +163,196 @@ namespace Smartstore.Web.Controllers
 
             return View(model.ProductTemplateViewPath, model);
         }
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateProductDetails(int productId, string itemType, int bundleItemId, ProductVariantQuery query)
+        {
+            var form = HttpContext.Request.Form;
+            int quantity = 1;
+            int galleryStartIndex = -1;
+            string galleryHtml = null;
+            string dynamicThumbUrl = null;
+            var isAssociated = itemType.EqualsNoCase("associateditem");
+            var m = new ProductDetailsModel();
+            var product = await _db.Products.FindByIdAsync(productId);
+            var bItem = await _db.ProductBundleItem.FindByIdAsync(bundleItemId);
+            IList<ProductBundleItemData> bundleItems = null;
+            ProductBundleItemData bundleItem = bItem == null ? null : new ProductBundleItemData(bItem);
+            
+            // Quantity required for tier prices.
+            string quantityKey = form.Keys.FirstOrDefault(k => k.EndsWith("EnteredQuantity"));
+            if (quantityKey.HasValue())
+            {
+                _ = int.TryParse(form[quantityKey], out quantity);
+            }
+
+            if (product.ProductType == ProductType.BundledProduct && product.BundlePerItemPricing)
+            {
+                var bundleItemQuery = await _db.ProductBundleItem
+                    .ApplyBundledProductsFilter(new[] { product.Id })
+                    .Include(x => x.Product)
+                    .Include(x => x.BundleProduct)
+                    .ToListAsync();
+
+                if (bundleItemQuery.Count > 0)
+                {
+                    bundleItems = new List<ProductBundleItemData>();
+                    bundleItemQuery.Each(x => bundleItems.Add(new ProductBundleItemData(x)));
+                }
+
+                if (query.Variants.Count > 0)
+                {
+                    // May add elements to query object if they are preselected by bundle item filter.
+                    foreach (var itemData in bundleItems)
+                    {
+                        await _helper.PrepareProductDetailsPageModelAsync(itemData.Item.Product, query, false, itemData);
+                    }
+                }
+            }
+
+            // Get merged model data.
+            await _helper.PrepareProductDetailModelAsync(m, product, query, isAssociated, bundleItem, bundleItems, quantity);
+
+            if (bundleItem != null)
+            {
+                // Update bundle item thumbnail.
+                if (!bundleItem.Item.HideThumbnail)
+                {
+                    var assignedMediaIds = m.SelectedCombination?.GetAssignedMediaIds() ?? new int[0];
+                    var hasFile = (await _db.MediaFiles.FindByIdAsync(assignedMediaIds[0])) != null;
+                    if (assignedMediaIds.Any() && hasFile)
+                    {
+                        var file = _db.ProductMediaFiles
+                            .ApplyProductFilter(new[] { bundleItem.Item.ProductId }, 1)
+                            .Select(x => x.MediaFile)
+                            .FirstOrDefault();
+
+                        dynamicThumbUrl = _mediaService.GetUrl(file, _mediaSettings.BundledProductPictureSize, null, false);
+                    }
+                }
+            }
+            else if (isAssociated)
+            {
+                // Update associated product thumbnail.
+                var assignedMediaIds = m.SelectedCombination?.GetAssignedMediaIds() ?? new int[0];
+                var hasFile = (await _db.MediaFiles.FindByIdAsync(assignedMediaIds[0])) != null;
+                if (assignedMediaIds.Any() && hasFile)
+                {
+                    var file = _db.ProductMediaFiles
+                        .ApplyProductFilter(new[] { productId }, 1)
+                        .Select(x => x.MediaFile)
+                        .FirstOrDefault();
+
+                    dynamicThumbUrl = _mediaService.GetUrl(file, _mediaSettings.AssociatedProductPictureSize, null, false);
+                }
+            }
+            else if (product.ProductType != ProductType.BundledProduct)
+            {
+                // Update image gallery.
+                var files = await _db.ProductMediaFiles
+                    .ApplyProductFilter(new[] { productId })
+                    .Select(x => _mediaService.ConvertMediaFile(x.MediaFile))
+                    .ToListAsync();
+
+                if (product.HasPreviewPicture && files.Count > 1)
+                {
+                    files.RemoveAt(0);
+                }
+
+                if (files.Count <= _catalogSettings.DisplayAllImagesNumber)
+                {
+                    // All pictures rendered... only index is required.
+                    galleryStartIndex = 0;
+
+                    var assignedMediaIds = m.SelectedCombination?.GetAssignedMediaIds() ?? new int[0];
+                    if (assignedMediaIds.Any())
+                    {
+                        var file = files.FirstOrDefault(p => p.Id == assignedMediaIds[0]);
+                        galleryStartIndex = file == null ? 0 : files.IndexOf(file);
+                    }
+                }
+                else
+                {
+                    var allCombinationPictureIds = await _productAttributeService.GetAttributeCombinationFileIdsAsync(product.Id);
+
+                    var mediaModel = _helper.PrepareProductDetailsMediaGalleryModel(
+                        files,
+                        product.GetLocalized(x => x.Name),
+                        allCombinationPictureIds,
+                        false,
+                        bundleItem,
+                        m.SelectedCombination);
+
+                    galleryStartIndex = mediaModel.GalleryStartIndex;
+                    galleryHtml = (await _razorViewInvoker.Value.InvokeViewAsync("Product.Media", mediaModel)).ToString();
+                }
+
+                m.PriceDisplayStyle = _catalogSettings.PriceDisplayStyle;
+                m.DisplayTextForZeroPrices = _catalogSettings.DisplayTextForZeroPrices;
+            }
+
+            object partials = null;
+
+            if (m.IsBundlePart)
+            {
+                partials = new
+                {
+                    BundleItemPrice = await _razorViewInvoker.Value.InvokeViewAsync("Product.Offer.Price", m),
+                    BundleItemStock = await _razorViewInvoker.Value.InvokeViewAsync("Product.StockInfo", m),
+                    BundleItemVariants = await _razorViewInvoker.Value.InvokeViewAsync("Product.Variants", m.ProductVariantAttributes)
+                };
+            }
+            else
+            {
+                var dataDictAddToCart = new ViewDataDictionary(ViewData);
+                dataDictAddToCart.TemplateInfo.HtmlFieldPrefix = $"addtocart_{m.Id}";
+
+                decimal adjustment = decimal.Zero;
+                decimal taxRate = decimal.Zero;
+
+                // TODO: (mh) (core) Implement when pricing is available.
+                //var finalPriceWithDiscountBase = _taxService.GetProductPrice(product, product.Price, Services.WorkContext.CurrentCustomer, out taxRate);
+
+                //if (!_taxSettings.Value.PricesIncludeTax && Services.WorkContext.TaxDisplayType == TaxDisplayType.IncludingTax)
+                //{
+                //    adjustment = (m.ProductPrice.PriceValue - finalPriceWithDiscountBase) / (taxRate / 100 + 1);
+                //}
+                //else if (_taxSettings.Value.PricesIncludeTax && Services.WorkContext.TaxDisplayType == TaxDisplayType.ExcludingTax)
+                //{
+                //    adjustment = (m.ProductPrice.PriceValue - finalPriceWithDiscountBase) * (taxRate / 100 + 1);
+                //}
+                //else
+                //{
+                //    adjustment = m.ProductPrice.PriceValue - finalPriceWithDiscountBase;
+                //}
+
+                partials = new
+                {
+                    Attrs = await _razorViewInvoker.Value.InvokeViewAsync("Product.Attrs", m),
+                    Price = await _razorViewInvoker.Value.InvokeViewAsync("Product.Offer.Price", m),
+                    Stock = await _razorViewInvoker.Value.InvokeViewAsync("Product.StockInfo", m),
+                    Variants = await _razorViewInvoker.Value.InvokeViewAsync("Product.Variants", m.ProductVariantAttributes),
+
+                    // TODO: (mc) (core) We may need another parameter for this.
+                    //OfferActions = await _razorViewInvoker.Value.InvokeViewAsync("Product.Offer.Actions", m, dataDictAddToCart),
+                    
+                    // TODO: (mh) (core) Implement when Component or Partial is available.
+                    //TierPrices = await _razorViewInvoker.Value.InvokeViewAsync("Product.TierPrices", await _razorViewInvoker.InvokeViewAsync(product, adjustment)),
+                    BundlePrice = product.ProductType == ProductType.BundledProduct ? await _razorViewInvoker.Value.InvokeViewAsync("Product.Bundle.Price", m) : null
+                };
+            }
+
+            object data = new
+            {
+                Partials = partials,
+                DynamicThumblUrl = dynamicThumbUrl,
+                GalleryStartIndex = galleryStartIndex,
+                GalleryHtml = galleryHtml
+            };
+
+            return new JsonResult(new { Data = data });
+        }
+
 
         #endregion
     }
