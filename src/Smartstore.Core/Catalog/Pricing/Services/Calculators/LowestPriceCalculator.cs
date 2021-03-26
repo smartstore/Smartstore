0f038a67f7d099341fc119db98da1ba81c7ac37d
Murat Cakir <muratc@smartstore.de>
2021-03-26 01:15:19 +0100

    LowestPriceCalculator review

--- src/Smartstore.Core/Catalog/Pricing/Services/Calculators/LowestPriceCalculator.cs
+++ src/Smartstore.Core/Catalog/Pricing/Services/Calculators/LowestPriceCalculator.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 namespace Smartstore.Core.Catalog.Pricing.Calculators
 {
     /// <summary>
+    /// TODO: (mg) (core) The decription is too inaccurate. What does lowest possible price mean? Explain please.
     /// Calculates the lowest possible product price.
     /// </summary>
     [CalculatorUsage(CalculatorTargets.Product, CalculatorOrdering.Default + 10)]
@@ -19,6 +20,7 @@ namespace Smartstore.Core.Catalog.Pricing.Calculators
             }
 
             // Process the whole pipeline with maximum quantity to get the minimum tier price applied.
+            // TODO: (core) Shift Quantity from context to options. Options are shallow copied, context not. (MC)
             context.Quantity = int.MaxValue;
             await next(context);
 
