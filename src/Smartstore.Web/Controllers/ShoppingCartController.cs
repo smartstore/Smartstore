c3bb40bf66ff3aa9f87d6a3e9d9978a2b6e6da57
Michael Herzog <herzog@smartstore.com>
2021-03-31 20:31:21 +0200

    Started OffCanvasShoppingCart

--- src/Smartstore.Web/Controllers/ShoppingCartController.cs
+++ src/Smartstore.Web/Controllers/ShoppingCartController.cs
@@ -1,13 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Catalog;
+using Smartstore.Core.Checkout.Cart;
+using Smartstore.Core.Checkout.Orders;
+using Smartstore.Core.Content.Media;
+using Smartstore.Core.Security;
+using Smartstore.Web.Models.ShoppingCart;
 
 namespace Smartstore.Web.Controllers
 {
     public class ShoppingCartController : PublicControllerBase
     {
+        private readonly ShoppingCartSettings _shoppingCartSettings;
+        private readonly CatalogSettings _catalogSettings;
+        private readonly OrderSettings _orderSettings;
+        private readonly MediaSettings _mediaSettings;
+
+        public ShoppingCartController(
+            ShoppingCartSettings shoppingCartSettings,
+            CatalogSettings catalogSettings,
+            OrderSettings orderSettings,
+            MediaSettings mediaSettings)
+        {
+            _shoppingCartSettings = shoppingCartSettings;
+            _catalogSettings = catalogSettings;
+            _orderSettings = orderSettings;
+            _mediaSettings = mediaSettings;
+        }
+
+        public async Task<IActionResult> OffCanvasShoppingCart()
+        {
+            if (!_shoppingCartSettings.MiniShoppingCartEnabled)
+                return Content("");
+
+            if (!await Services.Permissions.AuthorizeAsync(Permissions.Cart.AccessShoppingCart))
+                return Content("");
+
+            var model = await PrepareMiniShoppingCartModel();
+
+            // TODO: (ms) (core) What about SafeSet
+            //HttpContext.Session.Set(CheckoutState.CheckoutStateSessionKey, new CheckoutState());
+
+            return PartialView(model);
+        }
+
+        [NonAction]
+        protected async Task<MiniShoppingCartModel> PrepareMiniShoppingCartModel()
+        {
+            var customer = Services.WorkContext.CurrentCustomer;
+            var storeId = Services.StoreContext.CurrentStore.Id;
+
+            var model = new MiniShoppingCartModel
+            {
+                ShowProductImages = _shoppingCartSettings.ShowProductImagesInMiniShoppingCart,
+                ThumbSize = _mediaSettings.MiniCartThumbPictureSize,
+                CurrentCustomerIsGuest = customer.IsGuest(),
+                AnonymousCheckoutAllowed = _orderSettings.AnonymousCheckoutAllowed,
+                DisplayMoveToWishlistButton = await Services.Permissions.AuthorizeAsync(Permissions.Cart.AccessWishlist),
+                ShowBasePrice = _shoppingCartSettings.ShowBasePrice
+            };
+
+            // TODO: (ms) (core) Finish the job.
+
+            return model;
+        }
+
+
         public IActionResult CartSummary()
         {
             // Stop annoying MiniProfiler report.
---
+++ src/Smartstore.Web/Models/ShoppingCart/MiniShoppingCartModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Smartstore.Core.Catalog.Products;
+using Smartstore.Core.Localization;
+using Smartstore.Web.Modelling;
+using Smartstore.Web.Models.Catalog;
+using Smartstore.Web.Models.Media;
+
+namespace Smartstore.Web.Models.ShoppingCart
+{
+    public partial class MiniShoppingCartModel : ModelBase
+    {
+        public List<ShoppingCartItemModel> Items { get; set; } = new();
+        public int TotalProducts { get; set; }
+        public string SubTotal { get; set; }
+        public bool DisplayCheckoutButton { get; set; }
+        public bool CurrentCustomerIsGuest { get; set; }
+        public bool AnonymousCheckoutAllowed { get; set; }
+        public bool ShowProductImages { get; set; }
+        public int ThumbSize { get; set; }
+        public bool DisplayMoveToWishlistButton { get; set; }
+        public bool ShowBasePrice { get; set; }
+
+        #region Nested Classes
+
+        public partial class ShoppingCartItemModel : EntityModelBase, IQuantityInput
+        {
+            public int ProductId { get; set; }
+
+            public LocalizedValue<string> ProductName { get; set; }
+
+            public LocalizedValue<string> ShortDesc { get; set; }
+
+            public string ProductSeName { get; set; }
+
+            public string ProductUrl { get; set; }
+
+            public int EnteredQuantity { get; set; }
+
+            public LocalizedValue<string> QuantityUnitName { get; set; }
+
+            public List<SelectListItem> AllowedQuantities { get; set; } = new();
+
+            public int MinOrderAmount { get; set; }
+
+            public int MaxOrderAmount { get; set; }
+
+            public int QuantityStep { get; set; }
+
+            public QuantityControlType QuantiyControlType { get; set; }
+
+            public string UnitPrice { get; set; }
+
+            public string BasePriceInfo { get; set; }
+
+            public string AttributeInfo { get; set; }
+
+            public ImageModel Image { get; set; } = new();
+
+            public List<ShoppingCartItemBundleItem> BundleItems { get; set; } = new();
+
+            public DateTime CreatedOnUtc { get; set; }
+
+        }
+
+        public partial class ShoppingCartItemBundleItem : ModelBase
+        {
+            public string PictureUrl { get; set; }
+            public LocalizedValue<string> ProductName { get; set; }
+            public string ProductSeName { get; set; }
+            public string ProductUrl { get; set; }
+        }
+
+        #endregion
+    }
+}
