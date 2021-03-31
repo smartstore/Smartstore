ec078283a43215ac6dde5e169272670bbd3e689c
Michael Herzog <herzog@smartstore.com>
2021-03-31 20:25:10 +0200

    OffCanvasCartViewComponent

---
+++ src/Smartstore.Web/Components/OffCanvasCartViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Catalog;
+using Smartstore.Core.Catalog.Search;
+using Smartstore.Core.Checkout.Cart;
+using Smartstore.Core.Security;
+using Smartstore.Web.Models.ShoppingCart;
+
+namespace Smartstore.Web.Components
+{
+    public class OffCanvasCartViewComponent : SmartViewComponent
+    {
+        private readonly ShoppingCartSettings _shoppingCartSettings;
+        private readonly CatalogSettings _catalogSettings;
+
+        public OffCanvasCartViewComponent(
+            ShoppingCartSettings shoppingCartSettings,
+            CatalogSettings catalogSettings)
+        {
+            _shoppingCartSettings = shoppingCartSettings;
+            _catalogSettings = catalogSettings;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = new OffCanvasCartModel();
+
+            if (await Services.Permissions.AuthorizeAsync(Permissions.System.AccessShop))
+            {
+                model.ShoppingCartEnabled = _shoppingCartSettings.MiniShoppingCartEnabled && await Services.Permissions.AuthorizeAsync(Permissions.Cart.AccessShoppingCart);
+                model.WishlistEnabled = await Services.Permissions.AuthorizeAsync(Permissions.Cart.AccessWishlist);
+                model.CompareProductsEnabled = _catalogSettings.CompareProductsEnabled;
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Smartstore.Web/Models/ShoppingCart/OffCanvasCartModel.cs
@@ -0,0 +1,17 @@
+﻿using Smartstore.Web.Modelling;
+
+namespace Smartstore.Web.Models.ShoppingCart
+{
+    public partial class OffCanvasCartModel : ModelBase
+    {
+        // product counts
+        public int CartItemsCount { get; set; }
+        public int WishlistItemsCount { get; set; }
+        public int CompareItemsCount { get; set; }
+
+        // settings
+        public bool ShoppingCartEnabled { get; set; }
+        public bool WishlistEnabled { get; set; }
+        public bool CompareProductsEnabled { get; set; }
+    }
+}
