4a308d61c28120bf8c400f1dd552f8fcb7bc6676
Michael Herzog <herzog@smartstore.com>
2021-03-31 10:15:46 +0200

    ActiveFilters

---
+++ src/Smartstore.Web/Components/ActiveFiltersViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Catalog.Search;
+
+namespace Smartstore.Web.Components
+{
+    /// <summary>
+    /// Component to render active filters in product lists.
+    /// </summary>
+    public class ActiveFiltersViewComponent : SmartViewComponent
+    {
+        public IViewComponentResult Invoke(CatalogSearchResult model)
+        {
+            Guard.NotNull(model, nameof(model));
+            return View(model);
+        }
+    }
+}
