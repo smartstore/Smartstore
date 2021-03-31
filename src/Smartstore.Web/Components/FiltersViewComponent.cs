bd548989ee87701190bd0b9a4c8521f882795e44
Michael Herzog <herzog@smartstore.com>
2021-03-31 13:58:49 +0200

    Turned Filters into view component

---
+++ src/Smartstore.Web/Components/FiltersViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Catalog.Search;
+
+namespace Smartstore.Web.Components
+{
+    /// <summary>
+    /// Component to render filters for product lists.
+    /// </summary>
+    public class FiltersViewComponent : SmartViewComponent
+    {
+        public IViewComponentResult Invoke(CatalogSearchResult model)
+        {
+            Guard.NotNull(model, nameof(model));
+            return View(model);
+        }
+    }
+}
