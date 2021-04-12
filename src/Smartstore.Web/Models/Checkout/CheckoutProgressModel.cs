0cc920bde6e9c65afc876612e82b84ae9421f343
Marcel Schmidt <marcel.schmidt@smartstore.com>
2021-04-12 23:10:13 +0200

    Checkout Progress Model & View

---
+++ src/Smartstore.Web/Models/Checkout/CheckoutProgressModel.cs
@@ -0,0 +1,19 @@
+﻿using Smartstore.Web.Modelling;
+
+namespace Smartstore.Web.Models.Checkout
+{
+    public partial class CheckoutProgressModel : ModelBase
+    {
+        public CheckoutProgressStep CheckoutProgressStep { get; set; }
+    }
+
+    public enum CheckoutProgressStep
+    {
+        Cart,
+        Address,
+        Shipping,
+        Payment,
+        Confirm,
+        Complete
+    }
+}
