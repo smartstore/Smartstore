8f03204aa3745d5ca4458d5e3e92a93221385a22
Marcel Schmidt <marcel.schmidt@smartstore.com>
2021-04-14 23:09:47 +0200

    OrderSummary & OffCanvasCart ViewComponents

---
+++ src/Smartstore.Web/Components/OrderSummaryViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Checkout.Cart;
+using Smartstore.Core.Identity;
+using Smartstore.Web.Models.ShoppingCart;
+using System.Threading.Tasks;
+
+namespace Smartstore.Web.Components
+{
+    /// <summary>
+    /// Component for rendering order totals.
+    /// </summary>
+    public class OrderSummaryViewComponent : SmartViewComponent
+    {
+        private readonly IShoppingCartService _shoppingCartService;
+
+        public OrderSummaryViewComponent(IShoppingCartService shoppingCartService)
+        {
+            _shoppingCartService = shoppingCartService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(bool? prepareAndDisplayOrderReviewData, Customer customer = null, int? storeId = null)
+        {
+            customer ??= Services.WorkContext.CurrentCustomer;
+            var cart = await _shoppingCartService.GetCartItemsAsync(customer, ShoppingCartType.ShoppingCart, storeId ?? Services.StoreContext.CurrentStore.Id);
+            var model = new ShoppingCartModel();
+
+            await cart.MapAsync(model,
+                isEditable: false,
+                prepareEstimateShippingIfEnabled: false,
+                prepareAndDisplayOrderReviewData: prepareAndDisplayOrderReviewData ?? false);
+
+            return View(model);
+        }
+    }
+}
