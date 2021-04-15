2f2ebe8bf42a5da9d5deb55a3df7111a3fbcfcd6
Marcel Schmidt <marcel.schmidt@smartstore.com>
2021-04-15 23:06:02 +0200

    HttpExentions > Added GetCheckoutState

--- src/Smartstore.Web.Common/Extensions/HttpExtensions.cs
+++ src/Smartstore.Web.Common/Extensions/HttpExtensions.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
+using Smartstore.Core.Checkout.Orders;
 
 namespace Smartstore
 {
     public static class HttpExtensions
     {
-        //
+        public static CheckoutState GetCheckoutState(this HttpContext httpContext)
+        {
+            Guard.NotNull(httpContext, nameof(httpContext));
+
+            if (httpContext.Session.TryGetObject<CheckoutState>(CheckoutState.CheckoutStateSessionKey, out var state))
+            {
+                return state;
+            }
+
+            state = new CheckoutState();
+            httpContext.Session.TrySetObject(CheckoutState.CheckoutStateSessionKey, state);
+
+            return state;
+        }
     }
 }
