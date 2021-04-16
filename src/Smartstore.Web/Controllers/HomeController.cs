ec81fa169349e8c9fd409e7249609cb6458e65f7
Michael Herzog <herzog@smartstore.com>
2021-04-16 14:33:49 +0200

    ContactUs

--- src/Smartstore.Web/Controllers/HomeController.cs
+++ src/Smartstore.Web/Controllers/HomeController.cs
@@ -74,6 +74,10 @@ using System.Runtime.InteropServices;
 using Smartstore.Core.Catalog;
 using Smartstore.Utilities;
 using Smartstore.ComponentModel;
+using Smartstore.Web.Models.Common;
+using Smartstore.Core.Content.Topics;
+using Smartstore.Utilities.Html;
+using Smartstore.Net.Mail;
 
 namespace Smartstore.Web.Controllers
 {
@@ -119,6 +123,11 @@ namespace Smartstore.Web.Controllers
         private readonly IMediaService _mediaService;
         private readonly HomePageSettings _homePageSettings;
 
+        private readonly IMessageFactory _messageFactory;
+        private readonly PrivacySettings _privacySettings;
+        private readonly CaptchaSettings _captchaSettings;
+        private readonly CommonSettings _commonSettings;
+        
         public HomeController(
             SmartDbContext db,
             ILogger<HomeController> logger1,
@@ -144,7 +153,11 @@ namespace Smartstore.Web.Controllers
             UserManager<Customer> userManager,
             IUserAgent userAgent,
             IMediaService mediaService,
-            HomePageSettings homePageSettings)
+            HomePageSettings homePageSettings,
+            IMessageFactory messageFactory,
+            PrivacySettings privacySettings,
+            CaptchaSettings captchaSettings,
+            CommonSettings commonSettings)
         {
             _db = db;
             _eventPublisher = eventPublisher;
@@ -168,7 +181,12 @@ namespace Smartstore.Web.Controllers
             _userAgent = userAgent;
             _mediaService = mediaService;
             _homePageSettings = homePageSettings;
-            
+
+            _messageFactory = messageFactory;
+            _privacySettings = privacySettings;
+            _captchaSettings = captchaSettings;
+            _commonSettings = commonSettings;
+
             var currentStore = _storeContext.CurrentStore;
         }
 
@@ -188,6 +206,78 @@ namespace Smartstore.Web.Controllers
         public IActionResult StoreClosed()
         {
             return View();
+        }
+
+        // TODO: (mh) (core) Original route probably interfers with generic Topic routing.
+        [GdprConsent]
+        [LocalizedRoute("/contactus-temp", Name = "ContactUs")]
+        public async Task<IActionResult> ContactUs()
+        {
+            var topic = await _db.Topics.AsNoTracking()
+                .Where(x => x.SystemName == "ContactUs")
+                .ApplyStandardFilter()
+                .FirstOrDefaultAsync();
+
+            var model = new ContactUsModel
+            {
+                Email = Services.WorkContext.CurrentCustomer.Email,
+                FullName = Services.WorkContext.CurrentCustomer.GetFullName(),
+                FullNameRequired = _privacySettings.FullNameOnContactUsRequired,
+                DisplayCaptcha = _captchaSettings.CanDisplayCaptcha && _captchaSettings.ShowOnContactUsPage,
+                MetaKeywords = topic?.GetLocalized(x => x.MetaKeywords),
+                MetaDescription = topic?.GetLocalized(x => x.MetaDescription),
+                MetaTitle = topic?.GetLocalized(x => x.MetaTitle),
+            };
+
+            return View(model);
+        }
+
+        // TODO: (mh) (core) Original route probably interfers with generic Topic routing. Test when this was clarified.
+        [HttpPost, ActionName("ContactUs")]
+        [ValidateCaptcha, ValidateHoneypot]
+        [GdprConsent]
+        public async Task<IActionResult> ContactUsSend(ContactUsModel model, string captchaError)
+        {
+            if (_captchaSettings.ShowOnContactUsPage && captchaError.HasValue())
+            {
+                ModelState.AddModelError("", captchaError);
+            }
+
+            if (ModelState.IsValid)
+            {
+                var customer = Services.WorkContext.CurrentCustomer;
+                var email = model.Email.Trim();
+                var fullName = model.FullName;
+                var subject = T("ContactUs.EmailSubject", Services.StoreContext.CurrentStore.Name);
+                var body = HtmlUtils.ConvertPlainTextToHtml(model.Enquiry.HtmlEncode());
+
+                // Required for some SMTP servers.
+                MailAddress sender = null;
+                if (!_commonSettings.UseSystemEmailForContactUsForm)
+                {
+                    sender = new MailAddress(email, fullName);
+                }
+
+                var msg = await _messageFactory.SendContactUsMessageAsync(customer, email, fullName, subject, body, sender);
+
+                if (msg?.Email?.Id != null)
+                {
+                    model.SuccessfullySent = true;
+                    model.Result = T("ContactUs.YourEnquiryHasBeenSent");
+                    Services.ActivityLogger.LogActivity("PublicStore.ContactUs", T("ActivityLog.PublicStore.ContactUs"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", T("Common.Error.SendMail"));
+                    model.Result = T("Common.Error.SendMail");
+                }
+
+                return View(model);
+            }
+
+            model.DisplayCaptcha = _captchaSettings.CanDisplayCaptcha && _captchaSettings.ShowOnContactUsPage;
+
+            return View(model);
         }
 
         #region Testing area
---
+++ src/Smartstore.Web/Models/Common/ContactUsModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+using Smartstore.Core.Identity;
+using Smartstore.Web.Modelling;
+using Smartstore.Web.Modelling.Validation;
+
+namespace Smartstore.Web.Models.Common
+{
+    [LocalizedDisplay("ContactUs.")]
+    public partial class ContactUsModel : ModelBase
+    {
+        [LocalizedDisplay("*Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [SanitizeHtml]
+        [LocalizedDisplay("*Enquiry")]
+        public string Enquiry { get; set; }
+
+        [LocalizedDisplay("*FullName")]
+        public string FullName { get; set; }
+        public bool FullNameRequired { get; set; }
+
+        public bool SuccessfullySent { get; set; }
+        public string Result { get; set; }
+
+        public bool DisplayCaptcha { get; set; }
+
+        public string MetaTitle { get; set; }
+        public string MetaDescription { get; set; }
+        public string MetaKeywords { get; set; }
+    }
+
+    public class ContactUsValidator : SmartValidator<ContactUsModel>
+    {
+        public ContactUsValidator(PrivacySettings privacySettings)
+        {
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Enquiry).NotEmpty();
+
+            if (privacySettings.FullNameOnContactUsRequired)
+            {
+                RuleFor(x => x.FullName).NotEmpty();
+            }
+        }
+    }
+}
