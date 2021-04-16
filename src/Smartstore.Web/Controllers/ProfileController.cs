c25523311807bf2ffe2881e28f04a4ae4b9e799f
Michael Herzog <herzog@smartstore.com>
2021-04-16 18:18:04 +0200

    Profiles (this will be no fun to review, more comments then code in this commit :-)

---
+++ src/Smartstore.Web/Controllers/ProfileController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Common.Services;
+using Smartstore.Core.Data;
+using Smartstore.Core.Identity;
+using Smartstore.Core.Localization;
+using Smartstore.Core.Localization.Routing;
+using Smartstore.Web.Models.Customers;
+using Smartstore.Web.Models.Profiles;
+
+namespace Smartstore.Web.Controllers
+{
+    public class ProfileController : SmartController
+    {
+        private readonly SmartDbContext _db;
+        private readonly IDateTimeHelper _dateTimeHelper;
+        private readonly CustomerSettings _customerSettings;
+
+        public ProfileController(SmartDbContext db, IDateTimeHelper dateTimeHelper, CustomerSettings customerSettings)
+        {
+            _db = db;
+            _dateTimeHelper = dateTimeHelper;
+            _customerSettings = customerSettings;
+        }
+
+        // TODO: (mh) (core) What to do with this forum post paging param?
+        [LocalizedRoute("/profile/{id:int}", Name = "CustomerProfile")]
+        public async Task<IActionResult> Index(int id, int? page)
+        {
+            var customer = await _db.Customers.FindByIdAsync(id, false);
+            if (!_customerSettings.AllowViewingProfiles || customer == null || customer.IsGuest())
+            {
+                return NotFound();
+            }
+
+            var name = customer.FormatUserName(_customerSettings, T, true);
+
+            var model = new ProfileIndexModel
+            {
+                Id = customer.Id,
+                ProfileTitle = T("Profile.ProfileOf", name),
+                //PostsPage = page ?? 0,
+                //PagingPosts = page.HasValue,
+                //ForumsEnabled = _forumSettings.ForumsEnabled
+            };
+
+            // INFO: (mh) (core) Model preparation for old Info action starts here.
+            // TODO: (mh) (core) Make prepare model method so we dont always must refer to model.ProfileInfo...
+            model.ProfileInfo.Id = id;
+
+            model.ProfileInfo.Avatar = customer.ToAvatarModel(null, true);
+
+            // Location.
+            if (_customerSettings.ShowCustomersLocation)
+            {
+                model.ProfileInfo.LocationEnabled = true;
+
+                var country = await _db.Countries.FindByIdAsync((int)customer.GenericAttributes.CountryId);
+                    
+                if (country != null)
+                {
+                    model.ProfileInfo.Location = country.GetLocalized(x => x.Name);
+                }
+                else
+                {
+                    model.ProfileInfo.LocationEnabled = false;
+                }
+            }
+
+            // TODO: (mh) (core) Forum module must handle this somehow. Also ask if forum is enabled (because now PMs can be sent even if the Forum is turned off). 
+            // Private message.
+            //model.ProfileInfo.PMEnabled = _forumSettings.AllowPrivateMessages && !customer.IsGuest();
+
+            // TODO: (mh) (core) Forum module must handle this somehow. 
+            // Total forum posts.
+            //if (_forumSettings.ForumsEnabled && _forumSettings.ShowCustomersPostCount)
+            //{
+            //    model.TotalPostsEnabled = true;
+            //    model.TotalPosts = customer.GetAttribute<int>(SystemCustomerAttributeNames.ForumPostCount, _genericAttributeService);
+            //}
+
+            // Registration date.
+            if (_customerSettings.ShowCustomersJoinDate)
+            {
+                model.ProfileInfo.JoinDateEnabled = true;
+                model.ProfileInfo.JoinDate = _dateTimeHelper.ConvertToUserTime(customer.CreatedOnUtc, DateTimeKind.Utc).ToString("f");
+            }
+
+            // Birth date.
+            if (_customerSettings.DateOfBirthEnabled && customer.BirthDate.HasValue)
+            {
+                model.ProfileInfo.DateOfBirthEnabled = true;
+                model.ProfileInfo.DateOfBirth = customer.BirthDate.Value.ToString("D");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Smartstore.Web/Models/Profiles/ProfileIndexModel.cs
@@ -0,0 +1,37 @@
+﻿using Smartstore.Web.Modelling;
+using Smartstore.Web.Models.Customers;
+
+namespace Smartstore.Web.Models.Profiles
+{
+    public partial class ProfileIndexModel : EntityModelBase
+    {
+        public string ProfileTitle { get; set; }
+
+        public ProfileInfoModel ProfileInfo { get; set; } = new();
+
+        // TODO: (mh) (core) Implement in forum module.
+        //public int PostsPage { get; set; }
+        //public bool PagingPosts { get; set; }
+        //public bool ForumsEnabled { get; set; }
+    }
+
+    public partial class ProfileInfoModel : EntityModelBase
+    {
+        public CustomerAvatarModel Avatar { get; set; }
+
+        public bool LocationEnabled { get; set; }
+        public string Location { get; set; }
+
+        // TODO: (mh) (core) Implement in forum module.
+        //public bool PMEnabled { get; set; }
+
+        //public bool TotalPostsEnabled { get; set; }
+        //public int TotalPosts { get; set; }
+
+        public bool JoinDateEnabled { get; set; }
+        public string JoinDate { get; set; }
+
+        public bool DateOfBirthEnabled { get; set; }
+        public string DateOfBirth { get; set; }
+    }
+}
