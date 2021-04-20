48c71aa36d8cc7010cf7ed5c14d9cbea21cece03
Michael Herzog <herzog@smartstore.com>
2021-04-20 19:51:06 +0200

    Removed old code

--- src/Smartstore.Web.Common/TagHelpers/Shared/LanguageTagHelper.cs
+++ src/Smartstore.Web.Common/TagHelpers/Shared/LanguageTagHelper.cs
@@ -5,14 +5,12 @@ using Smartstore.Core.Localization;
 namespace Smartstore.Web.TagHelpers.Shared
 {
     [HtmlTargetElement("title")]
-    [HtmlTargetElement("body")]
     [HtmlTargetElement("*", Attributes = LangForAttributeName)]
     public class LanguageTagHelper : TagHelper
     {
         const string DirAttributeName = "dir";
         const string LangAttributeName = "lang";
         const string LangForAttributeName = "sm-language-attributes-for";
-        const string BodyTagName = "body";
         const string TitleTagName = "title";
 
         /// <summary>
@@ -28,14 +26,6 @@ namespace Smartstore.Web.TagHelpers.Shared
                 // Render meta accept-language right before the title tag
                 var acceptLang = CultureInfo.CurrentUICulture.ToString();
                 output.PreElement.AppendHtml($"<meta name='accept-language' content='{acceptLang}'/>");
-            }
-            else if (context.TagName == BodyTagName && !output.Attributes.ContainsName(DirAttributeName))
-            {
-                // Add dir attribute to body
-                // TODO: (mc) (core) IsRightToLeft always returns false no matter what. Also this attribute was set on html-tag level prior.
-                var isRtl = CultureInfo.CurrentUICulture.TextInfo.IsRightToLeft;
-                var val = isRtl ? "rtl" : "ltr";
-                output.Attributes.Add(DirAttributeName, val);
             }
 
             Language currentLanguage = null;
