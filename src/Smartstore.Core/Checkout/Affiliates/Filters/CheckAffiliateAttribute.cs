27427f8f5b44b85105602c9abb7236017515a89f
Murat Cakir <muratc@smartstore.de>
2021-04-28 02:14:28 +0200

    CheckAffiliateAttribute

---
+++ src/Smartstore.Core/Checkout/Affiliates/Filters/CheckAffiliateAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using Smartstore.Core.Data;
+
+namespace Smartstore.Core.Checkout.Affiliates
+{
+    public sealed class CheckAffiliateAttribute : TypeFilterAttribute
+    {
+        public CheckAffiliateAttribute()
+            : base(typeof(CheckAffiliateFilter))
+        {
+        }
+
+        class CheckAffiliateFilter : IAsyncActionFilter
+        {
+            private readonly SmartDbContext _db;
+            private readonly IWorkContext _workContext;
+
+            public CheckAffiliateFilter(SmartDbContext db, IWorkContext workContext)
+            {
+                _db = db;
+                _workContext = workContext;
+            }
+
+            public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+            {
+                var customer = _workContext.CurrentCustomer;
+                var query = context.HttpContext.Request.Query["affiliateId"].ToString();
+
+                if (int.TryParse(query, out int affiliateId) && affiliateId > 0 && !customer.IsSystemAccount && customer.AffiliateId != affiliateId)
+                {
+                    var isValidAffiliate = await _db.Affiliates
+                        .Where(x => x.Id == affiliateId && !x.Deleted && x.Active)
+                        .AnyAsync();
+
+                    if (isValidAffiliate)
+                    {
+                        customer.AffiliateId = affiliateId;
+                    }
+                }
+
+                await next();
+            }
+        }
+    }
+}
--- src/Smartstore.Web.Common/Controllers/PublicControllerBase.cs
+++ src/Smartstore.Web.Common/Controllers/PublicControllerBase.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using Smartstore.Core.Checkout.Affiliates;
 using Smartstore.Core.Data;
 using Smartstore.Core.Identity;
 using Smartstore.Core.Security;
 
 namespace Smartstore.Web.Controllers
 {
-    // TODO: (core) Implement base filters for PublicControllerBase
     [AuthorizeShopAccess]
     [TrackActivity(Order = 100)]
+    [CheckAffiliate(Order = 100)]
     [SaveChanges(typeof(SmartDbContext), Order = int.MaxValue)]
     public class PublicControllerBase : SmartController
     {
