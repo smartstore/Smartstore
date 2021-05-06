2983c1a44070784c0a7b743a7cf99f2f085b5d01
Marcel Schmidt <marcel.schmidt@smartstore.com>
2021-05-06 22:42:56 +0200

    Added all known activity log type system names as constants

--- src/Smartstore.Core/Platform/Logging/Services/KnownActivityLogTypes.cs
+++ src/Smartstore.Core/Platform/Logging/Services/KnownActivityLogTypes.cs
@@ -1,9 +1,70 @@
-﻿using System;
-
-namespace Smartstore.Core.Logging
+﻿namespace Smartstore.Core.Logging
 {
     public static class KnownActivityLogTypes
     {
-        // TODO: (ms) (core) Declare all known activity log type system names as constants.
+        public const string AddNewCategory = "AddNewCategory";
+        public const string AddNewCheckoutAttribute = "AddNewCheckoutAttribute";
+        public const string AddNewCustomer = "AddNewCustomer";
+        public const string AddNewCustomerRole = "AddNewCustomerRole";
+        public const string AddNewDiscount = "AddNewDiscount";
+        public const string AddNewGiftCard = "AddNewGiftCard";
+        public const string AddNewManufacturer = "AddNewManufacturer";
+        public const string AddNewProduct = "AddNewProduct";
+        public const string AddNewProductAttribute = "AddNewProductAttribute";
+        public const string AddNewSetting = "AddNewSetting";
+        public const string AddNewSpecAttribute = "AddNewSpecAttribute";
+        public const string AddNewWidget = "AddNewWidget";
+        public const string DeleteCategory = "DeleteCategory";
+        public const string DeleteCheckoutAttribute = "DeleteCheckoutAttribute";
+        public const string DeleteCustomer = "DeleteCustomer";
+        public const string DeleteCustomerRole = "DeleteCustomerRole";
+        public const string DeleteDiscount = "DeleteDiscount";
+        public const string DeleteGiftCard = "DeleteGiftCard";
+        public const string DeleteManufacturer = "DeleteManufacturer";
+        public const string DeleteProduct = "DeleteProduct";
+        public const string DeleteProductAttribute = "DeleteProductAttribute";
+        public const string DeleteReturnRequest = "DeleteReturnRequest";
+        public const string DeleteSetting = "DeleteSetting";
+        public const string DeleteSpecAttribute = "DeleteSpecAttribute";
+        public const string DeleteWidget = "DeleteWidget";
+        public const string EditCategory = "EditCategory";
+        public const string EditCheckoutAttribute = "EditCheckoutAttribute";
+        public const string EditCustomer = "EditCustomer";
+        public const string EditCustomerRole = "EditCustomerRole";
+        public const string EditDiscount = "EditDiscount";
+        public const string EditGiftCard = "EditGiftCard";
+        public const string EditManufacturer = "EditManufacturer";
+        public const string EditProduct = "EditProduct";
+        public const string EditProductAttribute = "EditProductAttribute";
+        public const string EditPromotionProviders = "EditPromotionProviders";
+        public const string EditReturnRequest = "EditReturnRequest";
+        public const string EditSettings = "EditSettings";
+        public const string EditSpecAttribute = "EditSpecAttribute";
+        public const string EditWidget = "EditWidget";
+        public const string EditThemeVars = "EditThemeVars";
+        public const string ResetThemeVars = "ResetThemeVars";
+        public const string ImportThemeVars = "ImportThemeVars";
+        public const string ExportThemeVars = "ExportThemeVars";
+        public const string PublicStoreViewCategory = "PublicStore.ViewCategory";
+        public const string PublicStoreViewManufacturer = "PublicStore.ViewManufacturer";
+        public const string PublicStoreViewProduct = "PublicStore.ViewProduct";
+        public const string PublicStorePlaceOrder = "PublicStore.PlaceOrder";
+        public const string PublicStoreSendPM = "PublicStore.SendPM";
+        public const string PublicStoreContactUs = "PublicStore.ContactUs";
+        public const string PublicStoreAddToCompareList = "PublicStore.AddToCompareList";
+        public const string PublicStoreAddToShoppingCart = "PublicStore.AddToShoppingCart";
+        public const string PublicStoreAddToWishlist = "PublicStore.AddToWishlist";
+        public const string PublicStoreLogin = "PublicStore.Login";
+        public const string PublicStoreLogout = "PublicStore.Logout";
+        public const string PublicStoreAddProductReview = "PublicStore.AddProductReview";
+        public const string PublicStoreAddNewsComment = "PublicStore.AddNewsComment";
+        public const string PublicStoreAddBlogComment = "PublicStore.AddBlogComment";
+        public const string PublicStoreAddForumTopic = "PublicStore.AddForumTopic";
+        public const string PublicStoreEditForumTopic = "PublicStore.EditForumTopic";
+        public const string PublicStoreDeleteForumTopic = "PublicStore.DeleteForumTopic";
+        public const string PublicStoreAddForumPost = "PublicStore.AddForumPost";
+        public const string PublicStoreEditForumPost = "PublicStore.EditForumPost";
+        public const string PublicStoreDeleteForumPost = "PublicStore.DeleteForumPost";
+        public const string EditOrder = "EditOrder";
     }
 }
