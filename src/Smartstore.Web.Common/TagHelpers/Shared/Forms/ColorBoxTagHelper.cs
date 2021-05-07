95b3a419b52496591030dae765b7433b9d0693f4
Michael Herzog <herzog@smartstore.com>
2021-05-07 14:32:30 +0200

    Minor refactoring of ColorBoxTagHelper so it can be used without asp-for

--- src/Smartstore.Web.Common/TagHelpers/Shared/Forms/ColorBoxTagHelper.cs
+++ src/Smartstore.Web.Common/TagHelpers/Shared/Forms/ColorBoxTagHelper.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 namespace Smartstore.Web.TagHelpers.Shared
 {
     [OutputElementHint("div")]
-    [HtmlTargetElement("colorbox", Attributes = ForAttributeName, TagStructure = TagStructure.WithoutEndTag)]
+    [HtmlTargetElement("colorbox", TagStructure = TagStructure.WithoutEndTag)]
     public class ColorBoxTagHelper : BaseFormTagHelper
     {
         const string DefaultColorAttributeName = "sm-default-color";
@@ -14,21 +14,32 @@ namespace Smartstore.Web.TagHelpers.Shared
 
         protected override void ProcessCore(TagHelperContext context, TagHelperOutput output)
         {
+            string value;
+            if (output.Attributes.TryGetAttribute("value", out var valueAttribute))
+            {
+                value = valueAttribute.Value.ToString();
+            }
+            else
+            {
+                value = For != null ? For.Model.ToString() : string.Empty;
+            }
+
             var defaultColor = DefaultColor.EmptyNull();
-            var color = For.Model.ToString();
-            var isDefault = color.EqualsNoCase(defaultColor);
+            var isDefault = value.EqualsNoCase(defaultColor);
 
             output.TagName = "div";
             output.TagMode = TagMode.StartTagAndEndTag;
             output.AppendCssClass("input-group colorpicker-component sm-colorbox");
             output.Attributes.Add("data-fallback-color", defaultColor);
 
-            output.Content.AppendHtml(HtmlHelper.TextBox(For.Name, isDefault ? string.Empty : color, new { @class = "form-control colorval", placeholder = defaultColor }));
+            output.Content.AppendHtml(HtmlHelper.TextBox(For != null ? For.Name : string.Empty, 
+                isDefault ? string.Empty : value, 
+                new { @class = "form-control colorval", placeholder = defaultColor }));
 
             var addon = new TagBuilder("div");
             addon.AddCssClass("input-group-append input-group-addon");
             addon.InnerHtml.AppendHtml(
-                "<div class=\"input-group-text\"><i class=\"thecolor\" style=\"{0}\">&nbsp;</i></div>".FormatWith(DefaultColor.HasValue() ? "background-color: " + DefaultColor : string.Empty));
+                $"<div class='input-group-text'><i class='thecolor' style='{(DefaultColor.HasValue() ? "background-color: " + DefaultColor : string.Empty)}'>&nbsp;</i></div>");
 
             output.Content.AppendHtml(addon);
         }
