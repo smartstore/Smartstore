03fd40dea2e77545918b8eddc70ac5b8c393dda0
Michael Herzog <herzog@smartstore.com>
2021-05-20 19:36:45 +0200

    Added ModelBoundEvent

---
+++ src/Smartstore.Web.Common/Modelling/ModelBoundEvent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Smartstore.Web.Modelling
+{
+    public class ModelBoundEvent
+    {
+        public ModelBoundEvent(TabbableModel boundModel, object entityModel, IFormCollection form)
+        {
+            BoundModel = boundModel;
+            EntityModel = entityModel;
+            Form = form;
+        }
+
+        public TabbableModel BoundModel { get; private set; }
+        public object EntityModel { get; private set; }
+        public IFormCollection Form { get; private set; }
+    }
+}
