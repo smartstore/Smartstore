28f8aba52fce1ec22dab18509b0faf93645cb2c3
Michael Herzog <herzog@smartstore.com>
2021-05-20 19:32:19 +0200

    Added ParameterBasedOnFormNameAttribute

---
+++ src/Smartstore.Web.Common/Controllers/Selectors/ParameterBasedOnFormNameAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Smartstore.Web.Controllers
+{
+    /// <summary>
+    /// If form name exists, then specified "actionParameterName" will be set to "true".
+    /// </summary>
+    public class ParameterBasedOnFormNameAttribute : ActionFilterAttribute
+    {
+        private readonly string _name;
+        private readonly string _actionParameterName;
+
+        public ParameterBasedOnFormNameAttribute(string name, string actionParameterName)
+        {
+            _name = name;
+            _actionParameterName = actionParameterName;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var formValue = filterContext.HttpContext.Request.Form[_name];
+            filterContext.ActionArguments[_actionParameterName] = !string.IsNullOrEmpty(formValue);
+        }
+    }
+}
