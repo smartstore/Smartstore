02039baffee37c63758ed4331831d274e6bb421e
Michael Herzog <herzog@smartstore.com>
2021-05-21 10:49:29 +0200

    Topic edit view

--- src/Smartstore.Web/Areas/Admin/Controllers/_TopicController.cs
+++ src/Smartstore.Web/Areas/Admin/Controllers/_TopicController.cs
@@ -1,15 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Dasync.Collections;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Smartstore.Admin.Models.Topics;
+using Smartstore.Collections;
+using Smartstore.ComponentModel;
 using Smartstore.Core.Catalog.Brands;
 using Smartstore.Core.Catalog.Categories;
 using Smartstore.Core.Catalog.Products;
+using Smartstore.Core.Content.Menus;
 using Smartstore.Core.Content.Topics;
 using Smartstore.Core.Data;
+using Smartstore.Core.Identity;
 using Smartstore.Core.Localization;
+using Smartstore.Core.Security;
+using Smartstore.Core.Seo;
+using Smartstore.Core.Stores;
 using Smartstore.Web.Controllers;
 using Smartstore.Web.Modelling;
 
@@ -18,16 +29,154 @@ namespace Smartstore.Admin.Controllers
     public class TopicController : AdminControllerBase
     {
         private readonly SmartDbContext _db;
+        private readonly ILocalizedEntityService _localizedEntityService;
+        private readonly IStoreMappingService _storeMappingService;
+        private readonly IAclService _aclService;
+        private readonly ILinkResolver _linkResolver;
+        private readonly IUrlService _urlService;
         
         public TopicController(
-            SmartDbContext db)
+            SmartDbContext db,
+            ILocalizedEntityService localizedEntityService,
+            IStoreMappingService storeMappingService,
+            IAclService aclService,
+            ILinkResolver linkResolver,
+            IUrlService urlService)
         {
             _db = db;
+            _localizedEntityService = localizedEntityService;
+            _storeMappingService = storeMappingService;
+            _aclService = aclService;
+            _linkResolver = linkResolver;
+            _urlService = urlService;
         }
 
         public IActionResult Index()
         {
             return RedirectToAction("List");
+        }
+
+        [Permission(Permissions.Cms.Topic.Read)]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var topic = await _db.Topics.FindByIdAsync(id, false);
+            if (topic == null)
+            {
+                return RedirectToAction("List");
+            }
+
+            // TODO: (mh) (core) Implement & use mapping extensions.
+            // TODO: (mh) (core) Research why the following call breaks.
+            //var model = await MapperFactory.MapAsync<Topic, TopicModel>(topic);
+
+            var model = new TopicModel();
+            await MapperFactory.MapAsync(topic, model);
+            await PrepareTopicModelAsync(topic, model);
+
+            model.WidgetZone = topic.WidgetZone.SplitSafe(",").ToArray();
+            model.CookieType = (int?)topic.CookieType;
+
+            AddLocales(model.Locales, async (locale, languageId) =>
+            {
+                locale.ShortTitle = topic.GetLocalized(x => x.ShortTitle, languageId, false, false);
+                locale.Title = topic.GetLocalized(x => x.Title, languageId, false, false);
+                locale.Intro = topic.GetLocalized(x => x.Intro, languageId, false, false);
+                locale.Body = topic.GetLocalized(x => x.Body, languageId, false, false);
+                locale.MetaKeywords = topic.GetLocalized(x => x.MetaKeywords, languageId, false, false);
+                locale.MetaDescription = topic.GetLocalized(x => x.MetaDescription, languageId, false, false);
+                locale.MetaTitle = topic.GetLocalized(x => x.MetaTitle, languageId, false, false);
+                locale.SeName = await topic.GetActiveSlugAsync(languageId, false, false);
+            });
+
+            // Get menu links.
+            IPagedList<MenuEntity> menus = null;
+            var pageIndex = 0;
+
+            do
+            {
+                menus = await _db.Menus
+                    .ApplyStandardFilter(true)
+                    .ToPagedList(pageIndex++, 500)
+                    .LoadAsync();
+                
+                foreach (var menu in menus)
+                {
+                    foreach (var item in menu.Items.Where(x => x.ProviderName != null && x.ProviderName == "entity"))
+                    {
+                        var link = await _linkResolver.ResolveAsync(item.Model);
+                        if (link.Type == LinkType.Topic && link.Id == topic.Id)
+                        {
+                            var url = Url.Action("EditItem", "Menu", new { id = item.Id, area = "Admin" });
+
+                            var label = string.Concat(
+                                menu.Title.NullEmpty() ?? menu.SystemName.NullEmpty() ?? "".NaIfEmpty(),
+                                " » ",
+                                item.Title.NullEmpty() ?? link.Label.NullEmpty() ?? "".NaIfEmpty());
+
+                            model.MenuLinks[url] = label;
+                        }
+                    }
+                }
+            }
+            while (menus.HasNextPage);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [Permission(Permissions.Cms.Topic.Update)]
+        public async Task<IActionResult> Edit(TopicModel model, bool continueEditing)
+        {
+            var topic = await _db.Topics.FindByIdAsync(model.Id);
+            if (topic == null)
+            {
+                return RedirectToAction("List");
+            }
+
+            if (!model.IsPasswordProtected)
+            {
+                model.Password = null;
+            }
+
+            if (ModelState.IsValid)
+            {
+                // TODO: (mh) (core) Implement & use mapping extensions.
+                await MapperFactory.MapAsync(model, topic);
+
+                if (model.WidgetZone != null)
+                {
+                    topic.WidgetZone = string.Join(",", model.WidgetZone);
+                }
+
+                topic.CookieType = (CookieType?)model.CookieType;
+
+                await _db.SaveChangesAsync();
+
+                var slugResult = await topic.ValidateSlugAsync(model.SeName, true);
+                model.SeName = slugResult.Slug;
+                await _urlService.ApplySlugAsync(slugResult, true);
+                
+                await SaveStoreMappingsAsync(topic, model.SelectedStoreIds);
+                await SaveAclMappingsAsync(topic, model.SelectedCustomerRoleIds);
+                await UpdateLocalesAsync(topic, model);
+                AddCookieTypes(model, model.CookieType);
+
+                await Services.EventPublisher.PublishAsync(new ModelBoundEvent(model, topic, Request.Form));
+
+                NotifySuccess(T("Admin.ContentManagement.Topics.Updated"));
+                return continueEditing ? RedirectToAction("Edit", topic.Id) : RedirectToAction("List");
+            }
+
+            // TODO: (mh) (core) Remove comment after review.
+            // INFO: (mh) (core) Not neccessary. There's no such message anymore.
+            // Chrome spat out an error message after validation with this rule .Must(u => u.IsEmpty() || !u.Any(x => char.IsWhiteSpace(x)))
+            // HttpContext.Response.Headers.Add("X-XSS-Protection", "0");
+
+            // If we got this far something failed. Redisplay form.
+            await PrepareTopicModelAsync(topic, model);
+
+            return View(model);
         }
 
         /// <summary>
@@ -77,5 +226,94 @@ namespace Smartstore.Admin.Controllers
 
             return new JsonResult(list);
         }
+
+        #region Helper functions
+
+        private async Task PrepareTopicModelAsync(Topic topic, TopicModel model)
+        {
+            model.Url = await GetTopicUrlAsync(topic);
+            model.SelectedStoreIds = await _storeMappingService.GetAuthorizedStoreIdsAsync(topic);
+            model.SelectedCustomerRoleIds = await _aclService.GetAuthorizedCustomerRoleIdsAsync(topic);
+            AddCookieTypes(model, model.CookieType);
+        }
+
+        private async Task UpdateLocalesAsync(Topic topic, TopicModel model)
+        {
+            foreach (var localized in model.Locales)
+            {
+                await _localizedEntityService.ApplyLocalizedValueAsync(topic, x => x.ShortTitle, localized.ShortTitle, localized.LanguageId);
+                await _localizedEntityService.ApplyLocalizedValueAsync(topic, x => x.Title, localized.Title, localized.LanguageId);
+                await _localizedEntityService.ApplyLocalizedValueAsync(topic, x => x.Intro, localized.Intro, localized.LanguageId);
+                await _localizedEntityService.ApplyLocalizedValueAsync(topic, x => x.Body, localized.Body, localized.LanguageId);
+                await _localizedEntityService.ApplyLocalizedValueAsync(topic, x => x.MetaKeywords, localized.MetaKeywords, localized.LanguageId);
+                await _localizedEntityService.ApplyLocalizedValueAsync(topic, x => x.MetaDescription, localized.MetaDescription, localized.LanguageId);
+                await _localizedEntityService.ApplyLocalizedValueAsync(topic, x => x.MetaTitle, localized.MetaTitle, localized.LanguageId);
+
+                await _db.SaveChangesAsync();
+
+                var slugResult = await topic.ValidateSlugAsync(localized.SeName, true);
+                model.SeName = slugResult.Slug;
+                await _urlService.ApplySlugAsync(slugResult, true);
+            }
+        }
+
+        private async Task<string> GetTopicUrlAsync(Topic topic)
+        {
+            string url = null;
+
+            try
+            {
+                if (topic.LimitedToStores)
+                {
+                    var storeIds = await _storeMappingService.GetAuthorizedStoreIdsAsync(topic);
+                    var currentStoreIsAuthorized = storeIds.Contains(Services.StoreContext.CurrentStore.Id);
+
+                    if (currentStoreIsAuthorized)
+                    {
+                        var store = await _db.Stores.FindByIdAsync(Services.StoreContext.CurrentStore.Id);
+                        if (store != null)
+                        {
+                            var baseUri = new Uri(store.GetHost());
+                            url = baseUri.GetLeftPart(UriPartial.Authority) + Url.RouteUrl("Topic", new { SeName = await topic.GetActiveSlugAsync() });
+                        }
+                    }
+                }
+
+                if (url.IsEmpty())
+                {
+                    url = Url.RouteUrl("Topic", new { SeName = await topic.GetActiveSlugAsync() }, Request.Scheme);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+
+            return url;
+        }
+
+        private static void AddCookieTypes(TopicModel model, int? selectedType = 0)
+        {
+            model.AvailableCookieTypes.Add(new SelectListItem()
+            {
+                Text = "Required",
+                Value = ((int)CookieType.Required).ToString(),
+                Selected = CookieType.Required == (CookieType?)selectedType
+            });
+            model.AvailableCookieTypes.Add(new SelectListItem()
+            {
+                Text = "Analytics",
+                Value = ((int)CookieType.Analytics).ToString(),
+                Selected = CookieType.Analytics == (CookieType?)selectedType
+            });
+            model.AvailableCookieTypes.Add(new SelectListItem()
+            {
+                Text = "ThirdParty",
+                Value = ((int)CookieType.ThirdParty).ToString(),
+                Selected = CookieType.ThirdParty == (CookieType?)selectedType
+            });
+        }
+
+        #endregion
     }
 }
---
+++ src/Smartstore.Web/Areas/Admin/Models/Topics/TopicModel.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Smartstore.ComponentModel;
+using Smartstore.Core.Content.Topics;
+using Smartstore.Core.Localization;
+using Smartstore.Core.Seo;
+using Smartstore.Web.Modelling;
+
+namespace Smartstore.Admin.Models.Topics
+{
+    [LocalizedDisplay("Admin.ContentManagement.Topics.Fields.")]
+    public class TopicModel : TabbableModel, ILocalizedModel<TopicLocalizedModel>
+    {
+        public TopicModel()
+        {
+            AvailableTitleTags.Add(new SelectListItem { Text = "h1", Value = "h1" });
+            AvailableTitleTags.Add(new SelectListItem { Text = "h2", Value = "h2" });
+            AvailableTitleTags.Add(new SelectListItem { Text = "h3", Value = "h3" });
+            AvailableTitleTags.Add(new SelectListItem { Text = "h4", Value = "h4" });
+            AvailableTitleTags.Add(new SelectListItem { Text = "h5", Value = "h5" });
+            AvailableTitleTags.Add(new SelectListItem { Text = "h6", Value = "h6" });
+            AvailableTitleTags.Add(new SelectListItem { Text = "div", Value = "div" });
+            AvailableTitleTags.Add(new SelectListItem { Text = "span", Value = "span" });
+        }
+
+        [UIHint("Stores")]
+        [LocalizedDisplay("Admin.Common.Store.LimitedTo")]
+        public int[] SelectedStoreIds { get; set; }
+
+        [LocalizedDisplay("Admin.Common.Store.LimitedTo")]
+        public bool LimitedToStores { get; set; }
+
+        [UIHint("CustomerRoles")]
+        [LocalizedDisplay("Admin.Common.CustomerRole.LimitedTo")]
+        public int[] SelectedCustomerRoleIds { get; set; }
+
+        [LocalizedDisplay("Admin.Common.CustomerRole.LimitedTo")]
+        public bool SubjectToAcl { get; set; }
+
+        [LocalizedDisplay("*CookieType")]
+        public int? CookieType { get; set; }
+        public List<SelectListItem> AvailableCookieTypes { get; set; } = new();
+
+        [LocalizedDisplay("*SystemName")]
+        public string SystemName { get; set; }
+
+        [LocalizedDisplay("*HtmlId")]
+        public string HtmlId { get; set; }
+
+        [LocalizedDisplay("*BodyCssClass")]
+        public string BodyCssClass { get; set; }
+
+        [LocalizedDisplay("*IncludeInSitemap")]
+        public bool IncludeInSitemap { get; set; }
+
+        [LocalizedDisplay("*IsPasswordProtected")]
+        public bool IsPasswordProtected { get; set; }
+
+        [LocalizedDisplay("*Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [LocalizedDisplay("*URL")]
+        public string Url { get; set; }
+
+        [LocalizedDisplay("*ShortTitle")]
+        public string ShortTitle { get; set; }
+
+        [LocalizedDisplay("*Title")]
+        public string Title { get; set; }
+
+        [LocalizedDisplay("*Intro")]
+        public string Intro { get; set; }
+
+        [LocalizedDisplay("*Body")]
+        public string Body { get; set; }
+
+        [LocalizedDisplay("Admin.Configuration.Seo.MetaKeywords")]
+        public string MetaKeywords { get; set; }
+
+        [LocalizedDisplay("Admin.Configuration.Seo.MetaDescription")]
+        public string MetaDescription { get; set; }
+
+        [LocalizedDisplay("Admin.Configuration.Seo.MetaTitle")]
+        public string MetaTitle { get; set; }
+
+        [LocalizedDisplay("Admin.Configuration.Seo.SeName")]
+        public string SeName { get; set; }
+
+        [LocalizedDisplay("*RenderAsWidget")]
+        public bool RenderAsWidget { get; set; }
+
+        [LocalizedDisplay("*WidgetZone")]
+        [UIHint("WidgetZone")]
+        public string[] WidgetZone { get; set; }
+
+        [LocalizedDisplay("*WidgetWrapContent")]
+        public bool WidgetWrapContent { get; set; } = true;
+
+        [LocalizedDisplay("*WidgetShowTitle")]
+        public bool WidgetShowTitle { get; set; }
+
+        [LocalizedDisplay("*WidgetBordered")]
+        public bool WidgetBordered { get; set; }
+
+        [LocalizedDisplay("*Priority")]
+        public int Priority { get; set; }
+
+        [LocalizedDisplay("*TitleTag")]
+        public string TitleTag { get; set; }
+
+        public bool IsSystemTopic { get; set; }
+
+        [LocalizedDisplay("Common.Published")]
+        public bool IsPublished { get; set; }
+
+        public List<SelectListItem> AvailableTitleTags { get; private set; } = new();
+
+        public List<TopicLocalizedModel> Locales { get; set; } = new();
+
+        [LocalizedDisplay("Admin.ContentManagement.MenuLinks")]
+        public Dictionary<string, string> MenuLinks { get; set; } = new();
+    }
+
+    [LocalizedDisplay("Admin.ContentManagement.Topics.Fields.")]
+    public class TopicLocalizedModel : ILocalizedLocaleModel
+    {
+        public int LanguageId { get; set; }
+
+        [LocalizedDisplay("*ShortTitle")]
+        public string ShortTitle { get; set; }
+
+        [LocalizedDisplay("*Title")]
+        public string Title { get; set; }
+
+        [LocalizedDisplay("*Intro")]
+        public string Intro { get; set; }
+
+        [LocalizedDisplay("*Body")]
+        public string Body { get; set; }
+
+        [LocalizedDisplay("*MetaKeywords")]
+        public string MetaKeywords { get; set; }
+
+        [LocalizedDisplay("*MetaDescription")]
+        public string MetaDescription { get; set; }
+
+        [LocalizedDisplay("*MetaTitle")]
+        public string MetaTitle { get; set; }
+
+        [LocalizedDisplay("*SeName")]
+        public string SeName { get; set; }
+    }
+
+    public partial class TopicValidator : AbstractValidator<TopicModel>
+    {
+        public TopicValidator(Localizer T)
+        {
+            RuleFor(x => x.SystemName).NotEmpty();
+            RuleFor(x => x.HtmlId)
+                .Must(u => u.IsEmpty() || !u.Any(x => char.IsWhiteSpace(x)))
+                .WithMessage(T("Admin.ContentManagement.Topics.Validation.NoWhiteSpace"));
+
+            RuleFor(x => x.IsPasswordProtected)
+                .Equal(false)
+                .When(x => x.RenderAsWidget)
+                .WithMessage(T("Admin.ContentManagement.Topics.Validation.NoPasswordAllowed"));
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .When(x => x.IsPasswordProtected && !x.RenderAsWidget)
+                .WithMessage(T("Admin.ContentManagement.Topics.Validation.NoEmptyPassword"));
+        }
+    }
+
+    public class TopicMapper : IMapper<Topic, TopicModel>
+    {
+        public async Task MapAsync(Topic from, TopicModel to, dynamic parameters = null)
+        {
+            MiniMapper.Map(from, to);
+            to.SeName = await from.GetActiveSlugAsync(0, true, false);
+            to.WidgetWrapContent = from.WidgetWrapContent ?? true;
+        }
+    }
+}
