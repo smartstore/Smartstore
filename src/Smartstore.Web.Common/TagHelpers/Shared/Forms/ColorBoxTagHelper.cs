dd25c1c42d762ca8aec89a8c2e81282f58e44dcf
Michael Herzog <herzog@smartstore.com>
2021-05-25 14:19:52 +0200

    ColorBoxTagHelper minor refactoring

--- src/Smartstore.Web.Common/TagHelpers/Shared/Forms/ColorBoxTagHelper.cs
+++ src/Smartstore.Web.Common/TagHelpers/Shared/Forms/ColorBoxTagHelper.cs
@@ -1,44 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Smartstore.Web.TagHelpers.Shared
 {
-    [OutputElementHint("div")]
+    [OutputElementHint("input")]
     [HtmlTargetElement("colorbox", TagStructure = TagStructure.WithoutEndTag)]
     public class ColorBoxTagHelper : BaseFormTagHelper
     {
+        const string IdAttributeName = "sm-id";
+        const string NameAttributeName = "sm-name";
+        const string ValueAttributeName = "sm-value";
         const string DefaultColorAttributeName = "sm-default-color";
 
+        /// <summary>
+        /// Specifies the id which will be set when rendering the field.
+        /// </summary>
+        [HtmlAttributeName(IdAttributeName)]
+        public string FieldId { get; set; }
+
+        /// <summary>
+        /// Specifies the name which will be set if the tag doesn't contain asp-for attribute.
+        /// </summary>
+        [HtmlAttributeName(NameAttributeName)]
+        public string FieldName { get; set; }
+
+        /// <summary>
+        /// Specifies the value which will be set when rendering the field.
+        /// </summary>
+        [HtmlAttributeName(ValueAttributeName)]
+        public string FieldValue { get; set; }
+
+        /// <summary>
+        /// Specifies the default color of the property.
+        /// </summary>
         [HtmlAttributeName(DefaultColorAttributeName)]
         public string DefaultColor { get; set; }
 
         protected override void ProcessCore(TagHelperContext context, TagHelperOutput output)
         {
-            // TODO: (mh) (core) This is not how model expressions work. "value" is just the value,
-            // but where is the name/id of the control? You can't just pass null to HtmlHelper.TextBox(),
-            // you need a name. TBD with MC. Also: expecting an attribute named "value" without declaring a public property
-            // for it in this class is really BAD API design!
             string value;
-            if (output.Attributes.TryGetAttribute("value", out var valueAttribute))
+            if (output.Attributes.TryGetAttribute("sm-value", out var valueAttribute))
             {
                 value = valueAttribute.Value.ToString();
             }
             else
             {
-                value = For != null ? For.Model.ToString() : string.Empty;
+                value = For != null ? For.Model.ToString() : FieldValue;
             }
-
+            
             var defaultColor = DefaultColor.EmptyNull();
             var isDefault = value.EqualsNoCase(defaultColor);
+            var htmlAttributes = new Dictionary<string, object>
+            {
+                { "class", "form-control colorval" },
+                { "placeholder", defaultColor }
+            };
+
+            if (FieldId.HasValue())
+            {
+                htmlAttributes.Add("id", FieldId);
+            }
 
             output.TagName = "div";
             output.TagMode = TagMode.StartTagAndEndTag;
             output.AppendCssClass("input-group colorpicker-component sm-colorbox");
             output.Attributes.Add("data-fallback-color", defaultColor);
-
-            output.Content.AppendHtml(HtmlHelper.TextBox(For != null ? For.Name : string.Empty, 
-                isDefault ? string.Empty : value, 
-                new { @class = "form-control colorval", placeholder = defaultColor }));
+            output.Content.AppendHtml(HtmlHelper.TextBox(For != null ? For.Name : FieldName, isDefault ? string.Empty : value, htmlAttributes));
 
             var addon = new TagBuilder("div");
             addon.AddCssClass("input-group-append input-group-addon");
