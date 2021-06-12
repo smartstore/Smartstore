a4a5317d69d2c6520275ff490e68fef6768d8d79
Murat Cakir <muratc@smartstore.de>
2021-06-12 02:08:17 +0200

    Export review

--- src/Smartstore.Web/Areas/Admin/Controllers/_ExportController.cs
+++ src/Smartstore.Web/Areas/Admin/Controllers/_ExportController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Dasync.Collections;
 using Humanizer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -338,7 +337,7 @@ namespace Smartstore.Admin.Controllers
 
                 return deploymentModel;
             })
-            .ToListAsync();
+            .AsyncToList();
 
             //...
         }
