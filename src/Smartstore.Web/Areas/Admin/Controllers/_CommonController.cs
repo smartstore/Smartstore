14a17cac0e9f7aae2545260db4769990d2b80519
Michael Herzog <herzog@smartstore.com>
2021-06-24 11:40:51 +0200

    RestartApplication, ClearCache, ClearDatabaseCache

--- src/Smartstore.Web/Areas/Admin/Controllers/_CommonController.cs
+++ src/Smartstore.Web/Areas/Admin/Controllers/_CommonController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Smartstore.Caching;
 using Smartstore.Core.Data;
+using Smartstore.Core.Security;
+using Smartstore.Data.Caching;
 using Smartstore.Web.Controllers;
 using System.Threading.Tasks;
 
@@ -8,10 +11,15 @@ namespace Smartstore.Web.Areas.Admin.Controllers
     public class CommonController : AdminControllerBase
     {
         private readonly SmartDbContext _db;
+        private readonly IRequestCache _requestCache;
+        private readonly IDbCache _dbCache;
 
-        public CommonController(SmartDbContext db)
+        // TODO: (mh) (core) dbCache cannot be resolved. Breaks...
+        public CommonController(SmartDbContext db, IRequestCache requestCache/*, IDbCache dbCache*/)
         {
             _db = db;
+            _requestCache = requestCache;
+            //_dbCache = dbCache;
         }
 
         public async Task<IActionResult> LanguageSelected(int customerlanguage)
@@ -23,6 +31,51 @@ namespace Smartstore.Web.Areas.Admin.Controllers
             }
 
             return Content(T("Admin.Common.DataEditSuccess"));
+        }
+
+        [Permission(Permissions.System.Maintenance.Execute)]
+        [HttpPost]
+        public IActionResult RestartApplication()
+        {
+            // TODO: (mh) (core) This must be tested in production environment. In VS _hostApplicationLifetime.StopApplication() just stops without restarting on next request.
+            Services.WebHelper.RestartAppDomain();
+
+            return new JsonResult(null);
+        }
+
+        [Permission(Permissions.System.Maintenance.Execute)]
+        [HttpPost]
+        public IActionResult ClearCache()
+        {
+            Services.Cache.Clear();
+
+            _requestCache.RemoveByPattern("*");
+
+            return new JsonResult
+            (
+                new
+                {
+                    Success = true,
+                    Message = T("Admin.Common.TaskSuccessfullyProcessed").Value
+                }
+            );
+        }
+
+        [Permission(Permissions.System.Maintenance.Execute)]
+        [HttpPost]
+        public ActionResult ClearDatabaseCache()
+        {
+            // TODO: (mh) (core) Uncomment when dbCache can be resolved.
+            //_dbCache.Clear();
+
+            return new JsonResult
+            (
+                new
+                {
+                    Success = true,
+                    Message = T("Admin.Common.TaskSuccessfullyProcessed").Value
+                }
+            );
         }
     }
 }
