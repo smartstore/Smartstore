c02b12290a910e8fdc80fb42903eca9eb851cf10
Murat Cakir <muratc@smartstore.de>
2021-06-24 04:13:47 +0200

    Bundling

---
+++ src/Smartstore.Web.Common/Assets/Caching/IAssetCache.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Smartstore.Web.Assets
+{
+    /// <summary>
+    /// A file system based caching mechanism for dynamically translated assets like Sass etc.
+    /// </summary>
+    public interface IAssetCache
+    {
+        Task<CachedAssetEntry> GetAssetAsync(string route);
+
+        Task<CachedAssetEntry> InsertAssetAsync(string route, IEnumerable<string> includedFiles, string content, params string[] processorCodes);
+
+        Task<bool> InvalidateAssetAsync(string route);
+
+        Task ClearAsync();
+    }
+}
