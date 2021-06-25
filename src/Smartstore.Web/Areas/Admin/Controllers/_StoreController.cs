d8f6a97676e6d774c174f0092e38b1d52a5f54e4
Michael Herzog <herzog@smartstore.com>
2021-06-25 12:50:55 +0200

    StoreDashboardReport

--- src/Smartstore.Web/Areas/Admin/Controllers/_StoreController.cs
+++ src/Smartstore.Web/Areas/Admin/Controllers/_StoreController.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 using Dasync.Collections;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Smartstore.Admin.Models.Store;
+using Smartstore.Core.Checkout.Cart;
 using Smartstore.Core.Data;
+using Smartstore.Core.Identity;
+using Smartstore.Core.Security;
 using Smartstore.Core.Stores;
 using Smartstore.Web.Controllers;
 using Smartstore.Web.Modelling;
@@ -52,6 +56,42 @@ namespace Smartstore.Admin.Controllers
                 };
 
             return new JsonResult(list.ToList());
+        }
+
+        [Permission(Permissions.Configuration.Store.ReadStats, false)]
+        public async Task<JsonResult> StoreDashboardReportAsync()
+        {
+            var allOrders = await _db.Orders.AsNoTracking().ToListAsync();
+            var registeredRole = await _db.CustomerRoles
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.SystemName == SystemCustomerRoleNames.Registered);
+
+            var registeredCustomers = await _db.Customers
+                .AsNoTracking()
+                .ApplyRolesFilter(new[] { registeredRole.Id })
+                .ToListAsync();
+
+            var sumAllOrders = allOrders.Sum(x => (decimal?)x.OrderTotal) ?? 0;
+            var sumOpenCarts = await _db.ShoppingCartItems.GetOpenCartTypeSubTotalAsync(ShoppingCartType.ShoppingCart);
+            var sumWishlists = await _db.ShoppingCartItems.GetOpenCartTypeSubTotalAsync(ShoppingCartType.Wishlist);
+
+            var model = new StoreDashboardReportModel
+            {
+                ProductsCount = (await _db.Products.CountAsync()).ToString("N0"),
+                CategoriesCount = (await _db.Categories.CountAsync()).ToString("N0"),
+                ManufacturersCount = (await _db.Manufacturers.CountAsync()).ToString("N0"),
+                AttributesCount = (await _db.ProductAttributes.CountAsync()).ToString("N0"),
+                AttributeCombinationsCount = (await _db.ProductVariantAttributeCombinations.CountAsync()).ToString("N0"),
+                MediaCount = (await _db.MediaFiles.CountAsync()).ToString("N0"),
+                CustomersCount = registeredCustomers.Count.ToString("N0"), 
+                OrdersCount = allOrders.Count.ToString("N0"),
+                Sales = Services.WorkContext.WorkingCurrency.AsMoney(sumAllOrders).ToString(),
+                OnlineCustomersCount = (await _db.Customers.ApplyOnlineCustomersFilter(15).CountAsync()).ToString("N0"),
+                CartsValue = Services.WorkContext.WorkingCurrency.AsMoney(sumOpenCarts).ToString(),
+                WishlistsValue = Services.WorkContext.WorkingCurrency.AsMoney(sumWishlists).ToString()
+            };
+
+            return new JsonResult(new { model });
         }
     }
 }
