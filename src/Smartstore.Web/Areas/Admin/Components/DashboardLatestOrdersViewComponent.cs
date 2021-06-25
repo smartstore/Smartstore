363a6caf246688eae90a7c0988f8667fd1f40c11
Michael Herzog <herzog@smartstore.com>
2021-06-25 17:42:31 +0200

    DashboardLatestOrders

---
+++ src/Smartstore.Web/Areas/Admin/Components/DashboardLatestOrdersViewComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Smartstore.Admin.Models.Orders;
+using Smartstore.Core.Common.Services;
+using Smartstore.Core.Data;
+using Smartstore.Web.Components;
+
+namespace Smartstore.Admin.Components
+{
+    public class DashboardLatestOrdersViewComponent : SmartViewComponent
+    {
+        private readonly SmartDbContext _db;
+        private readonly IDateTimeHelper _dateTimeHelper;
+
+        public DashboardLatestOrdersViewComponent(SmartDbContext db, IDateTimeHelper dateTimeHelper)
+        {
+            _db = db;
+            _dateTimeHelper = dateTimeHelper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = new DashboardLatestOrdersModel();
+            var latestOrders = await _db.Orders.ApplyPaging(0, 7).ToListAsync();
+            
+            foreach (var order in latestOrders)
+            {
+                model.LatestOrders.Add(
+                    new DashboardOrderModel(
+                        order.CustomerId,
+                        order.Customer.FindEmail() ?? order.Customer.FormatUserName(),
+                        order.OrderItems.Sum(x => x.Quantity),
+                        Services.WorkContext.WorkingCurrency.AsMoney(order.OrderTotal).ToString(),
+                        _dateTimeHelper.ConvertToUserTime(order.CreatedOnUtc, DateTimeKind.Utc).ToString("g"),
+                        order.OrderStatus,
+                        order.Id)
+                    );
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Smartstore.Web/Areas/Admin/Models/Orders/DashboardLatestOrdersModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Smartstore.Core.Checkout.Orders;
+
+namespace Smartstore.Admin.Models.Orders
+{
+    public class DashboardLatestOrdersModel
+    {
+        public IList<DashboardOrderModel> LatestOrders { get; set; } = new List<DashboardOrderModel>();
+    }
+
+    public class DashboardOrderModel
+    {
+        public DashboardOrderModel(
+            int customerId,
+            string customerDisplayName,
+            int productsTotal,
+            string totalAmount,
+            string created,
+            OrderStatus orderState,
+            int orderId)
+        {
+            CustomerId = customerId;
+            CustomerDisplayName = customerDisplayName;
+            ProductsTotal = productsTotal;
+            TotalAmount = totalAmount;
+            Created = created;
+            OrderState = orderState;
+            OrderId = orderId;
+        }
+
+        public int CustomerId { get; set; }
+        public string CustomerDisplayName { get; set; }
+        public int ProductsTotal { get; set; }
+        public string TotalAmount { get; set; }
+        public string Created { get; set; }
+        public OrderStatus OrderState { get; set; }
+        public int OrderId { get; set; }
+    }
+}
