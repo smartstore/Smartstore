0959eb95802069e5c092a6678a1c7b876fc5b76a
Michael Herzog <herzog@smartstore.com>
2021-07-01 12:23:28 +0200

    Stores

--- src/Smartstore.Web/Areas/Admin/Controllers/_StoreController.cs
+++ src/Smartstore.Web/Areas/Admin/Controllers/_StoreController.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
-using Dasync.Collections;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Smartstore.Admin.Models.Store;
+using Smartstore.Admin.Models.Stores;
+using Smartstore.ComponentModel;
 using Smartstore.Core.Catalog.Search;
 using Smartstore.Core.Checkout.Cart;
-using Smartstore.Core.Common.Services;
 using Smartstore.Core.Content.Media;
 using Smartstore.Core.Data;
 using Smartstore.Core.Identity;
+using Smartstore.Core.Localization;
 using Smartstore.Core.Security;
 using Smartstore.Core.Stores;
+using Smartstore.Data.Batching;
 using Smartstore.Web.Controllers;
 using Smartstore.Web.Modelling;
+using Smartstore.Web.Modelling.DataGrid;
 
 namespace Smartstore.Admin.Controllers
 {
@@ -31,9 +35,168 @@ namespace Smartstore.Admin.Controllers
             _catalogSearchService = catalogSearchService;
         }
 
+        private async Task PrepareStoreModelAsync(StoreModel model, Store store)
+        {
+            var currencies = await _db.Currencies
+                .AsNoTracking()
+                .ApplyStandardFilter(false, store == null ? 0 : store.Id)
+                .ToListAsync();
+
+            model.AvailableCurrencies = currencies
+                .Select(x => new SelectListItem
+                {
+                    Text = x.GetLocalized(y => y.Name),
+                    Value = x.Id.ToString()
+                })
+                .ToList();
+        }
+
         public IActionResult Index()
         {
             return RedirectToAction("List");
+        }
+
+        [Permission(Permissions.Configuration.Store.Read)]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Permission(Permissions.Configuration.Store.Read)]
+        public async Task<IActionResult> StoreList(GridCommand command, StoreModel model)
+        {
+            var storeModels = await Services.StoreContext.GetAllStores()
+                .SelectAsync(async x =>
+                {
+                    var model = await MapperFactory.MapAsync<Store, StoreModel>(x);
+
+                    await PrepareStoreModelAsync(model, x);
+
+                    model.Hosts = model.Hosts.EmptyNull().Replace(",", "<br />");
+                    model.ViewUrl = Url.Action("Edit", "Store", new { id = x.Id });
+
+                    return model;
+                })
+                .AsyncToList();
+
+            var stores = await storeModels.ToPagedList(command.Page - 1, command.PageSize).LoadAsync();
+            var gridModel = new GridModel<StoreModel>
+            {
+                Rows = storeModels,
+                Total = stores.TotalCount
+            };
+
+            return Json(gridModel);
+        }
+
+        [Permission(Permissions.Configuration.Store.Create)]
+        public async Task<IActionResult> Create()
+        {
+            var model = new StoreModel();
+            await PrepareStoreModelAsync(model, null);
+
+            return View(model);
+        }
+
+        [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [Permission(Permissions.Configuration.Store.Create)]
+        public async Task<IActionResult> Create(StoreModel model, bool continueEditing)
+        {
+            if (ModelState.IsValid)
+            {
+                var store = await MapperFactory.MapAsync<StoreModel, Store>(model);
+
+                // Ensure we have "/" at the end.
+                store.Url = store.Url.EnsureEndsWith("/");
+                _db.Stores.Add(store);
+                await _db.SaveChangesAsync();
+
+                NotifySuccess(T("Admin.Configuration.Stores.Added"));
+                return continueEditing ? RedirectToAction("Edit", new { id = store.Id }) : RedirectToAction("List");
+            }
+
+            await PrepareStoreModelAsync(model, null);
+            return View(model);
+        }
+
+        [Permission(Permissions.Configuration.Store.Read)]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var store = Services.StoreContext.GetStoreById(id);
+            if (store == null)
+            {
+                return RedirectToAction("List");
+            }
+
+            var model = await MapperFactory.MapAsync<Store, StoreModel>(store);
+            await PrepareStoreModelAsync(model, store);
+
+            return View(model);
+        }
+
+        [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [FormValueRequired("save", "save-continue")]
+        [Permission(Permissions.Configuration.Store.Update)]
+        public async Task<IActionResult> Edit(StoreModel model, bool continueEditing)
+        {
+            var store = Services.StoreContext.GetStoreById(model.Id);
+            if (store == null)
+            {
+                return RedirectToAction("List");
+            }
+
+            if (ModelState.IsValid)
+            {
+                store = await MapperFactory.MapAsync<StoreModel, Store>(model);
+
+                // Ensure we have "/" at the end.
+                store.Url = store.Url.EnsureEndsWith("/");
+                _db.Stores.Update(store);
+                await _db.SaveChangesAsync();
+                
+                NotifySuccess(T("Admin.Configuration.Stores.Updated"));
+                return continueEditing ? RedirectToAction("Edit", new { id = store.Id }) : RedirectToAction("List");
+            }
+
+            await PrepareStoreModelAsync(model, store);
+            return View(model);
+        }
+
+        [HttpPost]
+        [Permission(Permissions.Configuration.Store.Delete)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var store = Services.StoreContext.GetStoreById(id);
+            if (store == null)
+            {
+                return RedirectToAction("List");
+            }
+
+            try
+            {
+                _db.Stores.Remove(store);
+                await _db.SaveChangesAsync();
+                
+                // When we delete a store we should also ensure that all "per store" settings will also be deleted.
+                await _db.Settings.Where(x => x.StoreId == id).BatchDeleteAsync();
+
+                // When we had two stores and now have only one store, we also should delete all "per store" settings.
+                var allStores = Services.StoreContext.GetAllStores();
+                if (allStores.Count == 1)
+                {
+                    await _db.Settings.Where(x => x.StoreId == allStores.FirstOrDefault().Id).BatchDeleteAsync();
+                }
+
+                NotifySuccess(T("Admin.Configuration.Stores.Deleted"));
+                return RedirectToAction("List");
+            }
+            catch (Exception ex)
+            {
+                NotifyError(ex);
+            }
+
+            return RedirectToAction("Edit", new { id = store.Id });
         }
 
         /// <summary>
@@ -67,7 +230,6 @@ namespace Smartstore.Admin.Controllers
         [Permission(Permissions.Configuration.Store.ReadStats, false)]
         public async Task<JsonResult> StoreDashboardReportAsync()
         {
-            // INFO: (mh) (core) ???????????? Really?????!!!!! WTF!!!
             var ordersQuery = _db.Orders.AsNoTracking();
             var registeredRole = await _db.CustomerRoles
                 .AsNoTracking()
---
+++ src/Smartstore.Web/Areas/Admin/Models/Stores/StoreModel.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Smartstore.Core.Localization;
+using Smartstore.Web.Modelling;
+
+namespace Smartstore.Admin.Models.Stores
+{
+    [LocalizedDisplay("Admin.Configuration.Stores.Fields.")]
+    public partial class StoreModel : EntityModelBase
+    {
+        [LocalizedDisplay("*Name")]
+        public string Name { get; set; }
+
+        [LocalizedDisplay("*Url")]
+        public string Url { get; set; }
+
+        [LocalizedDisplay("*SslEnabled")]
+        public virtual bool SslEnabled { get; set; }
+
+        [LocalizedDisplay("*SecureUrl")]
+        public virtual string SecureUrl { get; set; }
+
+        [LocalizedDisplay("*ForceSslForAllPages")]
+        public bool ForceSslForAllPages { get; set; }
+
+        [LocalizedDisplay("*Hosts")]
+        public string Hosts { get; set; }
+
+        [LocalizedDisplay("*StoreLogo")]
+        [UIHint("Media")]
+        [AdditionalMetadata("album", "content")]
+        [AdditionalMetadata("transientUpload", true)]
+        public int LogoMediaFileId { get; set; }
+
+        [LocalizedDisplay("*FavIconMediaFileId")]
+        [UIHint("Media")]
+        [AdditionalMetadata("album", "content")]
+        [AdditionalMetadata("typeFilter", ".ico")]
+        [AdditionalMetadata("transientUpload", true)]
+        public int? FavIconMediaFileId { get; set; }
+
+        [LocalizedDisplay("*PngIconMediaFileId")]
+        [UIHint("Media")]
+        [AdditionalMetadata("album", "content")]
+        [AdditionalMetadata("typeFilter", ".png")]
+        [AdditionalMetadata("transientUpload", true)]
+        public int? PngIconMediaFileId { get; set; }
+
+        [LocalizedDisplay("*AppleTouchIconMediaFileId")]
+        [UIHint("Media")]
+        [AdditionalMetadata("album", "content")]
+        [AdditionalMetadata("typeFilter", "image")]
+        [AdditionalMetadata("transientUpload", true)]
+        public int? AppleTouchIconMediaFileId { get; set; }
+
+        [LocalizedDisplay("*MsTileImageMediaFileId")]
+        [UIHint("Media")]
+        [AdditionalMetadata("album", "content")]
+        [AdditionalMetadata("typeFilter", "image")]
+        [AdditionalMetadata("transientUpload", true)]
+        public int? MsTileImageMediaFileId { get; set; }
+
+        [LocalizedDisplay("*MsTileColor")]
+        [UIHint("Color")]
+        public string MsTileColor { get; set; }
+
+
+        [LocalizedDisplay("Common.DisplayOrder")]
+        public int DisplayOrder { get; set; }
+
+        [LocalizedDisplay("*HtmlBodyId")]
+        public string HtmlBodyId { get; set; }
+
+        [LocalizedDisplay("*ContentDeliveryNetwork")]
+        public string ContentDeliveryNetwork { get; set; }
+
+        [LocalizedDisplay("*PrimaryStoreCurrencyId")]
+        public int PrimaryStoreCurrencyId { get; set; }
+
+        [LocalizedDisplay("*PrimaryStoreCurrencyId")]
+        public string PrimaryStoreCurrencyName
+        {
+            get
+            {
+                try
+                {
+                    return AvailableCurrencies.First(x => x.Value == PrimaryStoreCurrencyId.ToString()).Text;
+                }
+                catch { }
+
+                return null;
+            }
+        }
+
+        [LocalizedDisplay("*PrimaryExchangeRateCurrencyId")]
+        public int PrimaryExchangeRateCurrencyId { get; set; }
+
+        [LocalizedDisplay("*PrimaryExchangeRateCurrencyId")]
+        public string PrimaryExchangeRateCurrencyName
+        {
+            get
+            {
+                try
+                {
+                    return AvailableCurrencies.First(x => x.Value == PrimaryExchangeRateCurrencyId.ToString()).Text;
+                }
+                catch { }
+
+                return null;
+            }
+        }
+
+        public List<SelectListItem> AvailableCurrencies { get; set; } = new();
+
+        public string ViewUrl { get; set; }
+    }
+
+    public partial class StoreValidator : AbstractValidator<StoreModel>
+    {
+        public StoreValidator(Localizer T)
+        {
+            RuleFor(x => x.Name).NotEmpty();
+
+            RuleFor(x => x.Url)
+                .Must(x => x.HasValue() && x.IsWebUrl())
+                .WithMessage(T("Admin.Validation.Url"));
+
+            RuleFor(x => x.SecureUrl)
+                .Must(x => x.HasValue() && x.IsWebUrl())
+                .When(x => x.SslEnabled)
+                .WithMessage(T("Admin.Validation.Url"));
+
+            RuleFor(x => x.HtmlBodyId).Matches(@"^([A-Za-z])(\w|\-)*$")
+                .WithMessage(T("Admin.Configuration.Stores.Fields.HtmlBodyId.Validation"));
+        }
+    }
+}
