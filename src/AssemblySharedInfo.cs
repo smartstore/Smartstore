deafbadf8803d6a00977d36e2d8c8fafbd7bbcdd
Murat Cakir <muratc@smartstore.de>
2021-08-06 04:34:36 +0200

    Modularity

---
+++ src/AssemblySharedInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyProduct("Smartstore")]
+[assembly: AssemblyDescription("Smartstore")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Smartstore AG")]
+[assembly: AssemblyCopyright("Copyright © SmartStore AG 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
