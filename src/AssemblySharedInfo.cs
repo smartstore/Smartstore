595635bc87767af0723e5154603aa291c424a76d
Murat Cakir <muratc@smartstore.de>
2021-08-13 00:08:45 +0200

    Modularity: Build

--- src/AssemblySharedInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyProduct("Smartstore")]
-[assembly: AssemblyDescription("Smartstore")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Smartstore AG")]
-[assembly: AssemblyCopyright("Copyright © SmartStore AG 2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
--- src/AssemblyVersionInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0.0")]
