a1516e58d632a83a8f1e8ed20dd5dc0ad1356271
Michael Herzog <herzog@smartstore.com>
2021-08-18 14:52:20 +0200

    DeliveryTimes

--- src/Smartstore.Web/Areas/Admin/Controllers/_DeliveryTimeController.cs
+++ src/Smartstore.Web/Areas/Admin/Controllers/_DeliveryTimeController.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 using Dasync.Collections;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Smartstore.Admin.Models.Directory;
+using Smartstore.ComponentModel;
+using Smartstore.Core.Common;
 using Smartstore.Core.Common.Services;
 using Smartstore.Core.Data;
 using Smartstore.Core.Localization;
+using Smartstore.Core.Security;
 using Smartstore.Web.Controllers;
 using Smartstore.Web.Modelling;
+using Smartstore.Web.Modelling.DataGrid;
 
 namespace Smartstore.Admin.Controllers
 {
@@ -18,13 +23,16 @@ namespace Smartstore.Admin.Controllers
     {
         private readonly SmartDbContext _db;
         private readonly IDeliveryTimeService _deliveryTimeService;
-        
+        private readonly ILocalizedEntityService _localizedEntityService;
+
         public DeliveryTimeController(
             SmartDbContext db,
-            IDeliveryTimeService deliveryTimeService)
+            IDeliveryTimeService deliveryTimeService,
+            ILocalizedEntityService localizedEntityService)
         {
             _db = db;
             _deliveryTimeService = deliveryTimeService;
+            _localizedEntityService = localizedEntityService;
         }
 
         public IActionResult Index()
@@ -57,6 +65,168 @@ namespace Smartstore.Admin.Controllers
                 .ToList();
 
             return new JsonResult(data);
+        }
+
+        [Permission(Permissions.Configuration.DeliveryTime.Read)]
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Permission(Permissions.Configuration.DeliveryTime.Read)]
+        public async Task<IActionResult> DeliveryTimeList(GridCommand command, DeliveryTimeModel model)
+        {
+            var deliveryTimeModels = await _db.DeliveryTimes
+                .ApplyGridCommand(command)
+                .SelectAsync(async x =>
+                {
+                    var model = await MapperFactory.MapAsync<DeliveryTime, DeliveryTimeModel>(x);
+                    model.DeliveryInfo = _deliveryTimeService.GetFormattedDeliveryDate(x);
+                    return model;
+                })
+                .AsyncToList();
+
+            var deliveryTimes = await deliveryTimeModels
+                .ToPagedList(command.Page - 1, command.PageSize)
+                .LoadAsync();
+
+            var gridModel = new GridModel<DeliveryTimeModel>
+            {
+                Rows = deliveryTimeModels,
+                Total = deliveryTimes.TotalCount
+            };
+
+            return Json(gridModel);
+        }
+
+        [Permission(Permissions.Configuration.DeliveryTime.Create)]
+        public IActionResult CreateDeliveryTimePopup(string btnId, string formId)
+        {
+            var model = new DeliveryTimeModel();
+            AddLocales(model.Locales);
+
+            ViewBag.BtnId = btnId;
+            ViewBag.FormId = formId;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Permission(Permissions.Configuration.DeliveryTime.Create)]
+        public async Task<IActionResult> CreateDeliveryTimePopup(DeliveryTimeModel model, string btnId, string formId)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var deliveryTime = await MapperFactory.MapAsync<DeliveryTimeModel, DeliveryTime>(model);
+                    _db.DeliveryTimes.Add(deliveryTime);
+                    await _db.SaveChangesAsync();
+
+                    await UpdateLocalesAsync(deliveryTime, model);
+
+                    NotifySuccess(T("Admin.Configuration.DeliveryTime.Added"));
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                    return View(model);
+                }
+
+                ViewBag.RefreshPage = true;
+                ViewBag.BtnId = btnId;
+                ViewBag.FormId = formId;
+            }
+
+            return View(model);
+        }
+
+        [Permission(Permissions.Configuration.DeliveryTime.Read)]
+        public async Task<IActionResult> EditDeliveryTimePopup(int id, string btnId, string formId)
+        {
+            var deliveryTime = _db.DeliveryTimes.FindById(id, false);
+            if (deliveryTime == null)
+            {
+                return NotFound();
+            }
+
+            var model = await MapperFactory.MapAsync<DeliveryTime, DeliveryTimeModel>(deliveryTime);
+
+            AddLocales(model.Locales, (locale, languageId) =>
+            {
+                locale.Name = deliveryTime.GetLocalized(x => x.Name, languageId, false, false);
+            });
+
+            ViewBag.BtnId = btnId;
+            ViewBag.FormId = formId;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Permission(Permissions.Configuration.DeliveryTime.Update)]
+        public async Task<IActionResult> EditDeliveryTimePopup(DeliveryTimeModel model, string btnId, string formId)
+        {
+            var deliveryTime = _db.DeliveryTimes.FindById(model.Id);
+            if (deliveryTime == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await MapperFactory.MapAsync(model, deliveryTime);
+                    await _db.SaveChangesAsync();
+                    await UpdateLocalesAsync(deliveryTime, model);
+
+                    NotifySuccess(T("Admin.Configuration.DeliveryTimes.Updated"));
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                    return View(model);
+                }
+
+                ViewBag.RefreshPage = true;
+                ViewBag.BtnId = btnId;
+                ViewBag.FormId = formId;
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Permission(Permissions.System.Log.Delete)]
+        public async Task<IActionResult> Delete(GridSelection selection)
+        {
+            var success = false;
+            var numDeleted = 0;
+            var ids = selection.GetEntityIds();
+
+            if (ids.Any())
+            {
+                var deliveryTimes = await _db.DeliveryTimes.GetManyAsync(ids, true);
+
+                _db.DeliveryTimes.RemoveRange(deliveryTimes);
+
+                numDeleted = await _db.SaveChangesAsync();
+                success = true;
+            }
+
+            return Json(new { Success = success, Count = numDeleted });
+        }
+
+        [NonAction]
+        private async Task UpdateLocalesAsync(DeliveryTime deliveryTime, DeliveryTimeModel model)
+        {
+            foreach (var localized in model.Locales)
+            {
+                await _localizedEntityService.ApplyLocalizedValueAsync(deliveryTime, x => x.Name, localized.Name, localized.LanguageId);
+            }
         }
     }
 }
---
+++ src/Smartstore.Web/Areas/Admin/Models/Directory/DeliveryTimeModel.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using FluentValidation;
+using Smartstore.Core.Localization;
+using Smartstore.Web.Modelling;
+
+namespace Smartstore.Admin.Models.Directory
+{
+    public class DeliveryTimeModel : EntityModelBase, ILocalizedModel<DeliveryTimeLocalizedModel>
+    {
+        [LocalizedDisplay("Admin.Configuration.DeliveryTimes.Fields.Name")]
+        public string Name { get; set; }
+        public string DeliveryInfo { get; set; }
+
+        [LocalizedDisplay("Admin.Configuration.DeliveryTimes.Fields.DisplayLocale")]
+        public string DisplayLocale { get; set; }
+
+        [UIHint("Color")]
+        [LocalizedDisplay("Admin.Configuration.DeliveryTimes.Fields.Color")]
+        public string ColorHexValue { get; set; }
+
+        [LocalizedDisplay("Common.DisplayOrder")]
+        public int DisplayOrder { get; set; }
+
+        [LocalizedDisplay("Admin.Configuration.DeliveryTimes.Fields.IsDefault")]
+        public bool IsDefault { get; set; }
+
+        [LocalizedDisplay("Admin.Configuration.DeliveryTimes.Fields.MinDays")]
+        public int? MinDays { get; set; }
+
+        [LocalizedDisplay("Admin.Configuration.DeliveryTimes.Fields.MaxDays")]
+        public int? MaxDays { get; set; }
+
+        public List<DeliveryTimeLocalizedModel> Locales { get; set; } = new();
+    }
+
+    public class DeliveryTimeLocalizedModel : ILocalizedLocaleModel
+    {
+        public int LanguageId { get; set; }
+
+        [LocalizedDisplay("Admin.Configuration.DeliveryTimes.Fields.Name")]
+        public string Name { get; set; }
+    }
+
+    public partial class DeliveryTimeValidator : AbstractValidator<DeliveryTimeModel>
+    {
+        public DeliveryTimeValidator(Localizer T)
+        {
+            RuleFor(x => x.Name).NotEmpty().Length(1, 50);
+            RuleFor(x => x.ColorHexValue).NotEmpty().Length(1, 50);
+
+            RuleFor(x => x.DisplayLocale)
+                .Must(x =>
+                {
+                    try
+                    {
+                        if (string.IsNullOrEmpty(x))
+                            return true;
+
+                        var culture = new CultureInfo(x);
+                        return culture != null;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                })
+                .WithMessage(T("Admin.Configuration.DeliveryTimes.Fields.DisplayLocale.Validation"));
+
+            RuleFor(x => x.MinDays)
+                .GreaterThan(0)
+                .When(x => x.MinDays.HasValue);
+
+            RuleFor(x => x.MaxDays)
+                .GreaterThan(0)
+                .When(x => x.MaxDays.HasValue);
+
+            When(x => x.MinDays.HasValue && x.MaxDays.HasValue, () =>
+            {
+                RuleFor(x => x.MaxDays).GreaterThanOrEqualTo(x => x.MinDays);
+            });
+        }
+    }
+}
