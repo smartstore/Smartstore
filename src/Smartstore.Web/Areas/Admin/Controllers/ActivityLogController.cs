9480c9c19593c212b243f8b47fbcc11188910c1c
Michael Herzog <herzog@smartstore.com>
2021-08-26 15:53:14 +0200

    ActivityLogTypes

---
+++ src/Smartstore.Web/Areas/Admin/Controllers/ActivityLogController.cs
@@ -0,0 +1,87 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Smartstore.Admin.Models.Logging;
+using Smartstore.ComponentModel;
+using Smartstore.Core.Common.Services;
+using Smartstore.Core.Common.Settings;
+using Smartstore.Core.Data;
+using Smartstore.Core.Identity;
+using Smartstore.Core.Logging;
+using Smartstore.Core.Security;
+using Smartstore.Web.Controllers;
+using Smartstore.Web.Modelling.DataGrid;
+
+namespace Smartstore.Admin.Controllers
+{
+    public class ActivityLogController : AdminController
+    {
+        private readonly SmartDbContext _db;
+        private readonly ICustomerService _customerService;
+        private readonly IDateTimeHelper _dateTimeHelper;
+        private readonly AdminAreaSettings _adminAreaSettings;
+
+        public ActivityLogController(
+            SmartDbContext db,
+            ICustomerService customerService,
+            IDateTimeHelper dateTimeHelper,
+            AdminAreaSettings adminAreaSettings)
+        {
+            _db = db;
+            _customerService = customerService;
+            _dateTimeHelper = dateTimeHelper;
+            _adminAreaSettings = adminAreaSettings;
+        }
+
+        [Permission(Permissions.Configuration.ActivityLog.Read)]
+        public IActionResult ListTypes()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Permission(Permissions.Configuration.Measure.Read)]
+        public async Task<IActionResult> ActivityTypesList(GridCommand command)
+        {
+            var activityLogTypes = await _db.ActivityLogTypes
+                .AsNoTracking()
+                .ApplyGridCommand(command)
+                .ToPagedList(command)
+                .LoadAsync();
+
+            var activityLogTypeModels = await activityLogTypes
+                .SelectAsync(async x =>
+                {
+                    return await MapperFactory.MapAsync<ActivityLogType, ActivityLogTypeModel>(x);
+                })
+                .AsyncToList();
+
+            var gridModel = new GridModel<ActivityLogTypeModel>
+            {
+                Rows = activityLogTypeModels,
+                Total = await activityLogTypes.GetTotalCountAsync()
+            };
+
+            return Json(gridModel);
+        }
+
+        [HttpPost]
+        [Permission(Permissions.Configuration.ActivityLog.Update)]
+        public async Task<IActionResult> ActivityTypesUpdate(ActivityLogTypeModel model)
+        {
+            var success = false;
+
+            var activityLogType = await _db.ActivityLogTypes.FindByIdAsync(model.Id);
+            if (activityLogType != null)
+            {
+                activityLogType.Enabled = model.Enabled;
+                await _db.SaveChangesAsync();
+                success = true;
+            }
+
+            return Json(new { success });
+        }
+
+
+    }
+}
---
+++ src/Smartstore.Web/Areas/Admin/Models/Logging/ActivityLogTypeModel.cs
@@ -0,0 +1,14 @@
+﻿using Smartstore.Web.Modelling;
+
+namespace Smartstore.Admin.Models.Logging
+{
+    [LocalizedDisplay("Admin.Configuration.ActivityLog.ActivityLogType.Fields.")]
+    public class ActivityLogTypeModel : EntityModelBase
+    {
+        [LocalizedDisplay("*Name")]
+        public string Name { get; set; }
+
+        [LocalizedDisplay("*Enabled")]
+        public bool Enabled { get; set; }
+    }
+}
