c232459ba996e68966c1c6eeaa83460453b01a12
mgesing <gesing@smartstore.com>
2021-08-30 21:29:03 +0200

    DiscountController

--- src/Smartstore.Web/Areas/Admin/Controllers/_DiscountController.cs
+++ src/Smartstore.Web/Areas/Admin/Controllers/_DiscountController.cs
@@ -6,25 +6,25 @@ using System.Threading.Tasks;
 using Dasync.Collections;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Smartstore.Admin.Models.Discounts;
+using Smartstore.ComponentModel;
 using Smartstore.Core.Catalog.Discounts;
 using Smartstore.Core.Data;
+using Smartstore.Core.Localization;
+using Smartstore.Core.Security;
 using Smartstore.Web.Controllers;
 using Smartstore.Web.Modelling;
+using Smartstore.Web.Modelling.DataGrid;
 
 namespace Smartstore.Admin.Controllers
 {
     public class DiscountController : AdminController
     {
         private readonly SmartDbContext _db;
-        
+
         public DiscountController(SmartDbContext db)
         {
             _db = db;
-        }
-
-        public IActionResult Index()
-        {
-            return RedirectToAction("List");
         }
 
         /// <summary>
@@ -58,6 +58,73 @@ namespace Smartstore.Admin.Controllers
                 .ToList();
 
             return new JsonResult(data);
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        [Permission(Permissions.Promotion.Discount.Read)]
+        public async Task<IActionResult> DiscountList(GridCommand command)
+        {
+            var mapper = MapperFactory.GetMapper<Discount, DiscountModel>();
+            // TODO: (mg) (core) add search for discount list: DiscountType, UsePercentage, RequiresCouponCode
+            var query = _db.Discounts.AsNoTracking();
+
+            var discounts = await query
+                .Include(x => x.RuleSets)
+                .ApplyGridCommand(command, false)
+                .ToPagedList(command)
+                .LoadAsync();
+
+            var rows = await discounts.SelectAsync(async x =>
+            {
+                var model = await mapper.MapAsync(x);
+                model.EditUrl = Url.Action("Edit", "Discount", new { id = x.Id, area = "Admin" });
+                model.NumberOfRules = x.RuleSets.Count;
+                model.DiscountTypeName = await Services.Localization.GetLocalizedEnumAsync(x.DiscountType);
+                model.FormattedDiscountAmount = !x.UsePercentage
+                    ? Services.CurrencyService.PrimaryCurrency.AsMoney(x.DiscountAmount).ToString(true)
+                    : string.Empty;
+
+                return model;
+            })
+            .AsyncToList();
+
+            return Json(new GridModel<DiscountModel>
+            {
+                Rows = rows,
+                Total = discounts.TotalCount
+            });
+        }
+
+
+        private void PrepareDiscountModel(DiscountModel model, Discount discount)
+        {
+            if (discount != null)
+            {
+                var language = Services.WorkContext.WorkingLanguage;
+
+                model.SelectedRuleSetIds = discount.RuleSets.Select(x => x.Id).ToArray();
+
+                model.AppliedToCategories = discount.AppliedToCategories
+                    .Where(x => x != null && !x.Deleted)
+                    .Select(x => new DiscountModel.AppliedToEntityModel { Id = x.Id, Name = x.GetLocalized(y => y.Name, language) })
+                    .ToList();
+
+                model.AppliedToManufacturers = discount.AppliedToManufacturers
+                    .Where(x => x != null && !x.Deleted)
+                    .Select(x => new DiscountModel.AppliedToEntityModel { Id = x.Id, Name = x.GetLocalized(y => y.Name, language) })
+                    .ToList();
+
+                model.AppliedToProducts = discount.AppliedToProducts
+                    .Where(x => x != null && !x.Deleted)
+                    .Select(x => new DiscountModel.AppliedToEntityModel { Id = x.Id, Name = x.GetLocalized(y => y.Name, language) })
+                    .ToList();
+            }
+
+            ViewBag.PrimaryStoreCurrencyCode = Services.StoreContext.CurrentStore.PrimaryStoreCurrency.CurrencyCode;
         }
     }
 }
---
+++ src/Smartstore.Web/Areas/Admin/Models/Discounts/DiscountModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+using Smartstore.Core.Rules;
+using Smartstore.Web.Modelling;
+
+namespace Smartstore.Admin.Models.Discounts
+{
+    [LocalizedDisplay("Admin.Promotions.Discounts.Fields.")]
+    public class DiscountModel : EntityModelBase
+    {
+        [LocalizedDisplay("*Name")]
+        public string Name { get; set; }
+
+        [LocalizedDisplay("*DiscountType")]
+        public int DiscountTypeId { get; set; }
+        public string DiscountTypeName { get; set; }
+
+        [LocalizedDisplay("*UsePercentage")]
+        public bool UsePercentage { get; set; }
+
+        [LocalizedDisplay("*DiscountPercentage")]
+        public decimal DiscountPercentage { get; set; }
+
+        [LocalizedDisplay("*DiscountPercentage")]
+        public string FormattedDiscountPercentage
+        {
+            get
+            {
+                if (UsePercentage)
+                {
+                    return string.Format("{0:0.##}", DiscountPercentage);
+                }
+
+                return string.Empty;
+            }
+        }
+
+        [LocalizedDisplay("*DiscountAmount")]
+        public decimal DiscountAmount { get; set; }
+
+        [LocalizedDisplay("*DiscountAmount")]
+        public string FormattedDiscountAmount { get; set; }
+
+        [LocalizedDisplay("*StartDate")]
+        public DateTime? StartDateUtc { get; set; }
+
+        [LocalizedDisplay("*EndDate")]
+        public DateTime? EndDateUtc { get; set; }
+
+        [LocalizedDisplay("*RequiresCouponCode")]
+        public bool RequiresCouponCode { get; set; }
+
+        [LocalizedDisplay("*CouponCode")]
+        public string CouponCode { get; set; }
+
+        [LocalizedDisplay("*DiscountLimitation")]
+        public int DiscountLimitationId { get; set; }
+
+        [LocalizedDisplay("*LimitationTimes")]
+        public int LimitationTimes { get; set; }
+
+
+        [LocalizedDisplay("*AppliedToCategories")]
+        public IList<AppliedToEntityModel> AppliedToCategories { get; set; }
+
+        [LocalizedDisplay("*AppliedToManufacturers")]
+        public IList<AppliedToEntityModel> AppliedToManufacturers { get; set; }
+
+        [LocalizedDisplay("*AppliedToProducts")]
+        public IList<AppliedToEntityModel> AppliedToProducts { get; set; }
+
+
+        [UIHint("RuleSets")]
+        [AdditionalMetadata("multiple", true)]
+        [AdditionalMetadata("scope", RuleScope.Cart)]
+        [LocalizedDisplay("Admin.Promotions.Discounts.RuleSetRequirements")]
+        public int[] SelectedRuleSetIds { get; set; }
+
+        [LocalizedDisplay("Admin.Rules.NumberOfRules")]
+        public int NumberOfRules { get; set; }
+
+        public string EditUrl { get; set; }
+
+        #region Nested classes
+
+        public class DiscountUsageHistoryModel : EntityModelBase
+        {
+            public int DiscountId { get; set; }
+
+            [LocalizedDisplay("Admin.Promotions.Discounts.History.Order")]
+            public int OrderId { get; set; }
+
+            [LocalizedDisplay("Common.CreatedOn")]
+            public DateTime CreatedOn { get; set; }
+        }
+
+        public class AppliedToEntityModel : EntityModelBase
+        {
+            public string Name { get; set; }
+        }
+
+        #endregion
+    }
+
+    public partial class DiscountValidator : AbstractValidator<DiscountModel>
+    {
+        public DiscountValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
