c26342c1500014d022c6c5a4545e3173e564bbc6
Michael Herzog <herzog@smartstore.com>
2021-09-01 13:51:58 +0200

    Campaign

---
+++ src/Smartstore.Web/Areas/Admin/Controllers/CampaignController.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Smartstore.Admin.Models.Messages;
+using Smartstore.ComponentModel;
+using Smartstore.Core.Data;
+using Smartstore.Core.Messaging;
+using Smartstore.Core.Security;
+using Smartstore.Core.Stores;
+using Smartstore.Web.Controllers;
+using Smartstore.Web.Modelling;
+using Smartstore.Web.Modelling.DataGrid;
+
+namespace Smartstore.Admin.Controllers
+{
+    public class CampaignController : AdminController
+    {
+        private readonly SmartDbContext _db;
+        private readonly ICampaignService _campaignService;
+        private readonly IMessageModelProvider _messageModelProvider;
+        private readonly IStoreMappingService _storeMappingService;
+        private readonly IAclService _aclService;
+
+        public CampaignController(
+            SmartDbContext db,
+            ICampaignService campaignService,
+            IMessageModelProvider messageModelProvider,
+            IStoreMappingService storeMappingService,
+            IAclService aclService)
+        {
+            _db = db;
+            _campaignService = campaignService;
+            _messageModelProvider = messageModelProvider;
+            _storeMappingService = storeMappingService;
+            _aclService = aclService;
+        }
+
+        private async Task PrepareCampaignModelAsync(CampaignModel model, Campaign campaign)
+        {
+            if (campaign != null)
+            {
+                model.CreatedOn = Services.DateTimeHelper.ConvertToUserTime(campaign.CreatedOnUtc, DateTimeKind.Utc);
+                model.SelectedStoreIds = await _storeMappingService.GetAuthorizedStoreIdsAsync(campaign);
+                model.SelectedCustomerRoleIds = await _aclService.GetAuthorizedCustomerRoleIdsAsync(campaign);
+            }
+
+            model.LastModelTree = await _messageModelProvider.GetLastModelTreeAsync(MessageTemplateNames.SystemCampaign);
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        [Permission(Permissions.Promotion.Campaign.Read)]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Permission(Permissions.Configuration.Currency.Read)]
+        public async Task<IActionResult> List(GridCommand command)
+        {
+            var campaigns = await _db.Campaigns
+                .AsNoTracking()
+                .ApplyGridCommand(command)
+                .ToPagedList(command)
+                .LoadAsync();
+
+            var campaignModels = await campaigns
+                .SelectAsync(async x =>
+                {
+                    var model = await MapperFactory.MapAsync<Campaign, CampaignModel>(x);
+                    model.EditUrl = Url.Action(nameof(Edit), "Campaign", new { id = x.Id });
+                    model.CreatedOn = Services.DateTimeHelper.ConvertToUserTime(x.CreatedOnUtc, DateTimeKind.Utc);
+                    return model;
+                })
+                .AsyncToList();
+
+            var gridModel = new GridModel<CampaignModel>
+            {
+                Rows = campaignModels,
+                Total = await campaigns.GetTotalCountAsync()
+            };
+
+            return Json(gridModel);
+        }
+
+        [Permission(Permissions.Promotion.Campaign.Create)]
+        public async Task<IActionResult> Create()
+        {
+            var model = new CampaignModel();
+            await PrepareCampaignModelAsync(model, null);
+
+            return View(model);
+        }
+
+
+        [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [Permission(Permissions.Promotion.Campaign.Create)]
+        public async Task<IActionResult> Create(CampaignModel model, bool continueEditing)
+        {
+            if (ModelState.IsValid)
+            {
+                var campaign = await MapperFactory.MapAsync<CampaignModel, Campaign>(model);
+                campaign.CreatedOnUtc = DateTime.UtcNow;
+                _db.Campaigns.Add(campaign);
+                await _db.SaveChangesAsync();
+
+                await SaveAclMappingsAsync(campaign, model.SelectedCustomerRoleIds);
+                await SaveStoreMappingsAsync(campaign, model.SelectedStoreIds);
+
+                NotifySuccess(T("Admin.Promotions.Campaigns.Added"));
+                return continueEditing ? RedirectToAction("Edit", new { id = campaign.Id }) : RedirectToAction("List");
+            }
+
+            await PrepareCampaignModelAsync(model, null);
+
+            return View(model);
+        }
+
+        [Permission(Permissions.Promotion.Campaign.Read)]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var campaign = await _db.Campaigns.FindByIdAsync(id, false);
+            if (campaign == null)
+            {
+                return NotFound();
+            }
+
+            var model = await MapperFactory.MapAsync<Campaign, CampaignModel>(campaign);
+            await PrepareCampaignModelAsync(model, campaign);
+
+            return View(model);
+        }
+
+        [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [FormValueRequired("save", "save-continue")]
+        [Permission(Permissions.Promotion.Campaign.Update)]
+        public async Task<IActionResult> Edit(CampaignModel model, bool continueEditing)
+        {
+            var campaign = await _db.Campaigns.FindByIdAsync(model.Id, false);
+            if (campaign == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await MapperFactory.MapAsync(model, campaign);
+                await _db.SaveChangesAsync();
+
+                await SaveAclMappingsAsync(campaign, model.SelectedCustomerRoleIds);
+                await SaveStoreMappingsAsync(campaign, model.SelectedStoreIds);
+
+                NotifySuccess(T("Admin.Promotions.Campaigns.Updated"));
+                return continueEditing ? RedirectToAction("Edit", new { id = campaign.Id }) : RedirectToAction("List");
+            }
+
+            await PrepareCampaignModelAsync(model, campaign);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Permission(Permissions.System.Message.Send)]
+        public async Task<IActionResult> SendCampaign(CampaignModel model)
+        {
+            var campaign = await _db.Campaigns.FindByIdAsync(model.Id, false);
+            if (campaign == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                var numberOfQueuedMessages = await _campaignService.SendCampaignAsync(campaign);
+
+                NotifySuccess(T("Admin.Promotions.Campaigns.MassEmailSentToCustomers", numberOfQueuedMessages));
+            }
+            catch (Exception ex)
+            {
+                NotifyError(ex, false);
+            }
+
+            return RedirectToAction("Edit", new { id = model.Id });
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [Permission(Permissions.Configuration.Currency.Delete)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var campaign = await _db.Campaigns.FindByIdAsync(id);
+            if (campaign == null)
+            {
+                return NotFound();
+            }
+
+            _db.Campaigns.Remove(campaign);
+            await _db.SaveChangesAsync();
+
+            NotifySuccess(T("Admin.Promotions.Campaigns.Deleted"));
+            return RedirectToAction("List");
+        }
+
+        [HttpPost]
+        [Permission(Permissions.Configuration.Currency.Delete)]
+        public async Task<IActionResult> DeleteSelection(GridSelection selection)
+        {
+            var success = false;
+            var numDeleted = 0;
+            var ids = selection.GetEntityIds();
+
+            if (ids.Any())
+            {
+                var campaigns = await _db.Campaigns.GetManyAsync(ids, true);
+
+                _db.Campaigns.RemoveRange(campaigns);
+
+                numDeleted = await _db.SaveChangesAsync();
+                success = true;
+            }
+
+            return Json(new { Success = success, Count = numDeleted });
+        }
+    }
+}
---
+++ src/Smartstore.Web/Areas/Admin/Models/Messages/CampaignModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+using Smartstore.Collections;
+using Smartstore.Core.Messaging;
+using Smartstore.Web.Modelling;
+
+namespace Smartstore.Admin.Models.Messages
+{
+    [LocalizedDisplay("Admin.Promotions.Campaigns.Fields.")]
+    public class CampaignModel : EntityModelBase
+    {
+        [LocalizedDisplay("*Name")]
+        public string Name { get; set; }
+
+        [LocalizedDisplay("*Subject")]
+        public string Subject { get; set; }
+
+        [LocalizedDisplay("*Body")]
+        [UIHint("Html")]
+        [AdditionalMetadata("lazy", false)]
+        public string Body { get; set; }
+
+        [LocalizedDisplay("Common.CreatedOn")]
+        public DateTime CreatedOn { get; set; }
+
+        [LocalizedDisplay("*AllowedTokens")]
+        [UIHint("ModelTree")]
+        public TreeNode<ModelTreeMember> LastModelTree { get; set; }
+
+        [LocalizedDisplay("*TestEmail")]
+        public string TestEmail { get; set; }
+
+        [UIHint("CustomerRoles")]
+        [AdditionalMetadata("multiple", true)]
+        [LocalizedDisplay("Admin.Common.CustomerRole.LimitedTo")]
+        public int[] SelectedCustomerRoleIds { get; set; }
+
+        [LocalizedDisplay("Admin.Common.CustomerRole.LimitedTo")]
+        public bool SubjectToAcl { get; set; }
+
+        [UIHint("Stores")]
+        [AdditionalMetadata("multiple", true)]
+        [LocalizedDisplay("Admin.Common.Store.LimitedTo")]
+        public int[] SelectedStoreIds { get; set; }
+
+        [LocalizedDisplay("Admin.Common.Store.LimitedTo")]
+        public bool LimitedToStores { get; set; }
+
+        public string EditUrl { get; set; }
+    }
+
+    public partial class CampaignValidator : AbstractValidator<CampaignModel>
+    {
+        public CampaignValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Subject).NotEmpty();
+            RuleFor(x => x.Body).NotEmpty();
+        }
+    }
+}
