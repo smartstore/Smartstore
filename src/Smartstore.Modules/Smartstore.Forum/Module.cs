3aef6f3d50ccbd1e2d00d2e62e3584a143451895
mgesing <gesing@smartstore.com>
2021-09-06 11:55:24 +0200

    Forum module

---
+++ src/Smartstore.Modules/Smartstore.Forum/Module.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Smartstore.Engine.Modularity;
+using Smartstore.Http;
+
+namespace Smartstore.Forum
+{
+    internal class Module : ModuleBase, IConfigurable
+    {
+        public ILogger Logger { get; set; } = NullLogger.Instance;
+
+        // TODO: (mg) (core) forum configuration route.
+        public RouteInfo GetConfigurationRoute()
+            => new("Configure", "Forum", new { area = "Admin" });
+
+        public override async Task InstallAsync()
+        {
+            await base.InstallAsync();
+            await ImportLanguageResources();
+			
+            Logger.Info($"Plugin installed: SystemName: {Descriptor.SystemName}, Version: {Descriptor.Version}, Description: '{Descriptor.FriendlyName}'");
+        }
+
+        public override async Task UninstallAsync()
+        {
+            await base.UninstallAsync();
+            await DeleteLanguageResources();
+        }
+    }
+}
---
+++ src/Smartstore.Modules/Smartstore.Forum/Permissions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Smartstore.Core.Identity;
+using Smartstore.Core.Security;
+
+namespace Smartstore.Forum
+{
+    internal static class ForumPermissions
+    {
+        public const string Self = "forum";
+    }
+
+    internal class ForumPermissionProvider : IPermissionProvider
+    {
+        public IEnumerable<PermissionRecord> GetPermissions()
+        {
+            // Get all permissions from above static class.
+            var permissionSystemNames = PermissionHelper.GetPermissions(typeof(ForumPermissions));
+            var permissions = permissionSystemNames.Select(x => new PermissionRecord { SystemName = x });
+
+            return permissions;
+        }
+
+        public IEnumerable<DefaultPermissionRecord> GetDefaultPermissions()
+        {
+            // Allow root permission for admin by default.
+            return new[]
+            {
+                new DefaultPermissionRecord
+                {
+                    CustomerRoleSystemName = SystemCustomerRoleNames.Administrators,
+                    PermissionRecords = new[]
+                    {
+                        new PermissionRecord { SystemName = ForumPermissions.Self }
+                    }
+                }
+            };
+        }
+    }
+}
