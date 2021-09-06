13a07bbf1263a63a73bef5c25ce33488145c2c69
Murat Cakir <muratc@smartstore.de>
2021-09-06 23:05:33 +0200

    Cleaning up summernote mess (!!)

--- src/Smartstore.Web/wwwroot/lib/editors/summernote/lang/summernote-fr-FR.js
+++ src/Smartstore.Web/wwwroot/lib/editors/summernote/lang/summernote-fr-FR.js
@@ -1,273 +1,156 @@
-/*!
- * 
- * Super simple wysiwyg editor v0.8.18
- * https://summernote.org
- * 
- * 
- * Copyright 2013- Alan Hong. and other contributors
- * summernote may be freely distributed under the MIT license.
- * 
- * Date: 2020-05-20T18:09Z
- * 
- */
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else {
-		var a = factory();
-		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
-	}
-})(window, function() {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 19);
-/******/ })
-/************************************************************************/
-/******/ ({
-
-/***/ 19:
-/***/ (function(module, exports) {
-
-(function ($) {
-  $.extend($.summernote.lang, {
-    'fr-FR': {
-      font: {
-        bold: 'Gras',
-        italic: 'Italique',
-        underline: 'Souligné',
-        clear: 'Effacer la mise en forme',
-        height: 'Interligne',
-        name: 'Famille de police',
-        strikethrough: 'Barré',
-        superscript: 'Exposant',
-        subscript: 'Indice',
-        size: 'Taille de police'
-      },
-      image: {
-        image: 'Image',
-        insert: 'Insérer une image',
-        resizeFull: 'Taille originale',
-        resizeHalf: 'Redimensionner à 50 %',
-        resizeQuarter: 'Redimensionner à 25 %',
-        floatLeft: 'Aligné à gauche',
-        floatRight: 'Aligné à droite',
-        floatNone: 'Pas d\'alignement',
-        shapeRounded: 'Forme: Rectangle arrondi',
-        shapeCircle: 'Forme: Cercle',
-        shapeThumbnail: 'Forme: Vignette',
-        shapeNone: 'Forme: Aucune',
-        dragImageHere: 'Faites glisser une image ou un texte dans ce cadre',
-        dropImage: 'Lachez l\'image ou le texte',
-        selectFromFiles: 'Choisir un fichier',
-        maximumFileSize: 'Taille de fichier maximale',
-        maximumFileSizeError: 'Taille maximale du fichier dépassée',
-        url: 'URL de l\'image',
-        remove: 'Supprimer l\'image',
-        original: 'Original'
-      },
-      video: {
-        video: 'Vidéo',
-        videoLink: 'Lien vidéo',
-        insert: 'Insérer une vidéo',
-        url: 'URL de la vidéo',
-        providers: '(YouTube, Vimeo, Vine, Instagram, DailyMotion ou Youku)'
-      },
-      link: {
-        link: 'Lien',
-        insert: 'Insérer un lien',
-        unlink: 'Supprimer un lien',
-        edit: 'Modifier',
-        textToDisplay: 'Texte à afficher',
-        url: 'URL du lien',
-        openInNewWindow: 'Ouvrir dans une nouvelle fenêtre'
-      },
-      table: {
-        table: 'Tableau',
-        addRowAbove: 'Ajouter une ligne au-dessus',
-        addRowBelow: 'Ajouter une ligne en dessous',
-        addColLeft: 'Ajouter une colonne à gauche',
-        addColRight: 'Ajouter une colonne à droite',
-        delRow: 'Supprimer la ligne',
-        delCol: 'Supprimer la colonne',
-        delTable: 'Supprimer le tableau'
-      },
-      hr: {
-        insert: 'Insérer une ligne horizontale'
-      },
-      style: {
-        style: 'Style',
-        p: 'Normal',
-        blockquote: 'Citation',
-        pre: 'Code source',
-        h1: 'Titre 1',
-        h2: 'Titre 2',
-        h3: 'Titre 3',
-        h4: 'Titre 4',
-        h5: 'Titre 5',
-        h6: 'Titre 6'
-      },
-      lists: {
-        unordered: 'Liste à puces',
-        ordered: 'Liste numérotée'
-      },
-      options: {
-        help: 'Aide',
-        fullscreen: 'Plein écran',
-        codeview: 'Afficher le code HTML'
-      },
-      paragraph: {
-        paragraph: 'Paragraphe',
-        outdent: 'Diminuer le retrait',
-        indent: 'Augmenter le retrait',
-        left: 'Aligner à gauche',
-        center: 'Centrer',
-        right: 'Aligner à droite',
-        justify: 'Justifier'
-      },
-      color: {
-        recent: 'Dernière couleur sélectionnée',
-        more: 'Plus de couleurs',
-        background: 'Couleur de fond',
-        foreground: 'Couleur de police',
-        transparent: 'Transparent',
-        setTransparent: 'Définir la transparence',
-        reset: 'Restaurer',
-        resetToDefault: 'Restaurer la couleur par défaut'
-      },
-      shortcut: {
-        shortcuts: 'Raccourcis',
-        close: 'Fermer',
-        textFormatting: 'Mise en forme du texte',
-        action: 'Action',
-        paragraphFormatting: 'Mise en forme des paragraphes',
-        documentStyle: 'Style du document',
-        extraKeys: 'Touches supplémentaires'
-      },
-      help: {
-        'insertParagraph': 'Insérer paragraphe',
-        'undo': 'Défaire la dernière commande',
-        'redo': 'Refaire la dernière commande',
-        'tab': 'Tabulation',
-        'untab': 'Tabulation arrière',
-        'bold': 'Mettre en caractère gras',
-        'italic': 'Mettre en italique',
-        'underline': 'Mettre en souligné',
-        'strikethrough': 'Mettre en texte barré',
-        'removeFormat': 'Nettoyer les styles',
-        'justifyLeft': 'Aligner à gauche',
-        'justifyCenter': 'Centrer',
-        'justifyRight': 'Aligner à droite',
-        'justifyFull': 'Justifier à gauche et à droite',
-        'insertUnorderedList': 'Basculer liste à puces',
-        'insertOrderedList': 'Basculer liste ordonnée',
-        'outdent': 'Diminuer le retrait du paragraphe',
-        'indent': 'Augmenter le retrait du paragraphe',
-        'formatPara': 'Changer le paragraphe en cours en normal (P)',
-        'formatH1': 'Changer le paragraphe en cours en entête H1',
-        'formatH2': 'Changer le paragraphe en cours en entête H2',
-        'formatH3': 'Changer le paragraphe en cours en entête H3',
-        'formatH4': 'Changer le paragraphe en cours en entête H4',
-        'formatH5': 'Changer le paragraphe en cours en entête H5',
-        'formatH6': 'Changer le paragraphe en cours en entête H6',
-        'insertHorizontalRule': 'Insérer séparation horizontale',
-        'linkDialog.show': 'Afficher fenêtre d\'hyperlien'
-      },
-      history: {
-        undo: 'Annuler la dernière action',
-        redo: 'Restaurer la dernière action annulée'
-      },
-      specialChar: {
-        specialChar: 'Caractères spéciaux',
-        select: 'Choisir des caractères spéciaux'
-      }
-    }
-  });
-})(jQuery);
-
-/***/ })
-
-/******/ });
-});
+(Funktion($) {
+    $.extend($.summernote.lang, {
+        'fr-FR': {
+            machen: {
+                fett: 'Fett',
+                kursiv: 'kursiv',
+                unterstreichen: 'unterstreichen',
+                clear: 'Formatierung löschen',
+                Höhe: 'Zeilenabstand',
+                Name: 'Schriftfamilie',
+                durchgestrichen: 'Durchgestrichen',
+                hochgestellt: 'Exponent',
+                tiefgestellt: 'tiefgestellt',
+                Größe: 'Schriftgröße',
+            },
+            Bild: {
+                Bild: 'Bild',
+                einfügen: 'Ein Bild einfügen',
+                resizeFull: 'Originalgröße',
+                resizeHalf: 'Größe auf 50% ändern',
+                resizeQuarter: 'Größe auf 25% ändern',
+                floatLeft: 'linksbündig',
+                floatRight: 'Rechts ausgerichtet',
+                floatNone: 'Keine Ausrichtung',
+                shapeRounded: 'Form: Abgerundetes Rechteck',
+                shapeCircle: 'Form: Kreis',
+                shapeThumbnail: 'Form: Vignette',
+                shapeNone: 'Form: Keine',
+                dragImageHere: 'Ziehen Sie ein Bild oder einen Text in diesen Rahmen',
+                dropImage: 'Bild oder Text ablegen',
+                selectFromFiles: 'Datei auswählen',
+                maximumFileSize: 'Maximale Dateigröße',
+                maximumFileSizeError: 'Maximale Dateigröße überschritten',
+                URL: 'Bild-URL',
+                entfernen: 'Bild entfernen',
+                Original: 'Original',
+            },
+            Video: {
+                Video: 'Video',
+                videoLink: 'Videolink',
+                einfügen: 'Video einfügen',
+                URL: 'Video-URL',
+                Anbieter: '(YouTube, Google Drive, Vimeo, Vine, Instagram, DailyMotion oder Youku)',
+            },
+            Verknüpfung: {
+                Link: 'Link',
+                einfügen: 'Link einfügen',
+                unlink: 'Link entfernen',
+                bearbeiten: 'Bearbeiten',
+                textToDisplay: 'Text zum Anzeigen',
+                URL: 'Link-URL',
+                openInNewWindow: 'In einem neuen Fenster öffnen',
+                useProtocol: 'Standardprotokoll verwenden',
+            },
+            Tisch: {
+                Tabelle: 'Tabelle',
+                addRowAbove: 'Zeile oben hinzufügen',
+                addRowBelow: 'Füge unten eine Zeile hinzu',
+                addColLeft: 'Spalte links hinzufügen',
+                addColRight: 'Spalte rechts hinzufügen',
+                delRow: 'Zeile löschen',
+                delCol: 'Spalte löschen',
+                delTable: 'Tabelle löschen',
+            },
+            Uhr: {
+                einfügen: 'Eine horizontale Linie einfügen',
+            },
+            Stil: {
+                Stil: 'Stil',
+                p: 'Normal',
+                blockquote: 'Zitat',
+                vor: 'Quellcode',
+                h1: 'Titel 1',
+                h2: 'Titel 2',
+                h3: 'Titel 3',
+                h4: 'Titel 4',
+                h5: 'Titel 5',
+                h6: 'Titel 6',
+            },
+            Listen: {
+                ungeordnet: 'Aufzählungsliste',
+                geordnet: 'Nummerierte Liste',
+            },
+            Optionen: {
+                Hilfe Hilfe',
+        Vollbild: 'Vollbild',
+                Codeansicht: 'HTML-Code anzeigen',
+            },
+            Absatz: {
+                Absatz: 'Absatz',
+                ausrücken: 'Einrückung verringern',
+                Einzug: 'Einzug vergrößern',
+                links: 'Links ausrichten',
+                Mitte: 'Mitte',
+                rechts: 'Rechts ausrichten',
+                rechtfertigen: 'begründen',
+            },
+            Farbe: {
+                kürzlich: 'Zuletzt ausgewählte Farbe',
+                mehr: 'Mehr Farben',
+                Hintergrund: 'Hintergrundfarbe',
+                Vordergrund: 'Schriftfarbe',
+                transparent: 'Transparent',
+                setTransparent: 'Transparenz einstellen',
+                zurücksetzen: 'Wiederherstellen',
+                resetToDefault: 'Standardfarbe wiederherstellen',
+            },
+            Abkürzung: {
+                Tastenkombinationen: 'Tastenkombinationen',
+                schließen: 'Schließen',
+                textFormatting: 'Textformatierung',
+                Aktion: 'Aktion',
+                AbsatzFormatierung: 'Absätze formatieren',
+                documentStyle: 'Dokumentenstil',
+                extraKeys: 'Zusätzliche Schlüssel',
+            },
+            Hilfe: {
+                'insertParagraph': 'Absatz einfügen',
+                'undo': 'Letzten Befehl rückgängig machen',
+                'redo': 'Letzten Befehl wiederholen',
+                'Tabulator': 'Tabulator',
+                'untab': 'Zurück-Tab',
+                'fett': 'Mache fett',
+                'kursiv': 'kursiv',
+                'unterstreichen': 'unterstreichen',
+                'durchgestrichen': 'Durchgestrichen',
+                'removeFormat': 'Stile bereinigen',
+                'justifyLeft': 'Links ausrichten',
+                'justifyCenter': 'Center',
+                'justifyRight': 'Rechts ausrichten',
+                'justifyFull': 'Rechts und links ausrichten',
+                'insertUnorderedList': 'Aufzählungsliste umschalten',
+                'insertOrderedList': 'Geordnete Liste umschalten',
+                'outdent': 'Absatzeinzug verkleinern',
+                'einzug': 'Absatzeinzug erhöhen',
+                'formatPara': 'Ändere den aktuellen Absatz in normal (P)',
+                'formatH1': 'Ändere den aktuellen Absatz in Kopfzeile H1',
+                'formatH2': 'Ändere den aktuellen Absatz in H2-Kopfzeile',
+                'formatH3': 'Ändere den aktuellen Absatz in H3-Kopfzeile',
+                'formatH4': 'Ändere den aktuellen Absatz in H4-Kopfzeile',
+                'formatH5': 'Ändere den aktuellen Absatz in H5-Kopfzeile',
+                'formatH6': 'Ändere den aktuellen Absatz in Überschrift H6',
+                'insertHorizontalRule': 'Horizontale Trennung einfügen',
+                'linkDialog.show': 'Hyperlinkfenster anzeigen',
+            },
+            Geschichte: {
+                rückgängig machen: 'Letzte Aktion rückgängig machen',
+                redo: 'Zuletzt rückgängig gemachte Aktion wiederherstellen',
+            },
+            Sonderzeichen: {
+                specialChar: 'Sonderzeichen',
+                wählen Sie: 'Sonderzeichen wählen',
+            },
+        },
+    });
+}) (jQuery);
