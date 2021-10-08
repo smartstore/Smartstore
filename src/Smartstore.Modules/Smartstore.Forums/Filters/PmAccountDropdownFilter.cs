001fdf54126dab49a48358a53cc309bcc7eb6b89
Murat Cakir <muratc@smartstore.de>
2021-10-08 03:19:00 +0200

    Cleanup

--- src/Smartstore.Modules/Smartstore.Forums/Filters/PmAccountDropdownFilter.cs
+++ src/Smartstore.Modules/Smartstore.Forums/Filters/PmAccountDropdownFilter.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
 using Smartstore.Core;
 using Smartstore.Core.Data;
 using Smartstore.Core.Identity;
