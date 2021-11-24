f2d9b4a432fcc1f0c9cc7aa0653e441d9aeefcd4
mgesing <gesing@smartstore.com>
2021-11-24 11:19:39 +0100

    Turning off EF's global query filter warning

--- src/Smartstore/Data/Providers/Bootstrapping/DbFactoryDbContextOptionsExtensions.cs
+++ src/Smartstore/Data/Providers/Bootstrapping/DbFactoryDbContextOptionsExtensions.cs
@@ -65,14 +65,19 @@ namespace Smartstore.Data.Providers
                     //.EnableSensitiveDataLogging(true)
                     .ConfigureWarnings(w =>
                     {
-                    // EF throws when query is untracked otherwise
-                    w.Ignore(CoreEventId.DetachedLazyLoadingWarning);
+                        // EF throws when query is untracked otherwise
+                        w.Ignore(CoreEventId.DetachedLazyLoadingWarning);
 
-                    #region Test
-                    //// To identify the query that's triggering MultipleCollectionIncludeWarning.
-                    ////w.Throw(RelationalEventId.MultipleCollectionIncludeWarning);
-                    ////w.Ignore(RelationalEventId.MultipleCollectionIncludeWarning);
-                    #endregion
+                        // Turn off the global query filter warning. We use global query filters only for ISoftDeletable.
+                        // Related entities are not required but we do not want to configure that for each relationship.
+                        // This way we can leave the configuration of the relationship to EF in many cases.
+                        w.Ignore(CoreEventId.PossibleIncorrectRequiredNavigationWithQueryFilterInteractionWarning);
+
+                        #region Test
+                        //// To identify the query that's triggering MultipleCollectionIncludeWarning.
+                        ////w.Throw(RelationalEventId.MultipleCollectionIncludeWarning);
+                        ////w.Ignore(RelationalEventId.MultipleCollectionIncludeWarning);
+                        #endregion
                 });
             }
 
