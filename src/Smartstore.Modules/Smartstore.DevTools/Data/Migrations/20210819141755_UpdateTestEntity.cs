ca63545edee1ba745a6c15164c3b078213080c80
mgesing <gesing@smartstore.com>
2021-11-24 09:37:54 +0100

    Commenting out the DevTools migrations. Intended for demonstration purposes for developers only. Unnecessary to migrate this into a productive database.

--- src/Smartstore.Modules/Smartstore.DevTools/Data/Migrations/20210819141755_UpdateTestEntity.cs
+++ src/Smartstore.Modules/Smartstore.DevTools/Data/Migrations/20210819141755_UpdateTestEntity.cs
@@ -3,35 +3,35 @@ using Smartstore.Core.Data.Migrations;
 
 namespace Smartstore.DevTools.Data.Migrations
 {
-    [MigrationVersion("2021-08-19 14:17:55", "DevTools: update test entity")]
-    internal class UpdateTestEntity : Migration
-    {
-        private const string TABLE_NAME = "DevToolsTestEntity";
+    //[MigrationVersion("2021-08-19 14:17:55", "DevTools: update test entity")]
+    //internal class UpdateTestEntity : Migration
+    //{
+    //    private const string TABLE_NAME = "DevToolsTestEntity";
 
-        public override void Up()
-        {
-            if (!Schema.Table(TABLE_NAME).Column("IsActive").Exists())
-            {
-                Create.Column("IsActive").OnTable(TABLE_NAME).AsBoolean().NotNullable();
-            }
+    //    public override void Up()
+    //    {
+    //        if (!Schema.Table(TABLE_NAME).Column("IsActive").Exists())
+    //        {
+    //            Create.Column("IsActive").OnTable(TABLE_NAME).AsBoolean().NotNullable();
+    //        }
 
-            if (!Schema.Table(TABLE_NAME).Column("Notes").Exists())
-            {
-                Create.Column("Notes").OnTable(TABLE_NAME).AsString(400).Nullable();
-            }
-        }
+    //        if (!Schema.Table(TABLE_NAME).Column("Notes").Exists())
+    //        {
+    //            Create.Column("Notes").OnTable(TABLE_NAME).AsString(400).Nullable();
+    //        }
+    //    }
 
-        public override void Down()
-        {
-            if (Schema.Table(TABLE_NAME).Column("IsActive").Exists())
-            {
-                Delete.Column("IsActive");
-            }
+    //    public override void Down()
+    //    {
+    //        if (Schema.Table(TABLE_NAME).Column("IsActive").Exists())
+    //        {
+    //            Delete.Column("IsActive");
+    //        }
 
-            if (Schema.Table(TABLE_NAME).Column("Notes").Exists())
-            {
-                Delete.Column("Notes");
-            }
-        }
-    }
+    //        if (Schema.Table(TABLE_NAME).Column("Notes").Exists())
+    //        {
+    //            Delete.Column("Notes");
+    //        }
+    //    }
+    //}
 }
