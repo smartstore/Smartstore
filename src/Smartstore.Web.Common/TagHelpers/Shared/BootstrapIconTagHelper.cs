497b848709637b781c9534d9d849ab3549d56981
Murat Cakir <muratc@smartstore.de>
2022-02-18 01:20:30 +0100

    BootstrapIconTagHelper (wip)

--- src/Smartstore.Web.Common/TagHelpers/Shared/BootstrapIconTagHelper.cs
+++ src/Smartstore.Web.Common/TagHelpers/Shared/BootstrapIconTagHelper.cs
@@ -4,15 +4,37 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Smartstore.Web.TagHelpers.Shared
 {
-    [OutputElementHint("svg")]
     [HtmlTargetElement("bootstrap-icon", Attributes = NameAttributeName, TagStructure = TagStructure.WithoutEndTag)]
     public class BootstrapIconTagHelper : TagHelper
     {
         const string NameAttributeName = "name";
+        const string ClassAttributeName = "class";
+        const string StyleAttributeName = "style";
+        const string IdAttributeName = "id";
+        const string FontScaleAttributeName = "font-scale";
+        const string FillAttributeName = "fill";
 
         [HtmlAttributeNotBound]
         [ViewContext]
         public ViewContext ViewContext { get; set; }
+
+        /// <summary>
+        /// CSS class.
+        /// </summary>
+        [HtmlAttributeName(ClassAttributeName)]
+        public string CssClass { get; set; }
+
+        /// <summary>
+        /// CSS style.
+        /// </summary>
+        [HtmlAttributeName(StyleAttributeName)]
+        public string Style { get; set; }
+
+        /// <summary>
+        /// HTML id.
+        /// </summary>
+        [HtmlAttributeName(IdAttributeName)]
+        public string Id { get; set; }
 
         /// <summary>
         /// The name of the Bootstrap icon to use.
@@ -20,17 +42,60 @@ namespace Smartstore.Web.TagHelpers.Shared
         [HtmlAttributeName(NameAttributeName)]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Scales the icons current font size. e.g.: 2.5 --> 250% font-size.
+        /// </summary>
+        [HtmlAttributeName(FontScaleAttributeName)]
+        public float? FontScale { get; set; }
+
+        /// <summary>
+        /// Icon color. Default: currentColor (inherits CSS <c>color</c>).
+        /// </summary>
+        [HtmlAttributeName(FillAttributeName)]
+        public string Fill { get; set; } = "currentColor";
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "svg";
             output.TagMode = TagMode.StartTagAndEndTag;
 
             output.AppendCssClass("bi");
-            output.MergeAttribute("fill", "currentColor");
+
+            ApplyRootAttributes(output);
 
             var urlHelper = ViewContext.HttpContext.RequestServices.GetService<IUrlHelper>();
-            var url = urlHelper.Content($"~/lib/bi/bootstrap-icons.svg#{Name}");
-            output.Content.AppendHtml($"<use xlink:href=\"{url}\" />");
+            var symbol = new TagBuilder("use");
+            symbol.Attributes["xlink:href"] = urlHelper.Content($"~/lib/bi/bootstrap-icons.svg#{Name}");
+
+            output.Content.AppendHtml(symbol);
+        }
+
+        protected virtual void ApplyRootAttributes(TagHelperOutput output)
+        {
+            if (Id.HasValue())
+            {
+                output.MergeAttribute("id", Id);
+            }
+
+            if (CssClass.HasValue())
+            {
+                output.AppendCssClass(CssClass);
+            }
+
+            if (Style.HasValue())
+            {
+                output.MergeAttribute("style", Style);
+            }
+
+            if (Fill.HasValue())
+            {
+                output.MergeAttribute("fill", Fill);
+            }
+
+            if (FontScale > 0)
+            {
+                output.AddCssStyle("font-size", $"{FontScale.Value * 100}%");
+            }
         }
     }
 }
