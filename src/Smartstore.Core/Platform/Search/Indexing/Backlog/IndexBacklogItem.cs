b20927f813efaa2f50e9843c50bb9b9f00c68815
mgesing <gesing@smartstore.com>
2022-05-24 16:20:27 +0200

    Minor improvement

--- src/Smartstore.Core/Platform/Search/Indexing/Backlog/IndexBacklogItem.cs
+++ src/Smartstore.Core/Platform/Search/Indexing/Backlog/IndexBacklogItem.cs
@@ -2,10 +2,21 @@
 {
     public class IndexBacklogItem
     {
-        public string Scope { get; set; }
-        public int EntityId { get; set; }
-        public IndexOperationType OperationType { get; set; }
-        public DateTime CreatedOnUtc { get; set; }
+        public IndexBacklogItem(string scope, int entityId, IndexOperationType operationType = IndexOperationType.Index)
+        {
+            Guard.NotEmpty(scope, nameof(scope));
+            Guard.NotZero(entityId, nameof(entityId));
+    
+            Scope = scope;
+            EntityId = entityId;
+            OperationType = operationType;
+            CreatedOnUtc = DateTime.UtcNow;
+        }
+
+        public string Scope { get; }
+        public int EntityId { get; }
+        public IndexOperationType OperationType { get; }
+        public DateTime CreatedOnUtc { get; init; }
 
         public override string ToString()
             => $"IndexBacklogItem (Scope: {Scope}, EntityId: {EntityId}, Operation: {OperationType}, CreatedOnUtc: {CreatedOnUtc})";
