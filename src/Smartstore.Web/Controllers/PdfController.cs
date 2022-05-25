2ede137692625e1603470f4f9c0360f69847ff62
mgesing <gesing@smartstore.com>
2022-05-25 15:46:28 +0200

    Added PdfReceiptMapper for easier usage of ReceiptHeader and ReceiptFooter

--- src/Smartstore.Web/Controllers/PdfController.cs
+++ src/Smartstore.Web/Controllers/PdfController.cs
@@ -1,7 +1,5 @@
 ﻿using Smartstore.Caching;
-using Smartstore.Core.Common.Services;
-using Smartstore.Core.Common.Settings;
-using Smartstore.Core.Configuration;
+using Smartstore.ComponentModel;
 using Smartstore.Core.Security;
 using Smartstore.Core.Stores;
 using Smartstore.Web.Models.Pdf;
@@ -10,17 +8,13 @@ namespace Smartstore.Web.Controllers
 {
     public class PdfController : Controller
     {
-        private readonly ISettingFactory _settingFactory;
         private readonly ICacheFactory _cacheFactory;
         private readonly IStoreContext _storeContext;
-        private readonly IAddressService _addressService;
 
-        public PdfController(ISettingFactory settingFactory, ICacheFactory cacheFactory, IStoreContext storeContext, IAddressService addressService)
+        public PdfController(ICacheFactory cacheFactory, IStoreContext storeContext)
         {
-            _settingFactory = settingFactory;
             _cacheFactory = cacheFactory;
             _storeContext = storeContext;
-            _addressService = addressService;
         }
 
         [NeverAuthorize]
@@ -28,8 +22,8 @@ namespace Smartstore.Web.Controllers
         {
             var model = await PreparePdfReceiptSectionModelAsync(storeId);
 
-            ViewBag.SectionVars = vars;
-            ViewBag.IsPartial = isPartial;
+            ViewData["SectionVars"] = vars;
+            ViewData["IsPartial"] = isPartial;
 
             if (isPartial)
                 return PartialView(model);
@@ -41,39 +35,25 @@ namespace Smartstore.Web.Controllers
         {
             var model = await PreparePdfReceiptSectionModelAsync(storeId);
 
-            ViewBag.SectionVars = vars;
-            ViewBag.IsPartial = isPartial;
+            ViewData["SectionVars"] = vars;
+            ViewData["IsPartial"] = isPartial;
 
             if (isPartial)
                 return PartialView(model);
             return View(model);
         }
 
-        protected async Task<PdfReceiptSectionModel> PreparePdfReceiptSectionModelAsync(int storeId)
+        private async Task<PdfReceiptSectionModel> PreparePdfReceiptSectionModelAsync(int storeId)
         {
-            return await _cacheFactory.GetMemoryCache().GetAsync("PdfReceiptSectionModel-{0}".FormatInvariant(storeId), async (o) =>
+            return await _cacheFactory.GetMemoryCache().GetAsync($"PdfReceiptSectionModel-{storeId}", async (o) =>
             {
                 // 1 min. (just for the duration of pdf processing)
                 o.ExpiresIn(TimeSpan.FromMinutes(1));
 
-                var model = new PdfReceiptSectionModel { StoreId = storeId };
-                var store = _storeContext.GetStoreById(model.StoreId) ?? _storeContext.CurrentStore;
+                var store = _storeContext.GetStoreById(storeId) ?? _storeContext.CurrentStore;
+                var mapper = MapperFactory.GetMapper<Store, PdfReceiptSectionModel>();
 
-                var companyInfoSettings = await _settingFactory.LoadSettingsAsync<CompanyInformationSettings>(store.Id);
-                var bankSettings = await _settingFactory.LoadSettingsAsync<BankConnectionSettings>(store.Id);
-                var contactSettings = await _settingFactory.LoadSettingsAsync<ContactDataSettings>(store.Id);
-                var pdfSettings = await _settingFactory.LoadSettingsAsync<PdfSettings>(store.Id);
-
-                model.StoreName = store.Name;
-                model.StoreUrl = store.Url;
-                model.LogoId = pdfSettings.LogoPictureId != 0 ? pdfSettings.LogoPictureId : store.LogoMediaFileId;
-
-                model.MerchantCompanyInfo = companyInfoSettings;
-                model.MerchantBankAccount = bankSettings;
-                model.MerchantContactData = contactSettings;
-                model.MerchantFormattedAddress = await _addressService.FormatAddressAsync(companyInfoSettings, true);
-
-                return model;
+                return await mapper.MapAsync(store);
             });
         }
     }
