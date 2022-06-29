4feea32b8d7b71122315132eade9500a5008ec17
mgesing <gesing@smartstore.com>
2022-06-29 15:44:00 +0200

    Code style

--- src/Smartstore/Extensions/XmlNodeExtensions.cs
+++ src/Smartstore/Extensions/XmlNodeExtensions.cs
@@ -5,8 +5,10 @@ namespace Smartstore
 {
     public static class XmlNodeExtensions
     {
-        /// <summary>Safe way to get inner text of an attribute.</summary>
-        public static T GetAttributeText<T>(this XmlNode node, string attributeName, T defaultValue = default(T))
+        /// <summary>
+        /// Safe way to get inner text of an attribute.
+        /// </summary>
+        public static T GetAttributeText<T>(this XmlNode node, string attributeName, T defaultValue = default)
         {
             try
             {
@@ -19,46 +21,56 @@ namespace Smartstore
                     }
                 }
             }
-            catch (Exception exc)
+            catch (Exception ex)
             {
-                exc.Dump();
+                ex.Dump();
             }
 
             return defaultValue;
         }
 
-        /// <summary>Safe way to get inner text of an attribute.</summary>
+        /// <summary>
+        /// Safe way to get inner text of an attribute.
+        /// </summary>
         public static string GetAttributeText(this XmlNode node, string attributeName)
         {
             return node.GetAttributeText<string>(attributeName, null);
         }
 
-        /// <summary>Safe way to get inner text of a node.</summary>
-        public static T GetText<T>(this XmlNode node, string xpath = null, T defaultValue = default(T), CultureInfo culture = null)
+        /// <summary>
+        /// Safe way to get inner text of a node.
+        /// </summary>
+        public static T GetText<T>(this XmlNode node, string xpath = null, T defaultValue = default, CultureInfo culture = null)
         {
             try
             {
                 if (node != null)
                 {
                     if (xpath.IsEmpty())
+                    {
                         return node.InnerText.Convert<T>();
+                    }
 
                     var n = node.SelectSingleNode(xpath);
 
                     if (n != null && n.InnerText.HasValue())
+                    {
                         return n.InnerText.Convert<T>(culture);
+                    }
                 }
             }
-            catch (Exception exc)
+            catch (Exception ex)
             {
-                exc.Dump();
+                ex.Dump();
             }
 
             return defaultValue;
         }
 
-        /// <summary>Safe way to get inner text of a node.</summary>
-        public static string GetText(this XmlNode node, string xpath = null, string defaultValue = default(string))
+        /// <summary>
+        /// Safe way to get inner text of a node.
+        /// </summary>
+        public static string GetText(this XmlNode node, string xpath = null, string defaultValue = default)
         {
             return node.GetText<string>(xpath, defaultValue);
         }
