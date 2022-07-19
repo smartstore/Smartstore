4e84bbcc85152fadde9bbe12ae1fa4d4a03bd927
Murat Cakir <muratc@smartstore.de>
2022-07-19 23:38:14 +0200

    Fixed "BrowseButton" problem: after falling back to ViewData.Model, ViewInvoker should ignore arguments not present in the component's parameter list.

--- src/Smartstore.Web.Common/Razor/DefaultViewInvoker.cs
+++ src/Smartstore.Web.Common/Razor/DefaultViewInvoker.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System.Collections.Concurrent;
+using System.Globalization;
+using System.Reflection;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Http;
@@ -6,10 +8,12 @@ using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Options;
+using Smartstore.ComponentModel;
 using Smartstore.Core.Web;
 
 namespace Smartstore.Web.Razor
@@ -18,6 +22,8 @@ namespace Smartstore.Web.Razor
     {
         private const string ActionNameKey = "action";
 
+        private static readonly ConcurrentDictionary<Type, ViewComponentDescriptor> _componentByTypeCache = new();
+
         private readonly IServiceProvider _serviceProvider;
         private readonly IViewDataAccessor _viewDataAccessor;
         private readonly IActionContextAccessor _actionContextAccessor;
@@ -25,6 +31,8 @@ namespace Smartstore.Web.Razor
         private readonly ITempDataDictionaryFactory _tempDataDictionaryFactory;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IViewEngine _viewEngine;
+        private readonly IViewComponentSelector _componentSelector;
+        private readonly IViewComponentDescriptorCollectionProvider _componentDescriptorProvider;
         private readonly HtmlHelperOptions _htmlHelperOptions;
         private readonly HtmlEncoder _htmlEncoder;
 
@@ -36,6 +44,8 @@ namespace Smartstore.Web.Razor
             ITempDataDictionaryFactory tempDataDictionaryFactory,
             IHttpContextAccessor httpContextAccessor,
             ICompositeViewEngine viewEngine,
+            IViewComponentSelector componentSelector,
+            IViewComponentDescriptorCollectionProvider componentDescriptorProvider,
             IOptions<MvcViewOptions> mvcViewOptions,
             HtmlEncoder htmlEncoder)
         {
@@ -46,6 +56,8 @@ namespace Smartstore.Web.Razor
             _tempDataDictionaryFactory = tempDataDictionaryFactory;
             _httpContextAccessor = httpContextAccessor;
             _viewEngine = viewEngine;
+            _componentSelector = componentSelector;
+            _componentDescriptorProvider = componentDescriptorProvider;
             _htmlHelperOptions = mvcViewOptions.Value.HtmlHelperOptions;
             _htmlEncoder = htmlEncoder;
         }
@@ -216,6 +228,11 @@ namespace Smartstore.Web.Razor
             Guard.NotNull(result, nameof(result));
             Guard.NotNull(writer, nameof(writer));
 
+            if (result.ViewComponentType == null && result.ViewComponentName == null)
+            {
+                throw new InvalidOperationException("View component name or type must be set.");
+            }
+
             var viewData = result.ViewData ?? new ViewDataDictionary(_modelMetadataProvider, context.ModelState);
             var tempData = result.TempData ?? _tempDataDictionaryFactory.GetTempData(context.HttpContext);
 
@@ -227,6 +244,11 @@ namespace Smartstore.Web.Razor
                 writer,
                 _htmlHelperOptions);
 
+            if (result.Arguments == null)
+            {
+                result.Arguments = FixComponentArguments(result);
+            }
+
             // IViewComponentHelper is stateful, we want to make sure to retrieve it every time we need it.
             var viewComponentHelper = context.HttpContext.RequestServices.GetRequiredService<IViewComponentHelper>();
             (viewComponentHelper as IViewContextAware)?.Contextualize(viewContext);
@@ -237,11 +259,7 @@ namespace Smartstore.Web.Razor
 
         private static Task<IHtmlContent> GetViewComponentResult(ViewComponentResult result, IViewComponentHelper viewComponentHelper)
         {
-            if (result.ViewComponentType == null && result.ViewComponentName == null)
-            {
-                throw new InvalidOperationException("View component name or type must be set.");
-            }
-            else if (result.ViewComponentType == null)
+            if (result.ViewComponentType == null)
             {
                 return viewComponentHelper.InvokeAsync(result.ViewComponentName, result.Arguments);
             }
@@ -249,6 +267,92 @@ namespace Smartstore.Web.Razor
             {
                 return viewComponentHelper.InvokeAsync(result.ViewComponentType, result.Arguments);
             }
+        }
+
+        private object FixComponentArguments(ViewComponentResult result)
+        {
+            var model = result.ViewData?.Model;
+
+            if (model == null)
+            {
+                return null;
+            }
+
+            var descriptor = SelectComponent(result);
+
+            if (descriptor.Parameters.Count == 0)
+            {
+                return null;
+            }
+
+            if (descriptor.Parameters.Count == 1 && descriptor.Parameters[0].ParameterType.IsAssignableFrom(model.GetType()))
+            {
+                return model;
+            }
+
+            var currentArguments = FastProperty.ObjectToDictionary(model);
+            if (currentArguments.Count == 0)
+            {
+                return null;
+            }
+
+            // We gonna select arguments from current args list only if they exist in the 
+            // component descriptor's parameter list and the types match.
+            var fixedArguments = new Dictionary<string, object>(currentArguments.Count, StringComparer.OrdinalIgnoreCase);
+
+            foreach (var para in descriptor.Parameters)
+            {
+                if (currentArguments.TryGetValue(para.Name, out var value))
+                {
+                    if (value != null && para.ParameterType.IsAssignableFrom(value.GetType()))
+                    {
+                        fixedArguments[para.Name] = value;
+                    }
+                }
+            }
+
+            return fixedArguments;
+        }
+
+        private ViewComponentDescriptor SelectComponent(ViewComponentResult result)
+        {
+            ViewComponentDescriptor descriptor;
+
+            if (result.ViewComponentType != null)
+            {
+                // Select component by type
+                descriptor = _componentByTypeCache.GetOrAdd(result.ViewComponentType, componentType => 
+                {
+                    var descriptors = _componentDescriptorProvider.ViewComponents;
+                    for (var i = 0; i < descriptors.Items.Count; i++)
+                    {
+                        var descriptor = descriptors.Items[i];
+                        if (descriptor.TypeInfo == componentType?.GetTypeInfo())
+                        {
+                            return descriptor;
+                        }
+                    }
+
+                    return null;
+                });
+                
+                if (descriptor == null)
+                {
+                    throw new InvalidOperationException($"A view component named '{result.ViewComponentType.FullName}' could not be found");
+                }
+            }
+            else
+            {
+                // Select component by name
+                descriptor = _componentSelector.SelectComponent(result.ViewComponentName);
+
+                if (descriptor == null)
+                {
+                    throw new InvalidOperationException($"A view component named '{result.ViewComponentName}' could not be found");
+                }
+            }
+
+            return descriptor;
         }
 
         #endregion
