3390972c71d78fa1ee849e1758c8c25b82054d14
Michael Herzog <herzog@smartstore.com>
2022-08-17 13:47:26 +0200

    Google Analytics: added cacheable route

---
+++ src/Smartstore.Modules/Smartstore.Google.Analytics/CacheableRoutes.cs
@@ -0,0 +1,17 @@
+﻿using Smartstore.Core.OutputCache;
+
+namespace Smartstore.Google.Analytics
+{
+    internal sealed class CacheableRoutes : ICacheableRouteProvider
+    {
+        public int Order => 0;
+
+        public IEnumerable<string> GetCacheableRoutes()
+        {
+            return new string[]
+            {
+                "vc:Smartstore.Google.Analytics/GoogleAnalytics"
+            };
+        }
+    }
+}
