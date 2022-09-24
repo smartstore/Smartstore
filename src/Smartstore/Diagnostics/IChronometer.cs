17b52143e146221012f995e4dc8e3194e2bfddda
Murat Cakir <muratc@smartstore.de>
2022-09-24 03:50:24 +0200

    Cleanup

--- src/Smartstore/Diagnostics/IChronometer.cs
+++ src/Smartstore/Diagnostics/IChronometer.cs
@@ -2,14 +2,31 @@
 
 namespace Smartstore.Diagnostics
 {
+    /// <summary>
+    /// Measures execution time of a unit of work and displays
+    /// the result in a profiler widget.
+    /// </summary>
     public interface IChronometer : IDisposable
     {
+        /// <summary>
+        /// Starts a timer with a given <paramref name="key"/>.
+        /// </summary>
+        /// <param name="key">Unique step key</param>
+        /// <param name="message">Message to display in the widget</param>
         void StepStart(string key, string message);
+
+        /// <summary>
+        /// Stops the timer with the given <paramref name="key"/>
+        /// </summary>
+        /// <param name="key"></param>
         void StepStop(string key);
     }
 
     public static class IChronometerExtensions
     {
+        /// <summary>
+        /// Starts a timer and stops it on disposing.
+        /// </summary>
         public static IDisposable Step(this IChronometer chronometer, string message)
         {
             Guard.NotEmpty(message, nameof(message));
