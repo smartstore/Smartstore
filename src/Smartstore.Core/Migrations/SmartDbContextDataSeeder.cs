ecf20e716caa30e8756de226e0ffcd43e1d55c5a
Murat Cakir <muratc@smartstore.de>
2022-10-12 00:31:09 +0200

    More "BackTo" buttons

--- src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
+++ src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
@@ -20,6 +20,27 @@ namespace Smartstore.Core.Data.Migrations
         public void MigrateLocaleResources(LocaleResourcesBuilder builder)
         {
             builder.AddOrUpdate("Admin.Plugins.KnownGroup.StoreFront", "Store Front", "Front-End");
+
+            builder.Delete(
+                "Admin.System.Log.BackToList",
+                "Admin.Promotions.Campaigns.BackToList",
+                "Admin.Orders.BackToList",
+                "Admin.Customers.Customers.BackToList",
+                "Admin.Customers.CustomerRoles.BackToList",
+                "Admin.ContentManagement.Polls.BackToList",
+                "Admin.ContentManagement.MessageTemplates.BackToList",
+                "Admin.Configuration.Tax.Providers.BackToList",
+                "Admin.Configuration.SMSProviders.BackToList",
+                "Admin.Configuration.Shipping.Providers.BackToList",
+                "Admin.Configuration.Shipping.Methods.BackToList",
+                "Admin.Configuration.Plugins.Misc.BackToList",
+                "Admin.Configuration.Payment.Methods.BackToList",
+                "Admin.Configuration.ExternalAuthenticationMethods.BackToList",
+                "Admin.Configuration.DeliveryTimes.BackToList",
+                "Admin.Configuration.Countries.BackToList",
+                "Admin.Catalog.Products.BackToList",
+                "Admin.Catalog.Attributes.CheckoutAttributes.BackToList",
+                "Admin.Affiliates.BackToList");
         }
     }
 }
