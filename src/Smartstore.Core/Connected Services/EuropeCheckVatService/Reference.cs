3bca96f5bac87f71f406632b60f70c7c57b50fb2
Murat Cakir <muratc@smartstore.de>
2022-10-12 03:48:34 +0200

    Compilation fix

--- src/Smartstore.Core/Connected Services/EuropeCheckVatService/Reference.cs
+++ src/Smartstore.Core/Connected Services/EuropeCheckVatService/Reference.cs
@@ -320,15 +320,15 @@ namespace Smartstore.Core.EuropeCheckVatService
             return base.Channel.checkVatApproxAsync(request);
         }
         
-        public virtual System.Threading.Tasks.Task OpenAsync()
-        {
-            return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
-        }
+        //public virtual System.Threading.Tasks.Task OpenAsync()
+        //{
+        //    return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
+        //}
         
-        public virtual System.Threading.Tasks.Task CloseAsync()
-        {
-            return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
-        }
+        //public virtual System.Threading.Tasks.Task CloseAsync()
+        //{
+        //    return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
+        //}
         
         private static System.ServiceModel.Channels.Binding GetBindingForEndpoint(EndpointConfiguration endpointConfiguration)
         {
