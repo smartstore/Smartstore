9ee4b07647337da01cd946c30c009461e8e68f1a
Murat Cakir <muratc@smartstore.de>
2022-10-20 23:08:35 +0200

    WebAPI review

--- src/Smartstore.Web.Common/Api/WebApiController.cs
+++ src/Smartstore.Web.Common/Api/WebApiController.cs
@@ -4,7 +4,11 @@ namespace Smartstore.Web.Api
 {
     [ODataRouteComponent("odata/v1")]
     [Route("odata/v1")]
-    [ApiExplorerSettings(GroupName = "webapi1")]
+    [ApiController, ApiExplorerSettings(GroupName = "webapi1")]
+    // TODO: (mg) (core) Check the benefits of ApiControllerAttribute for OData:
+    // https://www.strathweb.com/2018/02/exploring-the-apicontrollerattribute-and-its-features-for-asp-net-core-mvc-2-1/.
+    // Investigate what code parts can be removed/simplified when using the attribute (at least [FromBody] seems to be obsolete now).
+    // Furthermore check the impact on API exploration.
     public abstract class WebApiController<TEntity> : SmartODataController<TEntity>
         where TEntity : BaseEntity, new()
     {
