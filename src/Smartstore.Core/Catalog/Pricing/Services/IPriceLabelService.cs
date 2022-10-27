3a55421661f8fc4d7c3c0f4b94f4e02cfe2a774a
Murat Cakir <muratc@smartstore.de>
2022-10-28 01:42:50 +0200

    IPriceLabelService (to be implemented by MH)

---
+++ src/Smartstore.Core/Catalog/Pricing/Services/IPriceLabelService.cs
@@ -0,0 +1,21 @@
+﻿using Smartstore.Core.Catalog.Products;
+
+namespace Smartstore.Core.Catalog.Pricing
+{
+    public interface IPriceLabelService
+    {
+        /// <summary>
+        /// Gets the compare price label for a given product. Falls back to
+        /// system default compare label if product has no label.
+        /// </summary>
+        /// <param name="product">The product to get label for.</param>
+        Task<PriceLabel> GetComparePriceLabelAsync(Product product);
+
+        /// <summary>
+        /// Gets the regular price label for a given product. Falls back to
+        /// system default price label if product has no label.
+        /// </summary>
+        /// <param name="product">The product to get label for.</param>
+        Task<PriceLabel> GetRegularPriceLabelAsync(Product product);
+    }
+}
