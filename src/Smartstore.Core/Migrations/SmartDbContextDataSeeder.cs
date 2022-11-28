5f36bf0188565d9513de797b78485e20139d0cb1
Michael Herzog <herzog@smartstore.com>
2022-11-28 20:43:25 +0100

    Minor resource change

--- src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
+++ src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
@@ -22,6 +22,10 @@ namespace Smartstore.Core.Data.Migrations
             builder.AddOrUpdate("Products.Price.OfferCountdown",
                 "Ends in <b class=\"fwm\">{0}</b>",
                 "Endet in <b class=\"fwm\">{0}</b>");
+
+            builder.AddOrUpdate("Admin.Configuration.Settings.CustomerUser.CustomerNameFormat.Hint",
+                "Sets the customer's display name to be used for public content such as product reviews, comments, etc..",
+                "Legt den Anzeigenamen des Kunden fest, der für öffentliche Inhalte wie Produktbewertungen, Kommentare, etc. verwendet wird.");
         }
     }
 }
