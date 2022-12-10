80a88723c6f1722aa339ff0883fee80f173b0372
Murat Cakir <muratc@smartstore.de>
2022-12-10 04:15:01 +0100

    DisallowRobotsAttribute

---
+++ src/Smartstore.Core/Platform/Seo/Routing/DisallowRobotAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace Smartstore.Core.Seo.Routing;
+
+/// <summary>
+/// Disallows robots access to the route. Also used to dynamically populate the robots.txt file.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+public sealed class DisallowRobotAttribute : Attribute
+{
+    public DisallowRobotAttribute()
+    {
+    }
+
+    public DisallowRobotAttribute(string path)
+    {
+        Guard.NotEmpty(path);
+        Path = path;
+    }
+
+    public string Path { get; }
+}
