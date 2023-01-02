ac5276353a5ea3c6e5b1978ba38242d84df78a24
Murat Cakir <muratc@smartstore.de>
2023-01-02 20:44:19 +0100

    Enhanced CompositeFileSystem

--- src/Smartstore/IO/CompositeFileSystem.cs
+++ src/Smartstore/IO/CompositeFileSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Composite;
 using Microsoft.Extensions.Primitives;
 using Smartstore.Threading;
 
@@ -10,7 +11,7 @@ namespace Smartstore.IO
     public class CompositeFileSystem : IFileSystem
     {
         private readonly IFileSystem[] _fileSystems;
-        private readonly CompositeFileProvider _compositeFileProvider;
+        private readonly IFileSystem _primary;
         private readonly string _root;
 
         public CompositeFileSystem(params IFileSystem[] fileSystems)
@@ -21,14 +22,20 @@ namespace Smartstore.IO
         public CompositeFileSystem(IEnumerable<IFileSystem> fileSystems)
         {
             Guard.NotNull(fileSystems, nameof(fileSystems));
+            
             if (!fileSystems.Any())
             {
                 throw new ArgumentException("CompositeFileSystem must contain at least one item.", nameof(fileSystems));
             }
 
             _fileSystems = fileSystems.ToArray();
-            _compositeFileProvider = new CompositeFileProvider(_fileSystems);
             _root = _fileSystems[0].Root;
+
+            if (_fileSystems.Length == 1)
+            {
+                // Perf
+                _primary = _fileSystems[0];
+            }
         }
 
         /// <summary>
@@ -43,6 +50,11 @@ namespace Smartstore.IO
 
         private bool Check(string subpath, Func<IFileSystem, string, bool> checker)
         {
+            if (_primary != null) 
+            {
+                return checker(_primary, subpath);
+            }
+            
             foreach (var fs in _fileSystems)
             {
                 if (checker(fs, subpath))
@@ -56,6 +68,11 @@ namespace Smartstore.IO
 
         private async Task<bool> CheckAsync(string subpath, Func<IFileSystem, string, Task<bool>> checker)
         {
+            if (_primary != null)
+            {
+                return await checker(_primary, subpath);
+            }
+
             foreach (var fs in _fileSystems)
             {
                 if (await checker(fs, subpath))
@@ -70,6 +87,11 @@ namespace Smartstore.IO
         private T Get<T>(string subpath, Func<IFileSystem, string, T> getter)
             where T : IFileEntry
         {
+            if (_primary != null)
+            {
+                return getter(_primary, subpath);
+            }
+
             T entry = default;
             foreach (var fs in _fileSystems)
             {
@@ -86,6 +108,11 @@ namespace Smartstore.IO
         private async Task<T> GetAsync<T>(string subpath, Func<IFileSystem, string, Task<T>> getter)
             where T : IFileEntry
         {
+            if (_primary != null)
+            {
+                return await getter(_primary, subpath);
+            }
+
             T entry = default;
             foreach (var fs in _fileSystems)
             {
@@ -118,14 +145,54 @@ namespace Smartstore.IO
             => GetAsync(subpath, (fs, path) => fs.GetFileAsync(path));
 
         public IFileInfo GetFileInfo(string subpath)
-            => _compositeFileProvider.GetFileInfo(subpath);
+            => Get(subpath, (fs, path) => fs.GetFile(path));
+
         public IDirectoryContents GetDirectoryContents(string subpath)
-            => _compositeFileProvider.GetDirectoryContents(subpath);
-        public IChangeToken Watch(string filter)
-            => _compositeFileProvider.Watch(filter);
+        {
+            if (_primary != null)
+            {
+                return _primary.GetDirectoryContents(subpath);
+            }
+            else
+            {
+                return new CompositeDirectoryContents(_fileSystems, subpath);
+            }
+        }
+
+        public virtual IChangeToken Watch(string pattern)
+        {
+            if (_primary != null)
+            {
+                return _primary.Watch(pattern);
+            }
+
+            // Watch all file providers
+            var changeTokens = new List<IChangeToken>();
+            foreach (IFileProvider fileProvider in _fileSystems)
+            {
+                IChangeToken changeToken = fileProvider.Watch(pattern);
+                if (changeToken != null)
+                {
+                    changeTokens.Add(changeToken);
+                }
+            }
+
+            // There is no change token with active change callbacks
+            if (changeTokens.Count == 0)
+            {
+                return NullChangeToken.Singleton;
+            }
+
+            return new CompositeChangeToken(changeTokens);
+        }
 
         public string MapPath(string subpath)
         {
+            if (_primary != null)
+            {
+                return _primary.MapPath(subpath);
+            }
+
             IFileEntry entry = null;
             foreach (var fs in _fileSystems)
             {
