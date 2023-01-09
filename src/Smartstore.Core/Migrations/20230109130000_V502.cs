7732f01b28e1bd81f0c0919d4920a2086efd088a
Michael Herzog <herzog@smartstore.com>
2023-01-09 12:34:05 +0100

    Added migration for 5.0.2

---
+++ src/Smartstore.Core/Migrations/20230109130000_V502.cs
@@ -0,0 +1,39 @@
+﻿using FluentMigrator;
+using Smartstore.Data.Migrations;
+
+namespace Smartstore.Core.Data.Migrations
+{
+    [MigrationVersion("2023-01-09 13:00:00", "V502")]
+    internal class V502 : Migration, ILocaleResourcesProvider, IDataSeeder<SmartDbContext>
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+
+        public bool RollbackOnFailure => false;
+
+        public async Task SeedAsync(SmartDbContext context, CancellationToken cancelToken = default)
+        {
+            await context.MigrateLocaleResourcesAsync(MigrateLocaleResources);
+        }
+
+        public void MigrateLocaleResources(LocaleResourcesBuilder builder)
+        {
+            builder.AddOrUpdate("Products.Price.OfferCountdown",
+                "Ends in <b class=\"fwm\">{0}</b>",
+                "Endet in <b class=\"fwm\">{0}</b>");
+
+            builder.AddOrUpdate("Admin.Configuration.Settings.CustomerUser.CustomerNameFormat.Hint",
+                "Sets the customer's display name to be used for public content such as product reviews, comments, etc..",
+                "Legt den Anzeigenamen des Kunden fest, der für öffentliche Inhalte wie Produktbewertungen, Kommentare, etc. verwendet wird.");
+
+            builder.AddOrUpdate("Admin.System.Maintenance.DbBackup.SameServerNote",
+                "Backups and restores of databases are only possible if the database server (e.g. MS SQL Server or MySQL) and the physical location of the store installation are on the same server.",
+                "Sicherungen und Wiederherstellungen von Datenbanken sind nur möglich, wenn sich der Datenbankserver (z.B. MS SQL Server oder MySQL) und der physikalische Speicherort der Shop-Installation auf einem gemeinsamen Server befinden.");
+        }
+    }
+}
--- src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
+++ src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
@@ -19,17 +19,7 @@ namespace Smartstore.Core.Data.Migrations
 
         public void MigrateLocaleResources(LocaleResourcesBuilder builder)
         {
-            builder.AddOrUpdate("Products.Price.OfferCountdown",
-                "Ends in <b class=\"fwm\">{0}</b>",
-                "Endet in <b class=\"fwm\">{0}</b>");
 
-            builder.AddOrUpdate("Admin.Configuration.Settings.CustomerUser.CustomerNameFormat.Hint",
-                "Sets the customer's display name to be used for public content such as product reviews, comments, etc..",
-                "Legt den Anzeigenamen des Kunden fest, der für öffentliche Inhalte wie Produktbewertungen, Kommentare, etc. verwendet wird.");
-
-            builder.AddOrUpdate("Admin.System.Maintenance.DbBackup.SameServerNote",
-                "Backups and restores of databases are only possible if the database server (e.g. MS SQL Server or MySQL) and the physical location of the store installation are on the same server.",
-                "Sicherungen und Wiederherstellungen von Datenbanken sind nur möglich, wenn sich der Datenbankserver (z.B. MS SQL Server oder MySQL) und der physikalische Speicherort der Shop-Installation auf einem gemeinsamen Server befinden.");
         }
     }
 }
