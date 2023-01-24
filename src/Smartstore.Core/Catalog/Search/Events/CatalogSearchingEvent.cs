800509173a5350b60823d3956c318d68f22c4154
mgesing <gesing@smartstore.com>
2023-01-24 16:24:58 +0100

    Added code comments

--- src/Smartstore.Core/Catalog/Search/Events/CatalogSearchingEvent.cs
+++ src/Smartstore.Core/Catalog/Search/Events/CatalogSearchingEvent.cs
@@ -1,5 +1,10 @@
-﻿namespace Smartstore.Core.Catalog.Search
+﻿using Smartstore.Core.Search;
+
+namespace Smartstore.Core.Catalog.Search
 {
+    /// <summary>
+    /// An event that is fired before searching the catalog for products.
+    /// </summary>
     public class CatalogSearchingEvent
     {
         public CatalogSearchingEvent(CatalogSearchQuery query, bool direct)
@@ -10,10 +15,18 @@
             Direct = direct;
         }
 
+        /// <summary>
+        /// A value indicating whether the database is directly searched.
+        /// If <c>false</c> then <see cref="ISearchEngine"/> is used.
+        /// </summary>
         public bool Direct { get; init; }
+
         public CatalogSearchQuery Query { get; init; }
     }
 
+    /// <summary>
+    /// An event that is fired after the catalog has been searched for products.
+    /// </summary>
     public class CatalogSearchedEvent
     {
         public CatalogSearchedEvent(CatalogSearchQuery query, CatalogSearchResult result)
