a572a345caf47494acda315837db7e8df767216d
mgesing <gesing@smartstore.com>
2023-01-27 16:02:14 +0100

    Typo

--- src/Smartstore.Core/Platform/Security/Filters/RequireSslAttribute.cs
+++ src/Smartstore.Core/Platform/Security/Filters/RequireSslAttribute.cs
@@ -3,7 +3,7 @@
 namespace Smartstore.Core.Security
 {
     /// <summary>
-    /// A marker filter indicating that the current request should be recived over HTTPS (if the policies allow this).
+    /// A marker filter indicating that the current request should be received over HTTPS (if the policies allow this).
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
     public sealed class RequireSslAttribute : Attribute, IFilterMetadata
