732ef6085a16fa520e34d3557f1532ebc075bea2
Michael Herzog <herzog@smartstore.com>
2023-01-27 17:10:07 +0100

    PayPal Invoice: Correct payment info model validation

--- src/Smartstore.Modules/Smartstore.PayPal/Components/PayPalInvoiceViewComponent.cs
+++ src/Smartstore.Modules/Smartstore.PayPal/Components/PayPalInvoiceViewComponent.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Smartstore.Core.Checkout.Orders;
 using Smartstore.Web.Components;
 
 namespace Smartstore.PayPal.Components
 {
     public class PayPalInvoiceViewComponent : SmartViewComponent
     {
+        private readonly ICheckoutStateAccessor _checkoutStateAccessor;
         private readonly PayPalSettings _settings;
 
-        public PayPalInvoiceViewComponent(PayPalSettings settings)
+        public PayPalInvoiceViewComponent(ICheckoutStateAccessor checkoutStateAccessor, PayPalSettings settings)
         {
+            _checkoutStateAccessor = checkoutStateAccessor;
             _settings = settings;
         }
 
@@ -23,7 +26,7 @@ namespace Smartstore.PayPal.Components
             var customer = Services.WorkContext.CurrentCustomer;
             var model = new PublicInvoiceModel();
 
-            if (customer.BillingAddress != null && customer.BillingAddress.Country != null)
+            if ((customer.BillingAddress != null && customer.BillingAddress.Country != null) || !customer.BillingAddress.LastName.HasValue())
             {
                 var diallingCode = customer.BillingAddress.Country.DiallingCode;
                 model.DiallingCode = diallingCode.Value.ToString();
@@ -34,9 +37,30 @@ namespace Smartstore.PayPal.Components
                 return Empty();
             }
 
-            // Prepare model if customer is logged in and has already entered his data of birth
-            if (customer.BirthDate != null)
+            // Set postback values.
+            var paymentData = _checkoutStateAccessor.CheckoutState.PaymentData;
+            var phoneNumber = (string)paymentData.Get("PayPalInvoicePhoneNumber");
+
+            if (phoneNumber.HasValue())
             {
+                model.PhoneNumber = phoneNumber;
+            }
+
+            if (((string)paymentData.Get("PayPalInvoiceBirthdate")).HasValue())
+            {
+                try
+                {
+                    var birthDate = DateTime.Parse((string)paymentData.Get("PayPalInvoiceBirthdate"));
+
+                    model.DateOfBirthDay = birthDate.Day;
+                    model.DateOfBirthMonth = birthDate.Month;
+                    model.DateOfBirthYear = birthDate.Year;
+                }
+                catch { }
+            }
+            else if (customer.BirthDate != null)
+            {
+                // Prepare model if customer is logged in and has already entered his data of birth
                 model.DateOfBirthDay = customer.BirthDate.Value.Day;
                 model.DateOfBirthMonth = customer.BirthDate.Value.Month;
                 model.DateOfBirthYear = customer.BirthDate.Value.Year;
