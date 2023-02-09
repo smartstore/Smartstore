cec147af458a78f62111731478254dd567664719
Murat Cakir <muratc@smartstore.de>
2023-02-09 20:33:34 +0100

    MySqlConventionSource for UTC_TIMESTAMP default value

--- src/Smartstore.Data/Smartstore.Data.MySql/Migration/MySqlConventionSource.cs
+++ src/Smartstore.Data/Smartstore.Data.MySql/Migration/MySqlConventionSource.cs
@@ -1,28 +1,27 @@
-﻿using System.Data;
+﻿using FluentMigrator;
 using FluentMigrator.Expressions;
 using FluentMigrator.Runner.Conventions;
 using Smartstore.Core.Data.Migrations;
 
-namespace Smartstore.Data.PostgreSql.Migration
+namespace Smartstore.Data.MySql.Migration
 {
-    internal class PostgreSqlConventionSource : IConventionSource
+    internal class MySqlConventionSource : IConventionSource
     {
         public void Configure(IConventionSet conventionSet)
         {
-            conventionSet.ColumnsConventions.Add(new CitextColumnsConvention());
+            conventionSet.ColumnsConventions.Add(new UtcDateTimeColumnsConvention());
         }
     }
 
-    internal class CitextColumnsConvention : IColumnsConvention
+    internal class UtcDateTimeColumnsConvention : IColumnsConvention
     {
         public IColumnsExpression Apply(IColumnsExpression expression)
         {
             foreach (var column in expression.Columns)
             {
-                if (column.Type == DbType.String)
+                if (column.DefaultValue.Equals(SystemMethods.CurrentUTCDateTime))
                 {
-                    column.Type = null;
-                    column.CustomType = "citext";
+                    column.DefaultValue = RawSql.Insert("(UTC_TIMESTAMP)");
                 }
             }
 
