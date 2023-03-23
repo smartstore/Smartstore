a672e5bfcdc9ccb9b48563069bad0630255997cd
Michael Herzog <herzog@smartstore.com>
2023-03-23 20:00:31 +0100

    Minor stuff

--- src/Smartstore.Core/Platform/Packaging/ExtensionPackage.cs
+++ src/Smartstore.Core/Platform/Packaging/ExtensionPackage.cs
@@ -30,7 +30,7 @@ namespace Smartstore.Core.Packaging
                 var manifest = archive.GetEntry(PackagingUtility.ManifestFileName);
                 if (manifest == null)
                 {
-                    throw new Exception("Manifest file could not be extracted from zip archive.");
+                    throw new ArgumentException("Manifest file could not be extracted from zip archive.");
                 }
 
                 // TODO: (core) Throw typed message if deserialization fails, catch in controller and notify with localized message. 
