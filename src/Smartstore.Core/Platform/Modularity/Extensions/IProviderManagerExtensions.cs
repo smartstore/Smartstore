9217de833f12fa81c6ab24db6e8649d905e4a503
Murat Cakir <muratc@smartstore.de>
2023-04-01 04:21:43 +0200

    IProviderManager.TryGetProvider() extensions

---
+++ src/Smartstore.Core/Platform/Modularity/Extensions/IProviderManagerExtensions.cs
@@ -0,0 +1,46 @@
+﻿namespace Smartstore.Engine.Modularity
+{
+    public static class IProviderManagerExtensions
+    {
+        public static bool TryGetProvider<TProvider>(this IProviderManager manager, 
+            string systemName, 
+            int storeId, 
+            out Provider<TProvider> provider)
+            where TProvider : IProvider
+        {
+            Guard.NotNull(manager);
+            
+            provider = null;
+
+            try
+            {
+                provider = manager.GetProvider<TProvider>(systemName, storeId);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool TryGetProvider(this IProviderManager manager,
+            string systemName,
+            int storeId,
+            out Provider<IProvider> provider)
+        {
+            Guard.NotNull(manager);
+
+            provider = null;
+
+            try
+            {
+                provider = manager.GetProvider(systemName, storeId);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
