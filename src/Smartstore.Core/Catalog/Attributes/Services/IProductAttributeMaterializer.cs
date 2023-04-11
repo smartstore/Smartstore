ed6c07e5183bf24278c0d57dc3809979f368bb2d
mgesing <gesing@smartstore.com>
2023-04-11 16:47:45 +0200

    When an order was placed, the stock quantity of attribute combinations were not updated if the stock is managed by attributes

--- src/Smartstore.Core/Catalog/Attributes/Services/IProductAttributeMaterializer.cs
+++ src/Smartstore.Core/Catalog/Attributes/Services/IProductAttributeMaterializer.cs
@@ -1,4 +1,5 @@
-﻿using Smartstore.Core.Catalog.Products;
+﻿using System.Diagnostics;
+using Smartstore.Core.Catalog.Products;
 using Smartstore.Core.Checkout.Cart;
 
 namespace Smartstore.Core.Catalog.Attributes
@@ -56,8 +57,9 @@ namespace Smartstore.Core.Catalog.Attributes
         /// </summary>
         /// <param name="productId">Product identifier.</param>
         /// <param name="selection">Attribute selection.</param>
+        /// <param name="tracked">A value indicating whether to put prefetched entities to EF change tracker.</param>
         /// <returns>Found attribute combination or <c>null</c> if none was found.</returns>
-        Task<ProductVariantAttributeCombination> FindAttributeCombinationAsync(int productId, ProductVariantAttributeSelection selection);
+        Task<ProductVariantAttributeCombination> FindAttributeCombinationAsync(int productId, ProductVariantAttributeSelection selection, bool tracked = false);
 
         /// <summary>
         /// Finds an attribute combination by attribute selection and applies its data to the product.
