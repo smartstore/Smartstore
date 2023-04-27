2c48231ef284ef87a6a8fdb37cf5c33e18dd9e4a
mgesing <gesing@smartstore.com>
2023-04-27 16:01:27 +0200

    MainMenuShrinker was missing
    (cherry picked from commit 6e6117a415bbd74c41a3fc72e97cc548d3cd6db9)

---
+++ src/Smartstore.Web/Infrastructure/Menus/MainMenuShrinker.cs
@@ -0,0 +1,23 @@
+﻿using Smartstore.Core.Catalog;
+using Smartstore.Core.Content.Menus;
+using Smartstore.Events;
+
+namespace Smartstore.Web.Infrastructure
+{
+    public class MainMenuShrinker : IConsumer
+    {
+        public void HandleEvent(MenuBuiltEvent message,
+            CatalogSettings catalogSettings)
+        {
+            if (message.Name != "Main" || catalogSettings.MaxItemsToDisplayInCatalogMenu.GetValueOrDefault() < 1)
+            {
+                return;
+            }
+
+            message.Root.Children
+                .Where(x => x.Value.Id != "brand")
+                .Skip(catalogSettings.MaxItemsToDisplayInCatalogMenu.Value)
+                .Each(x => x.SetMetadata("spare", true));
+        }
+    }
+}
