6e136ace557bdf38ad0080002bd3eeb1d32115fb
Mark Wördenweber <mark.woerdenweber@smartstore.com>
2023-04-28 09:37:00 +0200

    Added a filter for countries

---
+++ src/Smartstore.Web/Areas/Admin/Models/Directory/CountrySearchListModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Smartstore.Admin.Models.Common
+{
+    [LocalizedDisplay("Admin.Configuration.Countries.Fields.")]
+    public class CountrySearchListModel : EntityModelBase
+    {
+        [LocalizedDisplay("*Name")]
+        public string SearchName { get; set; }
+
+        [LocalizedDisplay("*TwoLetterIsoCode")]
+        public string SearchTwoLetterIsoCode { get; set; }
+
+        [LocalizedDisplay("*AllowsBilling")]
+        public bool? SearchAllowsBilling { get; set; }
+
+        [LocalizedDisplay("*AllowsShipping")]
+        public bool? SearchAllowsShipping { get; set; }
+
+        [LocalizedDisplay("*SubjectToVat")]
+        public bool? SearchSubjectToVat { get; set; }
+
+        [LocalizedDisplay("*Published")]
+        public bool? SearchPublished { get; set; }
+    }
+}
