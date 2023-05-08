5e6800f6a73a5e39fd0773d09fc73bf00cd2a679
mgesing <gesing@smartstore.com>
2023-05-08 19:55:27 +0200

    Search term isolation

---
+++ src/Smartstore.Core/Platform/Search/SearchProviderBase.cs
@@ -0,0 +1,32 @@
+﻿using Smartstore.Core.Search.Facets;
+
+namespace Smartstore.Core.Search
+{
+    public abstract partial class SearchProviderBase : ISearchProvider
+    {
+        /// <inheritdoc/>
+        public virtual SearchSort GetSorting(SearchSort sort, ISearchQuery query)
+            => null;
+
+        /// <inheritdoc/>
+        public virtual float GetBoost(ISearchFilter filter)
+            => filter.Boost;
+
+        /// <inheritdoc/>
+        public virtual string GetLocalizedFieldName(ISearchFilter filter, string languageCulture)
+            => null;
+
+        /// <inheritdoc/>
+        public virtual bool IsFacetField(string fieldName, ISearchQuery query)
+            => false;
+
+        /// <inheritdoc/>
+        public abstract IList<SearchField> GetFields(ISearchQuery query, string languageCulture);
+
+        /// <inheritdoc/>
+        public abstract IDictionary<string, FacetGroup> GetFacetMap(ISearchEngine searchEngine, IFacetMetadataStorage storage);
+
+        /// <inheritdoc/>
+        public abstract Task<IDictionary<string, FacetGroup>> GetFacetMapAsync(ISearchEngine searchEngine, IFacetMetadataStorage storage);
+    }
+}
