7e82c4ed2adcd24f9a9fe0c72520bda6c0790158
Murat Cakir <muratc@smartstore.de>
2023-05-09 04:19:08 +0200

    LikeOperator: don't call string.ToLower() for EF

--- src/Smartstore.Core/Platform/Rules/Operators/LikeOperator.cs
+++ src/Smartstore.Core/Platform/Rules/Operators/LikeOperator.cs
@@ -12,6 +12,14 @@ namespace Smartstore.Core.Rules.Operators
 
     internal class LikeOperator : RuleOperator
     {
+        enum PatternMatchType : byte
+        {
+            Like,
+            Contains,
+            StartsWith,
+            EndsWith
+        }
+        
         internal LikeOperator()
             : this("Like", false) { }
 
@@ -25,27 +33,72 @@ namespace Smartstore.Core.Rules.Operators
 
         protected override Expression GenerateExpression(Expression left /* member expression */, Expression right /* term */, IQueryProvider provider)
         {
-            var constantExpr = right as ConstantExpression;
-            if (constantExpr == null)
+            if (right is not ConstantExpression constantExpr)
+            {
                 throw new ArgumentException($"The expression must be of type '{nameof(ConstantExpression)}'.", nameof(right));
+            } 
 
             if (constantExpr.Value == null || constantExpr.Type != typeof(string))
             {
                 throw new ArgumentException("The 'Like' operator only supports non-null strings.", nameof(right));
             }
 
-            var isEF = provider is EntityQueryProvider;
             var pattern = (string)constantExpr.Value;
+            var matchType = PatternMatchType.Like;
+
+            var anyCharIndex = pattern.IndexOf('*');
+            var singleCharIndex = pattern.IndexOf('?');
+
+            var hasAnyCharToken = anyCharIndex > -1;
+            var hasSingleCharToken = singleCharIndex > -1;
+            var hasAnyWildcard = hasAnyCharToken || hasSingleCharToken;
+
+            if (!hasAnyWildcard)
+            {
+                // Don't attemp GLOB matching if input has no valid GLOB chars
+                matchType = PatternMatchType.Contains;
+            }
+            else if (!hasSingleCharToken)
+            {
+                // If '?' is present in pattern we MUST perform GLOB match
+                if (anyCharIndex == 0 && pattern.LastIndexOf('*') == anyCharIndex)
+                {
+                    // Pattern starts with '*' and it is the only '*': EndsWith()
+                    matchType = PatternMatchType.EndsWith;
+                    // Strip leading '*'
+                    pattern = pattern[1..];
+                }
+                else if (pattern.Length > 1 && anyCharIndex == pattern.Length - 1)
+                {
+                    // Pattern ends with '*' and it is the only '*': StartsWith()
+                    matchType = PatternMatchType.StartsWith;
+                    // Strip trailing '*'
+                    pattern = pattern[..^1];
+                }
+            }
 
             Expression expression;
 
-            if (isEF)
+            if (matchType == PatternMatchType.Contains)
             {
-                var hasAnyCharToken = pattern.IndexOf('*') > -1;
-                var hasSingleCharToken = pattern.IndexOf('?') > -1;
-                var hasAnyWildcard = hasAnyCharToken || hasSingleCharToken;
-
-                if (hasAnyWildcard)
+                // Call "input".Contains(pattern)
+                expression = ExpressionHelper.StringContainsMethod.ToCaseInsensitiveStringMethodCall(left, Expression.Constant(pattern), provider);
+            }
+            else if (matchType == PatternMatchType.StartsWith)
+            {
+                // Call "input".StartsWith(pattern)
+                expression = ExpressionHelper.StringStartsWithMethod.ToCaseInsensitiveStringMethodCall(left, Expression.Constant(pattern), provider);
+            }
+            else if (matchType == PatternMatchType.EndsWith)
+            {
+                // Call "input".EndsWith(pattern)
+                expression = ExpressionHelper.StringEndsWithMethod.ToCaseInsensitiveStringMethodCall(left, Expression.Constant(pattern), provider);
+            }
+            else
+            {
+                // Handle LIKE
+                var isEF = provider is EntityQueryProvider;
+                if (isEF)
                 {
                     // Convert file wildcard pattern to SQL LIKE expression:
                     // my*new_file-?.png > my%new/_file-_.png
@@ -66,27 +119,23 @@ namespace Smartstore.Core.Rules.Operators
                     }
 
                     right = Expression.Constant(pattern);
+
+                    var likeMethod = hasUnderscore ? ExpressionHelper.DbLikeMethodWithEscape : ExpressionHelper.DbLikeMethod;
+                    // this DbFunctions, matchExpression, pattern, [escapeCharacter]
+                    var arguments = hasUnderscore 
+                        ? new Expression[] { Expression.Constant(EF.Functions), left, right, Expression.Constant("/") }
+                        : new Expression[] { Expression.Constant(EF.Functions), left, right };
+
+                    // Call EF.Functions.Like()
+                    expression = Expression.Call(likeMethod, arguments);
                 }
-
-                // Call EF.Functions.Like()
-                expression = Expression.Call(ExpressionHelper.DbLikeMethod, new Expression[]
+                else
                 {
-                    // this DbFunctions
-                    Expression.Constant(EF.Functions),
-                    // matchExpression
-                    left,
-                    // pattern
-                    right,
-                    // escapeCharacter
-                    Expression.Constant("/")
-                });
-            }
-            else
-            {
-                var wildcard = new Wildcard(pattern);
+                    var wildcard = new Wildcard(pattern);
 
-                // Call Wildcard.IsMatch()
-                expression = Expression.Call(Expression.Constant(wildcard), ExpressionHelper.WildcardIsMatchMethod, left);
+                    // Call Wildcard.IsMatch()
+                    expression = Expression.Call(Expression.Constant(wildcard), ExpressionHelper.WildcardIsMatchMethod, left);
+                }
             }
 
             return Expression.Equal(
