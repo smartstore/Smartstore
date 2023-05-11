fb1c730f590b72ed0c755c639f636d99cec054af
HEPTAPOD\Chris <developer@wissing.eu>
2023-05-11 10:05:59 +0200

    removed unneeded GetResult call

--- src/Smartstore/ComponentModel/MiniMapper.cs
+++ src/Smartstore/ComponentModel/MiniMapper.cs
@@ -104,7 +104,7 @@ namespace Smartstore.ComponentModel
                 }
             }
 
-            MapperFactory.MapCallback(from, to).GetAwaiter().GetResult();
+            MapperFactory.MapCallback(from, to).GetAwaiter();
         }
 
         private static FastProperty[] GetFastPropertiesFor(Type type)
