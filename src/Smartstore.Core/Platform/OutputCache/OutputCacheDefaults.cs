718a0f959f8edf221146aa1d1021982e9423ec90
Murat Cakir <muratc@smartstore.de>
2023-05-17 22:29:05 +0200

    OutputCacheDefaults (route name constants)

---
+++ src/Smartstore.Core/Platform/OutputCache/OutputCacheDefaults.cs
@@ -0,0 +1,34 @@
+﻿namespace Smartstore.Core.OutputCache
+{
+    public static class OutputCacheDefaults
+    {
+        public static string HomeRoute { get; } = "Home/Index";
+        public static string CategoryRoute { get; } = "Catalog/Category";
+        public static string ManufacturerRoute { get; } = "Catalog/Manufacturer";
+        public static string ManufacturerAllRoute { get; } = "Catalog/ManufacturerAll";
+        public static string ProductsByTagRoute { get; } = "Catalog/ProductsByTag";
+        public static string ProductTagsAllRoute { get; } = "Catalog/ProductTagsAll";
+        public static string RecentlyAddedProductsRoute { get; } = "Catalog/RecentlyAddedProducts";
+        public static string RecentlyAddedProductsRssRoute { get; } = "Catalog/RecentlyAddedProductsRss";
+        public static string ProductDetailsRoute { get; } = "Product/ProductDetails";
+        public static string SearchRoute { get; } = "Search/Search";
+        public static string TopicDetailsRoute { get; } = "Topic/TopicDetails";
+
+        public static string[] AllProductListsRoutes { get; } = new[] 
+        { 
+            CategoryRoute, 
+            ManufacturerRoute, 
+            ProductsByTagRoute, 
+            RecentlyAddedProductsRoute
+        };
+
+        public static string[] AllProductListsWithRssRoutes { get; } = new[]
+        {
+            CategoryRoute,
+            ManufacturerRoute,
+            ProductsByTagRoute,
+            RecentlyAddedProductsRoute,
+            RecentlyAddedProductsRssRoute
+        };
+    }
+}
