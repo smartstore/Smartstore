5cd8dae362f5d1a1f19d0bf92a7f9c508ed41098
Murat Cakir <muratc@smartstore.de>
2023-05-17 03:06:14 +0200

    RequestCache improvements

---
+++ src/Smartstore/Caching/Extensions/IRequestCacheExtensions.cs
@@ -0,0 +1,113 @@
+﻿using System.Text.RegularExpressions;
+using Smartstore.Utilities;
+
+namespace Smartstore.Caching
+{
+    public static class IRequestCacheExtensions
+    {
+        /// <summary>
+        /// Removes all items whose keys match the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <param name="predicate">The predicate to match.</param>
+        /// <returns>The number of removed cache items.</returns>
+        public static int RemoveMany(this IRequestCache cache, Func<object, bool> predicate)
+        {
+            var items = cache.Items;
+            var keysToRemove = cache.SelectKeys(predicate).ToArray();
+
+            foreach (var key in keysToRemove)
+            {
+                items.Remove(key);
+            }
+
+            return keysToRemove.Length;
+        }
+
+        /// <summary>
+        /// Removes all items whose string type keys match the given <paramref name="pattern"/>.
+        /// </summary>
+        /// <param name="pattern">
+        ///     The string pattern to match.
+        ///     <para>
+        ///         Supported glob-style patterns:
+        ///         - h?llo matches hello, hallo and hxllo
+        ///         - h*llo matches hllo and heeeello
+        ///         - h[ae]llo matches hello and hallo, but not hillo
+        ///         - h[^e]llo matches hallo, hbllo, ... but not hello
+        ///         - h[a-b]llo matches hallo and hbllo
+        ///     </para>
+        /// </param>
+        /// <returns>The number of removed cache items.</returns>
+        public static int RemoveByPattern(this IRequestCache cache, string pattern)
+        {
+            var items = cache.Items;
+            var keysToRemove = cache.SelectKeys(pattern).ToArray();
+
+            foreach (var key in keysToRemove)
+            {
+                items.Remove(key);
+            }
+
+            return keysToRemove.Length;
+        }
+
+        /// <summary>
+        /// Returns all string type keys that match the given <paramref name="pattern"/>.
+        /// </summary>
+        /// <param name="pattern">
+        ///     The string pattern to match.
+        ///     <para>
+        ///         Supported glob-style patterns:
+        ///         - h?llo matches hello, hallo and hxllo
+        ///         - h*llo matches hllo and heeeello
+        ///         - h[ae]llo matches hello and hallo, but not hillo
+        ///         - h[^e]llo matches hallo, hbllo, ... but not hello
+        ///         - h[a-b]llo matches hallo and hbllo
+        ///     </para>
+        /// </param>
+        /// <returns>List of matching keys.</returns>
+        public static IEnumerable<object> SelectKeys(this IRequestCache cache, string pattern)
+        {
+            var wildcard = pattern.IsEmpty() || pattern == "*"
+                ? null
+                : new Wildcard(pattern, RegexOptions.IgnoreCase);
+
+            return SelectKeys(cache, x => KeyMatcher(x, wildcard)).Cast<string>();
+
+            static bool KeyMatcher(object key, Wildcard w)
+            {
+                if (key is string str)
+                {
+                    return w == null || w.IsMatch(str);
+                }
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns all keys that match the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <param name="selector">The predicate to match.</param>
+        /// <returns>List of matching keys.</returns>
+        public static IEnumerable<object> SelectKeys(this IRequestCache cache, Func<object, bool> predicate)
+        {
+            Guard.NotNull(cache);
+            Guard.NotNull(predicate);
+
+            var items = cache.Items;
+            if (items.Count == 0)
+            {
+                yield break;
+            }
+
+            foreach (var kvp in items)
+            {
+                if (predicate(kvp.Key))
+                {
+                    yield return kvp.Key;
+                }
+            }
+        }
+    }
+}
--- src/Smartstore/Caching/IRequestCache.cs
+++ src/Smartstore/Caching/IRequestCache.cs
@@ -1,4 +1,6 @@
-﻿namespace Smartstore.Caching
+﻿#nullable enable
+
+namespace Smartstore.Caching
 {
     /// <summary>
     /// Request cache interface
@@ -6,12 +8,17 @@
     public interface IRequestCache
     {
         /// <summary>
+        /// Gets the underlying store.
+        /// </summary>
+        IDictionary<object, object?> Items { get; }
+
+        /// <summary>
         /// Gets a cache item associated with the specified key
         /// </summary>
         /// <typeparam name="T">The type of the item to get</typeparam>
         /// <param name="key">The cache item key</param>
         /// <returns>Cached item value or <c>null</c> if item with specified key does not exist in the cache</returns>
-        T Get<T>(string key);
+        T? Get<T>(object key);
 
         /// <summary>
         /// Gets a cache item associated with the specified key or adds the item
@@ -21,7 +28,7 @@
         /// <param name="key">The cache item key</param>
         /// <param name="acquirer">Func which returns value to be added to the cache</param>
         /// <returns>Cached item value</returns>
-        T Get<T>(string key, Func<T> acquirer);
+        T Get<T>(object key, Func<T> acquirer);
 
         /// <summary>
         /// Gets a cache item associated with the specified key or adds the item
@@ -31,39 +38,34 @@
         /// <param name="key">The cache item key</param>
         /// <param name="acquirer">Func which returns value to be added to the cache</param>
         /// <returns>Cached item value</returns>
-        Task<T> GetAsync<T>(string key, Func<Task<T>> acquirer);
+        Task<T> GetAsync<T>(object key, Func<Task<T>> acquirer);
 
         /// <summary>
         /// Adds a cache item with the specified key
         /// </summary>
         /// <param name="key">Key</param>
         /// <param name="value">Value</param>
-        void Put(string key, object value);
+        void Put(object key, object? value);
 
         /// <summary>
         /// Gets a value indicating whether the value associated with the specified key is cached
         /// </summary>
         /// <param name="key">key</param>
-        /// <returns>Result</returns>
-        bool Contains(string key);
+        bool Contains(object key);
 
         /// <summary>
         /// Removes the value with the specified key from the cache
         /// </summary>
-        /// <param name="key">/key</param>
-        void Remove(string key);
-
-        /// <summary>
-        /// Removes items by pattern
-        /// </summary>
-        /// <param name="pattern">pattern</param>
-        void RemoveByPattern(string pattern);
+        /// <param name="key">key</param>
+        void Remove(object key);
 
         /// <summary>
         /// Clear all cache data
         /// </summary>
         void Clear();
     }
+
+#nullable disable
 
     /// <summary>
     /// For testing purposes
@@ -72,32 +74,26 @@
     {
         public static NullRequestCache Instance { get; } = new NullRequestCache();
 
-        public void Clear()
-        { 
+        public IDictionary<object, object> Items
+        {
+            get => new Dictionary<object, object>();
         }
 
-        public bool Contains(string key)
-            => false;
-
-        public T Get<T>(string key)
+        public T Get<T>(object key)
             => default;
 
-        public T Get<T>(string key, Func<T> acquirer)
+        public T Get<T>(object key, Func<T> acquirer)
             => acquirer == null ? default : acquirer();
 
-        public Task<T> GetAsync<T>(string key, Func<Task<T>> acquirer)
+        public Task<T> GetAsync<T>(object key, Func<Task<T>> acquirer)
             => acquirer == null ? default : acquirer();
 
-        public void Remove(string key)
-        { 
-        }
+        public void Put(object key, object value) { }
 
-        public void RemoveByPattern(string pattern)
-        { 
-        }
+        public void Clear() { }
 
-        public void Put(string key, object value)
-        { 
-        }
+        public bool Contains(object key) => false;
+
+        public void Remove(object key) { }
     }
 }
