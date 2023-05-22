173153037c5d2f5839b1b2b7dd8417be51677895
Michael Herzog <herzog@smartstore.com>
2023-05-22 11:36:56 +0200

    Fixed typos for Adresszusatz (was Adressszusatz everywhere)

--- src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
+++ src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
@@ -32,6 +32,20 @@ namespace Smartstore.Core.Data.Migrations
             builder.AddOrUpdate("Admin.ContentManagement.Menus.Item.InvalidTargetLink",
                 "Unknown or invalid target \"{0}\" at menu link \"{1}\".",
                 "Unbekanntes oder ungültiges Ziel \"{0}\" bei Menü-Link \"{1}\".");
+
+            builder.AddOrUpdate("Account.Fields.StreetAddress2", "Street address 2", "Adresszusatz");
+            builder.AddOrUpdate("Address.Fields.Address2", "Address 2", "Adresszusatz");
+            builder.AddOrUpdate("Address.Fields.Address2.Required", "Address 2 is required.", "Adresszusatz wird benötigt");
+            builder.AddOrUpdate("Admin.Address.Fields.Address2", "Address 2", "Adresszusatz");
+            builder.AddOrUpdate("Admin.Address.Fields.Address2.Hint", "Enter address 2", "Adresszusatz bzw. zweite Adresszeile");
+            builder.AddOrUpdate("Admin.Configuration.Settings.CustomerUser.StreetAddress2Enabled", "'Street address addition' enabled", "\"Adresszusatz\" aktiv");
+            builder.AddOrUpdate("Admin.Configuration.Settings.CustomerUser.StreetAddress2Enabled.Hint", "Defines whether the input of 'street address addition' is enabled.", "Legt fest, ob das Eingabefeld \"Adresszusatz\" aktiviert ist");
+            builder.AddOrUpdate("Admin.Configuration.Settings.CustomerUser.StreetAddress2Required", "'Street address addition' required", "\"Adresszusatz\" ist erforderlich");
+            builder.AddOrUpdate("Admin.Configuration.Settings.CustomerUser.StreetAddress2Required.Hint", "Defines whether the input of 'street address addition' is required.", "Legt fest, ob die Eingabe von \"Adresszusatz\" erforderlich ist.");
+            builder.AddOrUpdate("Admin.Customers.Customers.Fields.StreetAddress2", "Address 2", "Adresszusatz");
+            builder.AddOrUpdate("Admin.Customers.Customers.Fields.StreetAddress2.Hint", "The address 2.", "Adresszusatz");
+            builder.AddOrUpdate("Admin.Orders.Address.Address2", "Address 2", "Adresszusatz");
+            builder.AddOrUpdate("PDFPackagingSlip.Address2", "Address 2: {0}", "Adresszusatz: {0}");
         }
     }
 }
