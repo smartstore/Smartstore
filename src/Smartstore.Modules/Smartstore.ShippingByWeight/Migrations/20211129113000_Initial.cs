1b13f7e2c7323b8cb7284d141a0a5db0a5e893b3
mgesing <gesing@smartstore.com>
2023-05-25 10:25:30 +0200

    ShippingByWeight: fixed missing SmallQuantityThreshold in initial migration
    (cherry picked from commit d22703bc6ccbc5f50d98f48aa5b9c077c49074d0)

--- src/Smartstore.Modules/Smartstore.ShippingByWeight/Migrations/20211129113000_Initial.cs
+++ src/Smartstore.Modules/Smartstore.ShippingByWeight/Migrations/20211129113000_Initial.cs
@@ -23,7 +23,8 @@ namespace Smartstore.ShippingByWeight.Migrations
                     .WithColumn(nameof(ShippingRateByWeight.UsePercentage)).AsBoolean().NotNullable()
                     .WithColumn(nameof(ShippingRateByWeight.ShippingChargePercentage)).AsDecimal(18, 2).NotNullable()
                     .WithColumn(nameof(ShippingRateByWeight.ShippingChargeAmount)).AsDecimal(18, 2).NotNullable()
-                    .WithColumn(nameof(ShippingRateByWeight.SmallQuantitySurcharge)).AsDecimal(18, 2).NotNullable();
+                    .WithColumn(nameof(ShippingRateByWeight.SmallQuantitySurcharge)).AsDecimal(18, 2).NotNullable()
+                    .WithColumn(nameof(ShippingRateByWeight.SmallQuantityThreshold)).AsDecimal(18, 2).NotNullable();
             }
         }
 
