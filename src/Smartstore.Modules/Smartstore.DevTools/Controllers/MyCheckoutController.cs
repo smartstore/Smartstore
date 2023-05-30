05c44d781081ae39fc3f38d0374bec2daa3cf5de
mgesing <gesing@smartstore.com>
2023-05-30 20:48:21 +0200

    DevTools: added a sample of how to add a route menu item for an action method

--- src/Smartstore.Modules/Smartstore.DevTools/Controllers/MyCheckoutController.cs
+++ src/Smartstore.Modules/Smartstore.DevTools/Controllers/MyCheckoutController.cs
@@ -10,5 +10,15 @@ namespace Smartstore.DevTools.Controllers
         {
             return View();
         }
+
+        // Example of how to add a route menu item for this action method under CMS > Menus.
+        // - Add menu item > Route
+        // - Target (Route name): Smartstore.DevTools.MenuItem
+        // - Route values (JSON): {"id":16,"name":"hello world!"}
+        //[Route("/mycheckout/menuitem/{id?}", Name = "Smartstore.DevTools.MenuItem")]
+        //public IActionResult MenuItem(int id, string name)
+        //{
+        //    return Content($"DevTools Menu Item... id:{id} name:{name}");
+        //}
     }
 }
