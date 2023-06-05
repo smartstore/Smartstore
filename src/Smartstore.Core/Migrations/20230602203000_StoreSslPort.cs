d2a4319e13c9b7bc1773a8dd856855e6d3dec342
Murat Cakir <muratc@smartstore.de>
2023-06-05 20:24:02 +0200

    Code style

--- src/Smartstore.Core/Migrations/20230602203000_StoreSslPort.cs
+++ src/Smartstore.Core/Migrations/20230602203000_StoreSslPort.cs
@@ -6,13 +6,23 @@ namespace Smartstore.Core.Data.Migrations
     [MigrationVersion("2023-06-02 20:30:00", "Core: StoreSslPort")]
     internal class StoreSslPortMigration : Migration
     {
+        const string StoreTable = nameof(Store);
+        const string SslPortColumn = nameof(Store.SslPort);
+
         public override void Up()
         {
-            Create.Column(nameof(Store.SslPort)).OnTable(nameof(Store)).AsInt32().Nullable();
+            if (!Schema.Table(StoreTable).Column(SslPortColumn).Exists())
+            {
+                Create.Column(nameof(Store.SslPort)).OnTable(nameof(Store)).AsInt32().Nullable();
+            }
         }
 
         public override void Down()
         {
+            if (Schema.Table(StoreTable).Column(SslPortColumn).Exists())
+            {
+                Delete.Column(SslPortColumn).FromTable(StoreTable);
+            }
         }
     }
 }
