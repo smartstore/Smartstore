77cca2088914026819b59e9791d7f868b2fa87d6
mgesing <gesing@smartstore.com>
2023-06-16 09:57:07 +0200

    Fixes rare DbMigrationException "The ALTER TABLE statement conflicted with the FOREIGN KEY SAME TABLE constraint FK_Category_ParentCategoryId_Category_Id" in TreeNodeMigration

--- src/Smartstore.Core/Migrations/20230404150000_TreeNode.cs
+++ src/Smartstore.Core/Migrations/20230404150000_TreeNode.cs
@@ -11,6 +11,8 @@ namespace Smartstore.Core.Data.Migrations
     {
         public override void Up()
         {
+            FixInvalidParentCategoryIds();
+
             // Make Category.ParentCategoryId nullable and create FK
             Alter.Column("ParentCategoryId").OnTable(nameof(Category)).AsInt32().Nullable();
 
@@ -74,6 +76,20 @@ namespace Smartstore.Core.Data.Migrations
                         BuildTreePath(childNode);
                     }
                 }
+            }
+        }
+
+        private void FixInvalidParentCategoryIds()
+        {
+            try
+            {
+                IfDatabase("sqlserver").Execute.Sql(@"
+                    UPDATE [dbo].[Category] SET ParentCategoryId = 0
+                    WHERE [Id] In (SELECT [Id] FROM [dbo].[Category] c1 WHERE c1.ParentCategoryId <> 0 AND NOT EXISTS (SELECT 1 FROM [dbo].[Category] c2 WHERE c2.Id = c1.ParentCategoryId))
+                ");
+            }
+            catch
+            {
             }
         }
     }
