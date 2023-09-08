56cefe90b3f06c7f73f66bbe972381fc72af012e
Zackery Anderson <zackatack108@gmail.com>
2023-09-08 14:23:54 -0600

    changes from class

--- test/Smartstore.Web.Tests/Common/Controllers/SearchControllerTests.cs
+++ test/Smartstore.Web.Tests/Common/Controllers/SearchControllerTests.cs
@@ -7,6 +7,9 @@ using Smartstore.Web.Controllers;
 using Smartstore.Web.Models.Search;
 using Microsoft.AspNetCore.Mvc;
 using Smartstore.Web.Models.Catalog;
+using Smartstore.Collections;
+using Moq;
+using Smartstore.Core.Catalog.Products;
 
 namespace Smartstore.Web.Tests.Common.Controllers;
 
@@ -17,7 +20,7 @@ public class SearchControllerTests
     public async Task Term_Not_Found()
     {
         SearchSettings settings = new SearchSettings();
-        settings.InstantSearchTermMinLength = 1;
+        settings.InstantSearchTermMinLength = 2;
 
         SearchController controller = new(null, null, null, null, settings, null, null, null);
         CatalogSearchQuery query = new CatalogSearchQuery();
@@ -30,8 +33,33 @@ public class SearchControllerTests
         expected_result.Error = "Search.SearchTermMinimumLengthIsNCharacters";
 
         //need to mock db for this assert
-        //Assert.AreEqual(expected_result.SearchResult, actual.SearchResult);
+        Assert.AreEqual(expected_result.SearchResult.TotalHitsCount, actual.SearchResult.TotalHitsCount);
         Assert.AreEqual(expected_result.TopProducts, actual.TopProducts);
         Assert.AreEqual(expected_result.Error, actual.Error);
+    }
+
+    [Test]
+    public async Task Search_Item()
+    {
+        SearchSettings settings = new SearchSettings();
+        settings.InstantSearchTermMinLength = 2;
+
+        SearchController controller = new(null, null, null, null, settings, null, null, null);
+        CatalogSearchQuery query = new CatalogSearchQuery();
+        query.DefaultTerm = "baseball";
+        var actual = await controller.GetSearchResultModel(query);
+
+        SearchResultModel expected_result = new SearchResultModel(query);
+        expected_result.SearchResult = new CatalogSearchResult(query);
+
+        var pagedMock = new Mock<IPagedList<Product>>();
+        var pagedList = pagedMock.Object;
+
+        pagedMock.Setup(x => x.it);
+
+        expected_result.TopProducts = new ProductSummaryModel(pagedList);
+
+        //need to mock db for this assert
+        Assert.AreEqual(expected_result.TopProducts.Items.Count, actual.TopProducts.Items.Count);
     }
 }
