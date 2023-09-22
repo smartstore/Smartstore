3a0ef747a3dea062551e15d5a420d8429f1f6230
Michael Herzog <herzog@smartstore.com>
2023-09-22 10:38:34 +0200

    PayPal CreditCard: Removed obsolete fields for address data

--- src/Smartstore.Modules/Smartstore.PayPal/Models/PublicCreditCardModel.cs
+++ src/Smartstore.Modules/Smartstore.PayPal/Models/PublicCreditCardModel.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using FluentValidation;
+﻿using FluentValidation;
 
 namespace Smartstore.PayPal.Models
 {
@@ -7,31 +6,6 @@ namespace Smartstore.PayPal.Models
     {
         [LocalizedDisplay("Payment.CardholderName")]
         public string CardholderName { get; set; }
-
-        [LocalizedDisplay("Address.Fields.City")]
-        public string City { get; set; }
-
-        [LocalizedDisplay("Address.Fields.Address1")]
-        public string Address1 { get; set; }
-
-        [LocalizedDisplay("Address.Fields.Address2")]
-        public string Address2 { get; set; }
-
-        [LocalizedDisplay("Address.Fields.ZipPostalCode")]
-        public string ZipPostalCode { get; set; }
-
-        [LocalizedDisplay("Address.Fields.Country")]
-        public string Country { get; set; }
-
-        [LocalizedDisplay("Address.Fields.Country")]
-        [UIHint("Countries")]
-        public int? CountryId { get; set; }
-
-        [LocalizedDisplay("Address.Fields.StateProvince")]
-        public int? StateProvinceId { get; set; }
-
-        [LocalizedDisplay("Address.Fields.StateProvince")]
-        public string StateProvince { get; set; }
 
         /// <summary>
         /// A value indicating whether the client token was retrieved from the PayPal API.
@@ -44,18 +18,6 @@ namespace Smartstore.PayPal.Models
         public PayPalCreditCardValidator(Localizer T)
         {
             RuleFor(x => x.CardholderName).NotEmpty();
-            RuleFor(x => x.City).NotEmpty();
-            RuleFor(x => x.Address1).NotEmpty();
-            RuleFor(x => x.Address2).NotEmpty();
-            RuleFor(x => x.ZipPostalCode).NotEmpty();
-
-            RuleFor(x => x.StateProvinceId)
-                .NotEmpty()
-                .WithMessage(T("Plugins.Smartstore.PayPal.StateProvince.NotEmpty"));
-
-            RuleFor(x => x.CountryId)
-                .NotEmpty()
-                .WithMessage(T("Plugins.Smartstore.PayPal.Country.NotEmpty"));
         }
     }
 }
