02117c741fa538c377ccc5d36c39cb575336adc6
Murat Cakir <muratc@smartstore.de>
2023-10-21 03:21:17 +0200

    GracefulDynamicObject

---
+++ src/Smartstore/ComponentModel/GracefulDynamicObject.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+
+using System.Dynamic;
+
+namespace Smartstore.ComponentModel
+{
+    /// <summary>
+    /// A simple <see cref="DynamicObject"/> implementaion that does not throw
+    /// when accessed member was not found.
+    /// </summary>
+    public class GracefulDynamicObject : DynamicObject
+    {
+        private readonly Dictionary<string, object?> _data;
+
+        public GracefulDynamicObject(bool ignoreCase = false)
+        {
+            _data = ignoreCase ? new Dictionary<string, object?>(StringComparer.OrdinalIgnoreCase) : new Dictionary<string, object?>();
+        }
+
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            return _data.Keys;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object? result)
+        {
+            Guard.NotNull(binder);
+
+            _data.TryGetValue(binder.Name, out result);
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object? value)
+        {
+            Guard.NotNull(binder);
+
+            _data[binder.Name] = value;
+            return true;
+        }
+    }
+}
