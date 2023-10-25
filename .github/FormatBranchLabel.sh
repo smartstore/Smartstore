88061338f0e971cf54263cb2f9bc67bd79dfa8dc
markwoerdenweber <116349750+markwoerdenweber@users.noreply.github.com>
2023-10-25 12:23:24 +0200

    Added FormatBranchLabel.sh for publish-release-docker Workflow

---
+++ .github/FormatBranchLabel.sh
@@ -0,0 +1,9 @@
+# Get the Linux and Windows branch name from the GitHub action's input
+BRANCH_NAME_LINUX=$GITHUB_REF_NAME
+BRANCH_NAME_WINDOWS=$env:GITHUB_REF_NAME
+
+# Use the Windows branch name if the linux branch name is unset
+BRANCH_NAME=${BRANCH_NAME_LINUX:-BRANCH_NAME_WINDOWS}
+
+# Remove '.x' from the end of the branch name.
+BRANCH_NAME=${BRANCH_NAME%.x}
