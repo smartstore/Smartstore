7eac37ccad1f24120a2d8a544ad228b91dc95a4d
Michael Herzog <herzog@smartstore.com>
2023-10-27 11:16:44 +0200

    Updated resources for X (Twitter)

--- src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
+++ src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
@@ -410,6 +410,19 @@ namespace Smartstore.Core.Data.Migrations
             builder.AddOrUpdate("Admin.System.Maintenance.StartDateMustBeBeforeEndDate",
                 "The start date must not be after the end date.",
                 "Das Anfangsdatum darf nicht nach dem Enddatum liegen.");
+
+            builder.AddOrUpdate("Admin.Configuration.Settings.GeneralCommon.SocialSettings.TwitterLink",
+                "X (Twitter) link",
+                "X (Twitter) Link");
+
+            builder.AddOrUpdate("Admin.Configuration.Settings.GeneralCommon.SocialSettings.TwitterSite",
+                "X (Twitter) Username",
+                "Benutzername auf X (Twitter)");
+
+            builder.AddOrUpdate("Admin.Configuration.Settings.GeneralCommon.SocialSettings.TwitterSite.Hint",
+                "X (Twitter) username that gets displayed on X (Twitter) cards when a product, category and manufacturer page is shared on X (Twitter). Starts with a '@'.",
+                "Benutzername auf X (Twitter), der auf Karten von X (Twitter) angezeigt wird, wenn ein Produkt, eine Kategorie oder eine Herstellerseite auf X (Twitter) geteilt wird. Beginnt mit einem '@'.");
+
         }
 
         /// <summary>
