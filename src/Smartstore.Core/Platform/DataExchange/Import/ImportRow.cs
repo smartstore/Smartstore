317b3276f1c6ded5a33b8c5d4068966872e56aba
Murat Cakir <muratc@smartstore.de>
2024-03-21 20:42:59 +0100

    ImportRow / StringLengthAttribute review

--- src/Smartstore.Core/Platform/DataExchange/Import/ImportRow.cs
+++ src/Smartstore.Core/Platform/DataExchange/Import/ImportRow.cs
@@ -205,6 +205,7 @@ namespace Smartstore.Core.DataExchange.Import
                     {
                         converted = value.Convert<TProp>(_segmenter.Culture);
 
+                        // TODO: (mg) (perf)!!! Don't do this please. This needs a decent caching strategy.
                         if (pi.TryGetAttribute<StringLengthAttribute>(false, out var attribute) && attribute.MaximumLength > 0)
                         {
                             converted = converted.ToString().Truncate(attribute.MaximumLength);
