cd3086e5d84c4fd2719a62d3edde0fc0a5a0f10d
Murat Cakir <muratc@smartstore.de>
2024-04-27 04:15:46 +0200

    Fixes #1070: Razor runtime compilation was broken due to missing reference paths

--- src/Smartstore/Engine/Modularity/ApplicationParts/HostRazorAssemblyPart.cs
+++ src/Smartstore/Engine/Modularity/ApplicationParts/HostRazorAssemblyPart.cs
@@ -1,13 +1,14 @@
 ﻿using System.Reflection;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.AspNetCore.Razor.Hosting;
+using Microsoft.Extensions.DependencyModel;
 
 namespace Smartstore.Engine.Modularity.ApplicationParts
 {
     /// <summary>
     /// An <see cref="ApplicationPart"/> for compiled host/web Razor assemblies.
     /// </summary>
-    public class HostRazorAssemblyPart : ApplicationPart, IRazorCompiledItemProvider, IApplicationPartTypeProvider
+    public class HostRazorAssemblyPart : ApplicationPart, IRazorCompiledItemProvider, IApplicationPartTypeProvider, ICompilationReferencesProvider
     {
         public HostRazorAssemblyPart(Assembly assembly)
         {
@@ -29,6 +30,29 @@ namespace Smartstore.Engine.Modularity.ApplicationParts
                 var loader = new HostRazorCompiledItemLoader();
                 return loader.LoadItems(Assembly);
             }
+        }
+
+        IEnumerable<string> ICompilationReferencesProvider.GetReferencePaths()
+        {
+            if (Assembly.IsDynamic)
+            {
+                // Skip loading process for dynamic assemblies. This prevents DependencyContextLoader from reading the
+                // .deps.json file from either manifest resources or the assembly location, which will fail.
+                return [];
+            }
+
+            var dependencyContext = DependencyContext.Load(Assembly);
+            if (dependencyContext != null)
+            {
+                var paths = dependencyContext.CompileLibraries.SelectMany(library => library.ResolveReferencePaths()).ToArray();
+                return paths;
+            }
+
+            // If an application has been compiled without preserveCompilationContext, return the path to the assembly
+            // as a reference. For runtime compilation, this will allow the compilation to succeed as long as it least
+            // one application part has been compiled with preserveCompilationContext and contains a super set of types
+            // required for the compilation to succeed.
+            return [Assembly.Location];
         }
     }
 }
