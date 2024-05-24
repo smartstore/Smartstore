ba73f62ab0feb375fd55baedf08e4e3c8a0f27e7
Murat Cakir <muratc@smartstore.de>
2024-05-24 20:14:46 +0200

    (Reverted) TrackActivityAttribute: don't check for AJAX

--- src/Smartstore.Core/Platform/Identity/Filters/TrackActivityAttribute.cs
+++ src/Smartstore.Core/Platform/Identity/Filters/TrackActivityAttribute.cs
@@ -82,7 +82,7 @@ namespace Smartstore.Core.Identity
 
         private void DoTrack(ActionExecutingContext context)
         {
-            if (!context.HttpContext.Request.IsGet())
+            if (!context.HttpContext.Request.IsNonAjaxGet())
             {
                 return;
             }
