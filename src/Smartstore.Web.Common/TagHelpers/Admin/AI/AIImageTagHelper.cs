bff100f694bcb15659f75507da42a82c63c5105a
Murat Cakir <muratc@smartstore.de>
2024-11-01 01:36:56 +0100

    AI review (TODO for mh)

--- src/Smartstore.Web.Common/TagHelpers/Admin/AI/AIImageTagHelper.cs
+++ src/Smartstore.Web.Common/TagHelpers/Admin/AI/AIImageTagHelper.cs
@@ -4,6 +4,8 @@ using Smartstore.Core.AI;
 
 namespace Smartstore.Web.TagHelpers.Admin
 {
+    // TODO: (mh) (ai) Should the image tool still be rendered even if the entity id is 0 or null?
+    
     /// <summary>
     /// Renders a button or dropdown (depending on the number of active AI providers) to open a dialog for image creation.
     /// </summary>
