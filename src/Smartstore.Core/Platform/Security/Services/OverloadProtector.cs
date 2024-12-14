b2fb215ec2c9d9fd49d44f76b6bbdc4c8f941deb
Murat Cakir <muratc@smartstore.de>
2024-12-14 04:24:58 +0100

    Overload protector (wip)

---
+++ src/Smartstore.Core/Platform/Security/Services/OverloadProtector.cs
@@ -0,0 +1,25 @@
+﻿namespace Smartstore.Core.Security
+{
+    public class OverloadProtector : IOverloadProtector
+    {
+        public Task<bool> DenyGuestAsync()
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<bool> DenyCustomerAsync()
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<bool> DenyBotAsync()
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<bool> ForbidNewGuestAsync()
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
