b31fa8a7633213d280cd79e8b320721d2ce698fa
mgesing <gesing@smartstore.com>
2024-12-17 09:39:22 +0100

    Migration to renew attribute combination hash codes
    (cherry picked from commit f1091472f750f44e3de68120a739354bbf920964)

---
+++ src/Smartstore.Core/Migrations/20241217093000_RenewAttributeHashCodes.cs
@@ -0,0 +1,45 @@
+﻿using FluentMigrator;
+using Smartstore.Data.Migrations;
+
+namespace Smartstore.Core.Data.Migrations
+{
+    [MigrationVersion("2024-12-17 09:30:00", "Core: Renew attribute combination hash codes")]
+    internal class RenewAttributeHashCodes : Migration, IDataSeeder<SmartDbContext>
+    {
+        private readonly SmartDbContext _db;
+        private readonly ILogger _logger;
+
+        public RenewAttributeHashCodes(SmartDbContext db, ILogger logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+
+        public DataSeederStage Stage => DataSeederStage.Late;
+        public bool AbortOnFailure => false;
+
+        public async Task SeedAsync(SmartDbContext context, CancellationToken cancelToken = default)
+        {
+            try
+            {
+                var migrator = new AttributesMigrator(_db, _logger);
+                var numUpdated = await migrator.CreateAttributeCombinationHashCodesAsync(true, cancelToken);
+
+                _logger.Debug($"Renewed hash codes for {numUpdated} attribute combinations.");
+            }
+            catch (Exception ex)
+            {
+                // Do not break any other data seeding. Hash code creation can be restarted on maintenance page.
+                _logger.Error(ex);
+            }
+        }
+    }
+}
