2c69852cd558e5cf243ea85721aec44c7705cc8d
Murat Cakir <muratc@smartstore.de>
2024-12-18 04:35:57 +0100

    Overload protector (wip)
    (cherry picked from commit 3d59c1452e65a65c23fec794bc09d3f436cb8032)

--- src/Smartstore.Core/Platform/Security/Services/IOverloadProtector.cs
+++ src/Smartstore.Core/Platform/Security/Services/IOverloadProtector.cs
@@ -1,31 +1,60 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿#nullable enable
+
+using Microsoft.AspNetCore.Http;
+using Smartstore.Core.Identity;
+using Smartstore.Core.Web;
 
 namespace Smartstore.Core.Security
 {
     /// <summary>
     /// Provides methods to protect the system from overload attacks, 
-    /// like too many guest users, too many bots, etc.
+    /// such as too many guest users, excessive bot activity, etc.
     /// </summary>
     public interface IOverloadProtector
     {
         /// <summary>
-        /// Returns <c>true</c> if the guest user should be denied due to the guest user rate limit policy.
+        /// Determines if a guest user should be denied access due to exceeding 
+        /// the allowed rate limit for guest users. 
         /// </summary>
-        Task<bool> DenyGuestAsync();
+        /// <remarks>
+        /// The rate limit is applied based on a predefined policy, which 
+        /// include thresholds for requests per time window and peak detection strategies. 
+        /// If the guest user exceeds the limits, this method will return <c>true</c>, 
+        /// indicating that the request should be denied.
+        /// </remarks>
+        /// <returns>
+        /// A <see cref="Task{TResult}"/> representing the asynchronous operation, 
+        /// containing a <see cref="bool"/> that indicates whether the guest user should be denied.
+        /// </returns>
+        Task<bool> DenyGuestAsync(Customer? customer = null);
 
         /// <summary>
-        /// Returns <c>true</c> if the customer should be denied due to the authenticated customer rate limit policy.
+        /// Determines if a bot should be denied access due to exceeding the allowed 
+        /// rate limit for bot activity.
         /// </summary>
-        Task<bool> DenyCustomerAsync();
+        /// <remarks>
+        /// Bots typically have stricter rate-limiting policies compared to guests, 
+        /// as they may generate high volumes of traffic. This method applies a bot-specific 
+        /// rate limit to protect the system from excessive bot activity. If the bot exceeds 
+        /// these limits, this method will return <c>true</c>.
+        /// </remarks>
+        /// <returns>
+        /// A <see cref="Task{TResult}"/> representing the asynchronous operation, 
+        /// containing a <see cref="bool"/> that indicates whether the bot should be denied.
+        /// </returns>
+        Task<bool> DenyBotAsync(IUserAgent userAgent);
 
         /// <summary>
-        /// Returns <c>true</c> if the bot should be denied due to the bot rate limit policy.
+        /// Determines if access for new guest users should be forbidden based on the current resiliency policy.
         /// </summary>
-        Task<bool> DenyBotAsync();
-
-        /// <summary>
-        /// Returns <c>true</c> if access should be forbidden for new guest users.
-        /// </summary>
-        Task<bool> ForbidNewGuestAsync(HttpContext httpContext);
+        /// <remarks>
+        /// This can be used to temporarily or permanently block new guests 
+        /// during periods of high load or maintenance.
+        /// </remarks>
+        /// <returns>
+        /// A <see cref="Task{TResult}"/> representing the asynchronous operation, 
+        /// containing a <see cref="bool"/> that indicates whether new guest users should be forbidden.
+        /// </returns>
+        Task<bool> ForbidNewGuestAsync(HttpContext? httpContext);
     }
 }
