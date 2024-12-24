1471ae4457e53dc69b5c45d7d62115b17a2d59e6
Murat Cakir <muratc@smartstore.de>
2024-12-24 05:11:12 +0100

    Performance settings (localization)

--- src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
+++ src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
@@ -1,4 +1,6 @@
+using Smartstore.Core.Common.Configuration;
 using Smartstore.Core.Configuration;
+using Smartstore.Core.Security;
 using Smartstore.Data.Migrations;
 
 namespace Smartstore.Core.Data.Migrations
@@ -11,13 +13,14 @@ namespace Smartstore.Core.Data.Migrations
         public async Task SeedAsync(SmartDbContext context, CancellationToken cancelToken = default)
         {
             await context.MigrateLocaleResourcesAsync(MigrateLocaleResources);
-            //await MigrateSettingsAsync(context, cancelToken);
+            await MigrateSettingsAsync(context, cancelToken);
         }
 
-        //public async Task MigrateSettingsAsync(SmartDbContext db, CancellationToken cancelToken = default)
-        //{
-
-        //}
+        public async Task MigrateSettingsAsync(SmartDbContext context, CancellationToken cancelToken = default)
+        {
+            await SettingFactory.SaveSettingsAsync(context, new PerformanceSettings(), false);
+            await SettingFactory.SaveSettingsAsync(context, new ResiliencySettings(), false);
+        }
 
         public void MigrateLocaleResources(LocaleResourcesBuilder builder)
         {
@@ -48,6 +51,48 @@ namespace Smartstore.Core.Data.Migrations
             builder.AddOrUpdate("Common.CartRules", "Cart rules", "Warenkorbregeln");
             builder.AddOrUpdate("Common.CustomerRules", "Customer rules", "Kundenregeln");
             builder.AddOrUpdate("Common.ProductRules", "Product rules", "Produktregeln");
+
+            #region Performance settings
+
+            var prefix = "Admin.Configuration.Settings.Performance";
+
+            builder.AddOrUpdate($"{prefix}", "Performance", "Leistung");
+
+            builder.AddOrUpdate($"{prefix}.Hint",
+                "For technically experienced users only. Pay attention to the CPU and memory usage when changing these settings.",
+                "Nur für technisch erfahrene Benutzer. Achten Sie auf die CPU- und Speicherauslastung, wenn Sie diese Einstellungen ändern.");
+
+            builder.AddOrUpdate($"{prefix}.CacheSegmentSize",
+                "Cache segment size", 
+                "Cache Segment Größe",
+                "The number of entries in a single cache segment when greedy loading is disabled. The larger the catalog, the smaller this value should be. We recommend segment size of 500 for catalogs with less than 100.000 items.",
+                "Die Anzahl der Einträge in einem einzelnen Cache-Segment, wenn Greedy Loading deaktiviert ist. Je größer der Katalog ist, desto kleiner sollte dieser Wert sein. Wir empfehlen eine Segmentgröße von 500 für Kataloge mit weniger als 100.000 Einträgen.");
+
+            builder.AddOrUpdate($"{prefix}.AlwaysPrefetchTranslations",
+                "Always prefetch translations",
+                "Übersetzungen immer vorladen (Prefetch)",
+                "By default, only Instant Search prefetches translations. All other product listings work against the segmented cache. For very large multilingual catalogs (> 500,000), enabling this can improve query performance and reduce resource usage.",
+                "Standardmäßig werden nur bei der Sofortsuche Übersetzungen vorgeladen. Alle anderen Produktauflistungen arbeiten mit dem segmentierten Cache. Bei sehr großen mehrsprachigen Katalogen (> 500.000) kann die Aktivierung dieser Option die Abfrageleistung verbessern und die Ressourcennutzung verringern.");
+
+            builder.AddOrUpdate($"{prefix}.AlwaysPrefetchUrlSlugs",
+                "Always prefetch URL slugs",
+                "URL Slugs immer vorladen  (Prefetch)",
+                "By default, only Instant Search prefetches URL slugs. All other product listings work against the segmented cache. For very large multilingual catalogs (> 500,000), enabling this can improve query performance and reduce resource usage.",
+                "Standardmäßig werden nur bei der Sofortsuche URL slugs vorgeladen. Alle anderen Produktauflistungen arbeiten mit dem segmentierten Cache. Bei sehr großen mehrsprachigen Katalogen (> 500.000) kann die Aktivierung dieser Option die Abfrageleistung verbessern und die Ressourcennutzung verringern.");
+
+            builder.AddOrUpdate($"{prefix}.MaxUnavailableAttributeCombinations",
+                "Max. unavailable attribute combinations",
+                "Max. nicht verfügbare Attributkombinationen",
+                "Maximum number of attribute combinations that will be loaded and parsed to make them unavailable for selection on the product detail page.",
+                "Maximale Anzahl von Attributkombinationen, die geladen und analysiert werden, um nicht verfügbare Kombinationen zu ermitteln.");
+
+            builder.AddOrUpdate($"{prefix}.MediaDupeDetectorMaxCacheSize",
+                "Media Duplicate Detector max. cache size",
+                "Max. Cache-Größe für Medien-Duplikat-Detektor",
+                "Maximum number of MediaFile entities to cache for duplicate file detection. If a media folder contains more files, no caching is done for scalability reasons and the MediaFile entities are loaded directly from the database.",
+                "Maximale Anzahl der MediaFile-Entitäten, die für die Duplikat-Erkennung zwischengespeichert werden. Enthält ein Medienordner mehr Dateien, erfolgt aus Gründen der Skalierbarkeit keine Zwischenspeicherung und die MediaFile-Entitäten werden direkt aus der Datenbank geladen.");
+
+            #endregion
         }
     }
 }
