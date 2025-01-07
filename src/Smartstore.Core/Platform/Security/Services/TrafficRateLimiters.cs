5e2471a223acd820f6d1a19161c0a33bd83d6d3e
Murat Cakir <muratc@smartstore.de>
2025-01-07 01:14:47 +0100

    TrafficRateLimiters: stop & dispose
    (cherry picked from commit 43c68bbb1142183262f920a9bf7ca5e6c9488e5e)

--- src/Smartstore.Core/Platform/Security/Services/TrafficRateLimiters.cs
+++ src/Smartstore.Core/Platform/Security/Services/TrafficRateLimiters.cs
@@ -1,21 +1,21 @@
 ﻿#nullable enable
 
 using System.Threading.RateLimiting;
+using Microsoft.Extensions.Hosting;
 
 namespace Smartstore.Core.Security
 {
-    public class TrafficRateLimiters(ResiliencySettings settings)
+    public class TrafficRateLimiters(ResiliencySettings settings) : Disposable, IHostedService
     {
-        // Type-specific limiters
-        public RateLimiter? GuestLongLimiter { get; } = CreateTokenBucket(settings.LongTrafficLimitGuest, settings.LongTrafficWindow);
-        public RateLimiter? GuestPeakLimiter { get; } = CreateTokenBucket(settings.PeakTrafficLimitGuest, settings.PeakTrafficWindow);
+        // Long limiters
+        public RateLimiter? LongGuestLimiter { get; private set; } = CreateTokenBucket(settings.LongTrafficLimitGuest, settings.LongTrafficWindow);
+        public RateLimiter? LongBotLimiter { get; private set; } = CreateTokenBucket(settings.LongTrafficLimitBot, settings.LongTrafficWindow);
+        public RateLimiter? LongGlobalLimiter { get; private set; } = CreateTokenBucket(settings.LongTrafficLimitGlobal, settings.LongTrafficWindow);
 
-        public RateLimiter? BotLongLimiter { get; } = CreateTokenBucket(settings.LongTrafficLimitBot, settings.LongTrafficWindow);
-        public RateLimiter? BotPeakLimiter { get; } = CreateTokenBucket(settings.PeakTrafficLimitBot, settings.PeakTrafficWindow);
-
-        // Global limiters that are more generous
-        public RateLimiter? GlobalLongLimiter { get; } = CreateTokenBucket(settings.LongTrafficLimitGlobal, settings.LongTrafficWindow);
-        public RateLimiter? GlobalPeakLimiter { get; } = CreateTokenBucket(settings.PeakTrafficLimitGlobal, settings.PeakTrafficWindow);
+        // Peak limiters
+        public RateLimiter? PeakGuestLimiter { get; private set; } = CreateTokenBucket(settings.PeakTrafficLimitGuest, settings.PeakTrafficWindow);
+        public RateLimiter? PeakBotLimiter { get; private set; } = CreateTokenBucket(settings.PeakTrafficLimitBot, settings.PeakTrafficWindow);
+        public RateLimiter? PeakGlobalLimiter { get; private set; } = CreateTokenBucket(settings.PeakTrafficLimitGlobal, settings.PeakTrafficWindow);
 
         private static TokenBucketRateLimiter? CreateTokenBucket(int? limit, TimeSpan period)
         {
@@ -36,6 +36,47 @@ namespace Smartstore.Core.Security
                     QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
                     QueueLimit = 0 // No queue
                 });
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                Dispose();
+            }
+            catch
+            {
+                // Ignore exceptions thrown as a result of a cancellation.
+            }   
+
+            return Task.CompletedTask;
+        }
+
+        protected override void OnDispose(bool disposing)
+        {
+            if (disposing)
+            {
+                DisposeInternal();
+            }
+        }
+
+        private void DisposeInternal()
+        {
+            LongGuestLimiter?.Dispose();
+            LongGuestLimiter = null;
+            LongBotLimiter?.Dispose();
+            LongBotLimiter = null;
+            LongGlobalLimiter?.Dispose();
+            LongGlobalLimiter = null;
+            PeakGuestLimiter?.Dispose();
+            PeakGuestLimiter = null;
+            PeakBotLimiter?.Dispose();
+            PeakBotLimiter = null;
+            PeakGlobalLimiter?.Dispose();
+            PeakGlobalLimiter = null;
         }
     }
 }
