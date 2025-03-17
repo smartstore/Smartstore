e6e212ff379e87a456b1a88d16391dba286ce8ea
Murat Cakir <muratc@smartstore.de>
2025-03-18 00:08:32 +0100

    Dropdown fix review

--- src/Smartstore.Web/wwwroot/js/smartstore.globalinit.js
+++ src/Smartstore.Web/wwwroot/js/smartstore.globalinit.js
@@ -499,7 +499,7 @@ jQuery(function () {
 
         let dropdown = $(e.currentTarget).find('> .dropdown-toggle, > [data-toggle=dropdown]').data('bs.dropdown');
         if (!dropdown) {
-            dropdown = $(e.target).find('> .dropdown-toggle, > [data-toggle=dropdown]').data('bs.dropdown');
+            return;
         }
 
         let popperConfig = dropdown._config.popperConfig;
