8707a155cf7ca8910701c8ae8e6371efdee07817
mgesing <gesing@smartstore.com>
2025-05-13 16:05:41 +0200

    WCAG: Forms

--- src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
+++ src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
@@ -21,6 +21,12 @@ namespace Smartstore.Core.Data.Migrations
         public void MigrateLocaleResources(LocaleResourcesBuilder builder)
         {
             builder.AddOrUpdate("AriaLabel.MainNavigation", "Main navigation", "Hauptnavigation");
+            builder.AddOrUpdate("AriaLabel.SearchBox",
+                "Type in a search term and press Enter to search for products.",
+                "Geben Sie einen Suchbegriff ein und drücken Sie die Eingabetaste, um nach Produkten zu suchen.");
+
+            builder.AddOrUpdate("Search.SearchBox.Clear", "Clear search term", "Suchbegriff löschen");
+            builder.AddOrUpdate("Common.ScrollUp", "Scroll up", "Nach oben scrollen");
         }
     }
 }
