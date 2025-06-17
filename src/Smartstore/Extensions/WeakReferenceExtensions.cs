e5bb94fdf146b2ddaef5c5f395f384f0a4bf5335
Murat Cakir <muratc@smartstore.de>
2025-06-17 04:36:59 +0200

    WCAG: Styling

---
+++ src/Smartstore/Extensions/WeakReferenceExtensions.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+
+namespace Smartstore
+{
+    public static class WeakReferenceExtensions
+    {
+        /// <summary>
+        /// Gets the target of the weak reference if it is still alive.
+        /// </summary>
+        public static T? GetTarget<T>(this WeakReference<T> weakReference) where T : class
+        {
+            if (weakReference.TryGetTarget(out var  target))
+            {
+                return target;
+            }
+
+            return null;
+        }
+    }
+}
