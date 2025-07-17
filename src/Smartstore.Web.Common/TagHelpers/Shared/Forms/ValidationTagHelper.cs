630f5ca8fea9e046b4cee7bfc24f979f1501e612
mgesing <gesing@smartstore.com>
2025-07-17 12:40:46 +0200

    WCAG: `aria-live=assertive` is redundant if `role=alert` is already set. An element with `role=alert` is also always an assertive live region.

--- src/Smartstore.Web.Common/TagHelpers/Shared/Forms/ValidationTagHelper.cs
+++ src/Smartstore.Web.Common/TagHelpers/Shared/Forms/ValidationTagHelper.cs
@@ -13,13 +13,10 @@ namespace Smartstore.Web.TagHelpers.Shared
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             // Screen readers recognize a text change and read it out immediately.
+            // INFO: "aria-live=assertive" is redundant if "role=alert" is already set.
+            // An element with "role=alert" is also always an assertive live region.
+            // Using both can carry the risk of screen readers triggering double events.
             output.Attributes.SetAttributeNoReplace("role", "alert");
-
-            // A live region informs assistive technologies (AT) that the content may change without changing the focus, so it must be read.
-            output.Attributes.SetAttributeNoReplace("aria-live", "assertive");
-
-            // It ensures that the entire message will be read again when an update is made (not just updated parts). 
-            output.Attributes.SetAttributeNoReplace("aria-atomic", "true");
         }
     }
 }
