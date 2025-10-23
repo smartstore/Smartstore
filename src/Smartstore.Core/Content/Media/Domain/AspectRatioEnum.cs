89999d6f2e7ffec9582bcab062ca2003805dab04
Michael Herzog <herzog@smartstore.com>
2025-10-23 15:09:25 +0200

    Added AspectRatioEnum

---
+++ src/Smartstore.Core/Content/Media/Domain/AspectRatioEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace Smartstore.Core.Content.Media
+{
+    /// <summary>
+    /// Represents standardized aspect ratios for media content.
+    /// </summary>
+    public enum AspectRatio
+    {
+        AR1by1,
+        AR4by3,
+        AR16by9,
+        AR16by10,
+        AR21by9
+    }
+}
