4a20d730b87ad623ba4513eeffedffb88f36a13d
Murat Cakir <muratc@smartstore.de>
2025-10-23 20:39:56 +0200

    Blog review

--- src/Smartstore.Core/Content/Media/Domain/AspectRatioEnum.cs
+++ src/Smartstore.Core/Content/Media/Domain/AspectRatioEnum.cs
@@ -1,5 +1,9 @@
 ﻿namespace Smartstore.Core.Content.Media
 {
+    // TODO: (mh) Remove this enum
+    // TODO: (mh) Use a float number in blog settings instead (0.75, 0.625, 0.5625 etc.)
+    // TODO: (mh) Localization: 1:1 (square), 4:3 (standard), 16:9 (widescreen), 16:10 (HD), 21:9 (cinematic)
+
     /// <summary>
     /// Represents standardized aspect ratios for media content.
     /// </summary>
