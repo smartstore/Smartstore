b48c78636828c997c70d4add58b14c27e20b3318
mgesing <gesing@smartstore.com>
2025-10-30 13:33:48 +0100

    AI: Composite prompt

--- src/Smartstore.Core/Platform/AI/Prompting/IPromptContainer.cs
+++ src/Smartstore.Core/Platform/AI/Prompting/IPromptContainer.cs
@@ -5,18 +5,31 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace Smartstore.Core.AI.Prompting
 {
     /// <summary>
-    /// Model interface for prompt UI.
+    /// Represents an AI prompt.
     /// </summary>
     public interface IPromptContainer
     {
-        string? EntityName { get; }
+        /// <summary>
+        /// The final prompt to be sent to the AI service.
+        /// </summary>
         string? Prompt { get; }
     }
 
     /// <summary>
-    /// Model interface for the text generation prompt UI.
+    /// Represents a composite prompt consisting of a string template and user input.
     /// </summary>
-    public interface ITextPromptContainer : IPromptContainer
+    public interface ICompositePrompt : IPromptContainer
+    {
+        /// <summary>
+        /// The entity name/title that is used as the user input.
+        /// </summary>
+        string? EntityName { get; }
+    }
+
+    /// <summary>
+    /// Represents a prompt for text generation.
+    /// </summary>
+    public interface ITextPromptContainer : ICompositePrompt
     {
         int? WordLimit { get; }
         string? Style { get; }
@@ -31,9 +44,9 @@ namespace Smartstore.Core.AI.Prompting
     }
 
     /// <summary>
-    /// Model interface for the suggestion generation prompt UI.
+    /// Represents a prompt for generating suggestions.
     /// </summary>
-    public interface ISuggestionPromptContainer : IPromptContainer
+    public interface ISuggestionPromptContainer : ICompositePrompt
     {
         int? NumSuggestions { get; }
     }
