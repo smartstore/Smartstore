c2b62386a560b4956e28a0116e57ccd6766a9ea3
Murat Cakir <muratc@smartstore.de>
2025-12-17 04:59:35 +0100

    AI metadata: Async loading pattern
    (cherry picked from commit db3fd53589cd97f0986e1cf8fbd1de3dd79effe8)

--- src/Smartstore.Core/Platform/AI/Metadata/IAIMetadataLoader.cs
+++ src/Smartstore.Core/Platform/AI/Metadata/IAIMetadataLoader.cs
@@ -11,5 +11,12 @@
         /// <param name="moduleSystemName">The AI modules's system name to load metadata.json from.</param>"
         /// <exception cref="InvalidOperationException"></exception>
         AIMetadata LoadMetadata(string moduleSystemName);
+
+        /// <summary>
+        /// Asynchronously loads and caches metadata for the given <paramref name="moduleSystemName"/>.
+        /// </summary>
+        /// <param name="moduleSystemName">The system name of the AI module for which to load metadata. Cannot be null or empty.</param>
+        /// <remarks>By default, this method delegates to <see cref="LoadMetadata(string)"/></remarks>
+        Task<AIMetadata> LoadMetadataAsync(string moduleSystemName);
     }
 }
