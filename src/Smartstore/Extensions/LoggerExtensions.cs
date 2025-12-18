33b76fae8338174e4273423f8d374023c51c44d2
Murat Cakir <muratc@smartstore.de>
2025-12-19 00:15:42 +0100

    net10: LoggerExtensions extension block syntax

--- src/Smartstore/Extensions/LoggerExtensions.cs
+++ src/Smartstore/Extensions/LoggerExtensions.cs
@@ -1,153 +1,148 @@
-﻿namespace Microsoft.Extensions.Logging
+﻿#nullable enable
+
+namespace Microsoft.Extensions.Logging;
+
+public static class LoggerExtensions
 {
-    public static class LoggerExtensions
+    #region Is[X]Enabled
+
+    extension (ILogger l)
     {
-        #region Is[X]Enabled
+        public bool IsTraceEnabled()
+            => l.IsEnabled(LogLevel.Trace);
 
-        public static bool IsTraceEnabled(this ILogger l)
-        {
-            return l.IsEnabled(LogLevel.Trace);
-        }
+        public bool IsDebugEnabled()
+            => l.IsEnabled(LogLevel.Debug);
 
-        public static bool IsDebugEnabled(this ILogger l)
-        {
-            return l.IsEnabled(LogLevel.Debug);
-        }
+        public bool IsInfoEnabled()
+            => l.IsEnabled(LogLevel.Information);
 
-        public static bool IsInfoEnabled(this ILogger l)
-        {
-            return l.IsEnabled(LogLevel.Information);
-        }
+        public bool IsWarnEnabled()
+            => l.IsEnabled(LogLevel.Warning);
 
-        public static bool IsWarnEnabled(this ILogger l)
-        {
-            return l.IsEnabled(LogLevel.Warning);
-        }
+        public bool IsErrorEnabled()
+            => l.IsEnabled(LogLevel.Error);
 
-        public static bool IsErrorEnabled(this ILogger l)
-        {
-            return l.IsEnabled(LogLevel.Error);
-        }
+        public bool IsCriticalEnabled()
+            => l.IsEnabled(LogLevel.Critical);
+    }
 
-        public static bool IsCriticalEnabled(this ILogger l)
-        {
-            return l.IsEnabled(LogLevel.Critical);
-        }
+    #endregion
 
-        #endregion
+    #region Log methods
 
-        #region Log methods
-
-        public static void Trace(this ILogger l, string msg, params object[] args)
+    extension (ILogger l)
+    {
+        public void Trace(string? msg, params object?[] args)
         {
             l.LogTrace(msg, args);
         }
 
-        public static void Trace(this ILogger l, Exception ex, params object[] args)
+        public void Trace(Exception? ex, params object?[] args)
         {
             l.LogTrace(ex, null, args);
         }
 
-        public static void Trace(this ILogger l, Func<string> msgFactory, params object[] args)
+        public void Trace(Func<string?> msgFactory, params object?[] args)
         {
             if (l.IsEnabled(LogLevel.Trace))
                 l.LogTrace(msgFactory(), args);
         }
 
-        public static void Trace(this ILogger l, Exception ex, string msg, params object[] args)
+        public void Trace(Exception? ex, string? msg, params object?[] args)
         {
             l.LogTrace(ex, msg, args);
         }
 
 
-        public static void Debug(this ILogger l, string msg, params object[] args)
+        public void Debug(string? msg, params object?[] args)
         {
             l.LogDebug(msg, args);
         }
 
-        public static void Debug(this ILogger l, Exception ex, params object[] args)
+        public void Debug(Exception? ex, params object?[] args)
         {
             l.LogDebug(ex, null, args);
         }
 
-        public static void Debug(this ILogger l, Func<string> msgFactory, params object[] args)
+        public void Debug(Func<string?> msgFactory, params object?[] args)
         {
             if (l.IsEnabled(LogLevel.Debug))
                 l.LogDebug(msgFactory(), args);
         }
 
-        public static void Debug(this ILogger l, Exception ex, string msg, params object[] args)
+        public void Debug(Exception? ex, string? msg, params object?[] args)
         {
             l.LogDebug(ex, msg, args);
         }
 
 
-        public static void Info(this ILogger l, string msg, params object[] args)
+        public void Info(string? msg, params object?[] args)
         {
             l.LogInformation(msg, args);
         }
 
-        public static void Info(this ILogger l, Exception ex, params object[] args)
+        public void Info(Exception? ex, params object?[] args)
         {
             l.LogInformation(ex, null, args);
         }
 
-        public static void Info(this ILogger l, Func<string> msgFactory, params object[] args)
+        public void Info(Func<string?> msgFactory, params object?[] args)
         {
             if (l.IsEnabled(LogLevel.Information))
                 l.LogInformation(msgFactory(), args);
         }
 
-        public static void Info(this ILogger l, Exception ex, string msg, params object[] args)
+        public void Info(Exception? ex, string? msg, params object?[] args)
         {
             l.LogInformation(ex, msg, args);
         }
 
 
-        public static void Warn(this ILogger l, string msg, params object[] args)
+        public void Warn(string? msg, params object?[] args)
         {
             l.LogWarning(msg, args);
         }
 
-        public static void Warn(this ILogger l, Exception ex, params object[] args)
+        public void Warn(Exception? ex, params object?[] args)
         {
             l.LogWarning(ex, null, args);
         }
 
-        public static void Warn(this ILogger l, Func<string> msgFactory, params object[] args)
+        public void Warn(Func<string?> msgFactory, params object?[] args)
         {
             if (l.IsEnabled(LogLevel.Warning))
                 l.LogWarning(msgFactory(), args);
         }
 
-        public static void Warn(this ILogger l, Exception ex, string msg, params object[] args)
+        public void Warn(Exception? ex, string? msg, params object?[] args)
         {
             l.LogWarning(ex, msg, args);
         }
 
 
-        public static void Error(this ILogger l, string msg, params object[] args)
+        public void Error(string? msg, params object?[] args)
         {
             l.LogError(msg, args);
         }
 
-        public static void Error(this ILogger l, Exception ex, params object[] args)
+        public void Error(Exception? ex, params object?[] args)
         {
-            l.LogError(ex, ex.Message, args);
+            l.LogError(ex, ex?.Message, args);
         }
 
-        public static void Error(this ILogger l, Func<string> msgFactory, params object[] args)
+        public void Error(Func<string?> msgFactory, params object?[] args)
         {
             if (l.IsEnabled(LogLevel.Error))
                 l.LogError(msgFactory(), args);
         }
 
-        public static void Error(this ILogger l, Exception ex, string msg, params object[] args)
+        public void Error(Exception? ex, string? msg, params object?[] args)
         {
             l.LogError(ex, msg, args);
         }
 
-        public static void ErrorsAll(this ILogger l, Exception exception)
+        public void ErrorsAll(Exception? exception)
         {
             if (!l.IsEnabled(LogLevel.Error))
             {
@@ -156,33 +151,33 @@
 
             while (exception != null)
             {
-                l.LogError(exception, exception.Message);
-                exception = exception.InnerException;
+                l.LogError(exception, exception?.Message);
+                exception = exception?.InnerException;
             }
         }
 
 
-        public static void Critical(this ILogger l, string msg, params object[] args)
+        public void Critical(string? msg, params object?[] args)
         {
             l.LogCritical(msg, args);
         }
 
-        public static void Critical(this ILogger l, Exception ex, params object[] args)
+        public void Critical(Exception? ex, params object?[] args)
         {
             l.LogCritical(ex, null, args);
         }
 
-        public static void Critical(this ILogger l, Func<string> msgFactory, params object[] args)
+        public void Critical(Func<string?> msgFactory, params object?[] args)
         {
             if (l.IsEnabled(LogLevel.Critical))
                 l.LogCritical(msgFactory(), args);
         }
 
-        public static void Critical(this ILogger l, Exception ex, string msg, params object[] args)
+        public void Critical(Exception? ex, string? msg, params object?[] args)
         {
             l.LogCritical(ex, msg, args);
         }
-
-        #endregion
     }
+
+    #endregion
 }
