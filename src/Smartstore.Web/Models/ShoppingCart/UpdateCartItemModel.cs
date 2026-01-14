2a71c7b9070729676004d3e3667e5b28c70d5ad0
Murat Cakir <muratc@smartstore.de>
2026-01-14 22:10:31 +0100

    Json: UpdateCartItemModel review (don't be so sure about that)

--- src/Smartstore.Web/Models/ShoppingCart/UpdateCartItemModel.cs
+++ src/Smartstore.Web/Models/ShoppingCart/UpdateCartItemModel.cs
@@ -1,4 +1,5 @@
-﻿using Smartstore.Core.Checkout.Cart;
+﻿using System.Text.Json.Serialization;
+using Smartstore.Core.Checkout.Cart;
 
 namespace Smartstore.Web.Models.Cart
 {
@@ -7,31 +8,37 @@ namespace Smartstore.Web.Models.Cart
         /// <summary>
         /// Identifier of <see cref="ShoppingCartItem"/>.
         /// </summary>
+        [JsonPropertyName("cartItemId")]
         public int CartItemId { get; set; }
 
         /// <summary>
         /// A value indicating whether the customer is on the cart page or on any other page.
         /// </summary>
+        [JsonPropertyName("isCartPage")]
         public bool IsCartPage { get; set; }
 
         /// <summary>
         /// A value indicating whether the <see cref="ShoppingCartType"/> is <see cref="ShoppingCartType.Wishlist"/> or <see cref="ShoppingCartType.ShoppingCart"/>.
         /// </summary>
+        [JsonPropertyName("isWishlist")]
         public bool IsWishlist { get; set; }
 
         /// <summary>
         /// A value indicating whether the cart item is active.
         /// </summary>
+        [JsonPropertyName("active")]
         public bool? Active { get; set; }
 
         /// <summary>
         /// A value indicating whether to activate all cart items.
         /// </summary>
+        [JsonPropertyName("activateAll")]
         public bool? ActivateAll { get; set; }
 
         /// <summary>
         /// The new cart item quantity.
         /// </summary>
+        [JsonPropertyName("newQuantity")]
         public int? NewQuantity { get; set; }
     }
 }
