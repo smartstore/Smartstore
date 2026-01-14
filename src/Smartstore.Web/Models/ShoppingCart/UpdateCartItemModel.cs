dcfd1758f4417eed502463fab215925017982c47
Michael Herzog <herzog@smartstore.com>
2026-01-14 13:41:19 +0100

    Json: NSJ --> STJ (UpdateCartItemModel) wasn't even serialized anywhere

--- src/Smartstore.Web/Models/ShoppingCart/UpdateCartItemModel.cs
+++ src/Smartstore.Web/Models/ShoppingCart/UpdateCartItemModel.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using Smartstore.Core.Checkout.Cart;
+﻿using Smartstore.Core.Checkout.Cart;
 
 namespace Smartstore.Web.Models.Cart
 {
@@ -8,37 +7,31 @@ namespace Smartstore.Web.Models.Cart
         /// <summary>
         /// Identifier of <see cref="ShoppingCartItem"/>.
         /// </summary>
-        [JsonProperty("cartItemId")]
         public int CartItemId { get; set; }
 
         /// <summary>
         /// A value indicating whether the customer is on the cart page or on any other page.
         /// </summary>
-        [JsonProperty("isCartPage")]
         public bool IsCartPage { get; set; }
 
         /// <summary>
         /// A value indicating whether the <see cref="ShoppingCartType"/> is <see cref="ShoppingCartType.Wishlist"/> or <see cref="ShoppingCartType.ShoppingCart"/>.
         /// </summary>
-        [JsonProperty("isWishlist")]
         public bool IsWishlist { get; set; }
 
         /// <summary>
         /// A value indicating whether the cart item is active.
         /// </summary>
-        [JsonProperty("active")]
         public bool? Active { get; set; }
 
         /// <summary>
         /// A value indicating whether to activate all cart items.
         /// </summary>
-        [JsonProperty("activateAll")]
         public bool? ActivateAll { get; set; }
 
         /// <summary>
         /// The new cart item quantity.
         /// </summary>
-        [JsonProperty("newQuantity")]
         public int? NewQuantity { get; set; }
     }
 }
