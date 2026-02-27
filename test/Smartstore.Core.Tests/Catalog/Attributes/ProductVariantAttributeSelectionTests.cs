747594891b9929ce61ca43c5da73a2351132f479
mgesing <gesing@smartstore.com>
2026-02-27 10:15:39 +0100

    Added a unit test

--- test/Smartstore.Core.Tests/Catalog/Attributes/ProductVariantAttributeSelectionTests.cs
+++ test/Smartstore.Core.Tests/Catalog/Attributes/ProductVariantAttributeSelectionTests.cs
@@ -10,7 +10,7 @@ namespace Smartstore.Core.Tests.Catalog
     public class ProductVariantAttributeSelectionTests
     {
         [Test]
-        public void CanSerializeAttributeSelectionToJson()
+        public void Can_serialize_attribute_selection_to_json()
         {
             var source = CreateSelection();
             var json = source.AsJson();
@@ -20,7 +20,7 @@ namespace Smartstore.Core.Tests.Catalog
         }
 
         [Test]
-        public void CanSerializeEmptyAttributeSelectionToJson()
+        public void Can_serialize_empty_attribute_selection_to_json()
         {
             var source = new ProductVariantAttributeSelection(string.Empty);
             var json = source.AsJson();
@@ -29,7 +29,17 @@ namespace Smartstore.Core.Tests.Catalog
         }
 
         [Test]
-        public void CanSerializeAttributeSelectionToXml()
+        public void Ensure_no_object_types_in_attribute_selection_json()
+        {
+            var source = CreateSelection(false);
+            var json = source.AsJson();
+            var expectedJson = """{"Attributes":[{"Key":123,"Value":[11,12,13]},{"Key":987,"Value":[36]},{"Key":375,"Value":["Any text."]}]}""";
+
+            Assert.That(json, Is.EqualTo(expectedJson));
+        }
+
+        [Test]
+        public void Can_serialize_attribute_selection_to_xml()
         {
             var source = CreateSelection();
             var xml = source.AsXml();
@@ -39,7 +49,7 @@ namespace Smartstore.Core.Tests.Catalog
         }
 
         [Test]
-        public void AttributeSelectionEqualTo()
+        public void Can_modify_attribute_selection()
         {
             var selection1 = CreateSelection();
             var selection2 = CreateSelection();
@@ -70,60 +80,67 @@ namespace Smartstore.Core.Tests.Catalog
             ProductVariantAttributeSelection emptySelection4 = null;
 
             Assert.That(emptySelection1, Is.EqualTo(emptySelection2));
-            Assert.That(emptySelection1, Is.EqualTo(emptySelection2));
-            Assert.That(emptySelection3, Is.EqualTo(emptySelection4));
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(emptySelection1, Is.EqualTo(emptySelection2));
+                Assert.That(emptySelection3, Is.EqualTo(emptySelection4));
+            }
 
             Assert.That(emptySelection1, Is.Not.EqualTo(emptySelection3));
             Assert.That(emptySelection1, Is.Not.EqualTo(emptySelection3));
         }
 
-        private static readonly int[] expected = new[] { 11, 12, 13 };
-
         private static void ValidateSelection(ProductVariantAttributeSelection selection)
         {
-            Assert.Multiple(() =>
+            using (Assert.EnterMultipleScope())
             {
                 Assert.That(selection.AttributesMap.Count(), Is.EqualTo(3));
                 Assert.That(selection.AttributesMap.Count(x => x.Key == 987), Is.EqualTo(1));
-            });
+            }
 
             var values = selection.GetAttributeValues(123)
                 .Select(x => x.ToString())
                 .Select(x => x.ToInt())
                 .ToArray();
-            values.ShouldSequenceEqual(expected);
+            values.ShouldSequenceEqual([11, 12, 13]);
 
             var textValue = selection.GetAttributeValues(375);
 
-            Assert.Multiple(() =>
+            using (Assert.EnterMultipleScope())
             {
                 Assert.That(textValue.Count(), Is.EqualTo(1));
                 Assert.That(textValue.First().ToString(), Is.EqualTo("Any text."));
-            });
+            }
 
             var giftCard = selection.GetGiftCardInfo();
 
-            Assert.That(giftCard, Is.Not.Null);
-            Assert.That(giftCard.RecipientName == "John Doe" && giftCard.RecipientEmail == "jdow@web.com", Is.True);
-            Assert.That(giftCard.Message, Is.Not.Empty);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(giftCard, Is.Not.Null);
+                Assert.That(giftCard.RecipientName == "John Doe" && giftCard.RecipientEmail == "jdow@web.com", Is.True);
+                Assert.That(giftCard.Message, Is.Not.Empty);
+            }
         }
 
-        private static ProductVariantAttributeSelection CreateSelection()
+        private static ProductVariantAttributeSelection CreateSelection(bool addGiftCardInfo = true)
         {
             var selection = new ProductVariantAttributeSelection(null);
 
-            selection.AddAttribute(123, new object[] { 11, 12, 13 });
-            selection.AddAttribute(987, new object[] { 36 });
+            selection.AddAttribute(123, [11, 12, 13]);
+            selection.AddAttribute(987, [36]);
             selection.AddAttributeValue(375, "Any text.");
 
-            selection.AddGiftCardInfo(new GiftCardInfo
+            if (addGiftCardInfo)
             {
-                RecipientName = "John Doe",
-                RecipientEmail = "jdow@web.com",
-                SenderName = "me",
-                SenderEmail = "me@web.com",
-                Message = "Hello world!"
-            });
+                selection.AddGiftCardInfo(new GiftCardInfo
+                {
+                    RecipientName = "John Doe",
+                    RecipientEmail = "jdow@web.com",
+                    SenderName = "me",
+                    SenderEmail = "me@web.com",
+                    Message = "Hello world!"
+                });
+            }
 
             return selection;
         }
