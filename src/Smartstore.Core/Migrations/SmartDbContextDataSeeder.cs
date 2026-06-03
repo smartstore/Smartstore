4853d3eba392fb6f39f0fcbed960869efc9890d2
mgesing <gesing@smartstore.com>
2026-06-03 13:05:39 +0200

    Rename tax description in order field labels (wip)

--- src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
+++ src/Smartstore.Core/Migrations/SmartDbContextDataSeeder.cs
@@ -23,11 +23,18 @@ public class SmartDbContextDataSeeder : IDataSeeder<SmartDbContext>
             "Admin.Orders.Products.AddNew.UnitPriceInclTax.Hint",
             "Admin.Orders.Products.AddNew.UnitPriceExclTax.Hint",
             "Admin.Orders.Products.AddNew.SubTotalInclTax.Hint",
-            "Admin.Orders.Products.AddNew.SubTotalExclTax.Hint");
+            "Admin.Orders.Products.AddNew.SubTotalExclTax.Hint",
+            "Admin.Orders.Products.Edit",
+            "Admin.Orders.Products.Edit.InclTax",
+            "Admin.Orders.Products.Edit.ExclTax");
 
         builder.AddOrUpdate("Admin.Orders.Products.AddNew.UnitPriceInclTax", "Unit price (gross)", "Einzelpreis (brutto)");
         builder.AddOrUpdate("Admin.Orders.Products.AddNew.UnitPriceExclTax", "Unit price (net)", "Einzelpreis (netto)");
         builder.AddOrUpdate("Admin.Orders.Products.AddNew.SubTotalInclTax", "Total (gross)", "Gesamt (brutto)");
         builder.AddOrUpdate("Admin.Orders.Products.AddNew.SubTotalExclTax", "Total (net)", "Gesamt (netto)");
+
+        builder.AddOrUpdate("Admin.Orders.Fields.Edit.InclTax", "gross", "brutto");
+        builder.AddOrUpdate("Admin.Orders.Fields.Edit.ExclTax", "net", "netto");
+        builder.AddOrUpdate("Admin.Common.TaxPercent", "tax %", "Steuer %");
     }
 }
