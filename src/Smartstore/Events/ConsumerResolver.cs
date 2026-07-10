e1472bc3cb355949bf4f88a7fba2de0e184e6a10
Murat Cakir <muratc@smartstore.de>
2026-07-10 22:38:14 +0200

    Don't resolve module event consumers during installation
    (cherry picked from commit 2574737875ed5265a097ca824b3832ef1d36c192)

--- src/Smartstore/Events/ConsumerResolver.cs
+++ src/Smartstore/Events/ConsumerResolver.cs
@@ -7,16 +7,33 @@ namespace Smartstore.Events;
 
 public class ConsumerResolver : IConsumerResolver
 {
+    private readonly IApplicationContext _appContext;
+
+    public ConsumerResolver(IApplicationContext appContext)
+    {
+        _appContext = appContext;
+    }
+
+
     public virtual IConsumer Resolve(ConsumerDescriptor descriptor)
     {
         var scope = EngineContext.Current.Scope;
 
-        if (descriptor.ModuleDescriptor == null || scope.Resolve<IModuleConstraint>().Matches(descriptor.ModuleDescriptor, null))
+        if (descriptor.ModuleDescriptor != null)
         {
-            return scope.ResolveKeyed<IConsumer>(descriptor.ContainerType);
+            if (!_appContext.IsInstalled)
+            {
+                return null;
+            }
+
+            if (!scope.Resolve<IModuleConstraint>().Matches(descriptor.ModuleDescriptor, null))
+            {
+                return null;
+            }
         }
 
-        return null;
+        return scope.ResolveKeyed<IConsumer>(descriptor.ContainerType);
+
     }
 
     public virtual object ResolveParameter(ParameterInfo p, IComponentContext c = null)
